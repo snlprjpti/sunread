564c4833263a50136078ad7c2b57abe93cf855be
Hemant Achhami <hemant.achhami@hazesoft.co>
2020-03-02 17:49:41 +0545

    minor change

--- Modules/User/Http/Controllers/UserController.php
+++ Modules/User/Http/Controllers/UserController.php
@@ -118,7 +118,7 @@ class UserController extends BaseController
             }
 
             $admin = Admin::findOrFail($id);
-            $admin = $admin->update(
+            $admin->update(
                 $request->only('name', 'email', 'password', 'role_id', 'status')
             );
 
