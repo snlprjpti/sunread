d89907338db2c653d50bd08a7782486f5b28fd5e
Hemant Achhami <hemant.achhami@hazesoft.co>
2021-01-21 09:57:17 +0545

    customer address docs updated

--- Modules/Customer/Http/Controllers/AddressController.php
+++ Modules/Customer/Http/Controllers/AddressController.php
@@ -14,11 +14,10 @@ use Modules\Customer\Entities\CustomerAddress;
 
 /**
  * CustomerAddress controller for the Admin
- * @author    Hemant Achhami
- * @copyright 2020 Hazesoft Pvt Ltd
  */
 class AddressController extends BaseController
 {
+
     protected $pagination_limit;
 
     /**
@@ -33,7 +32,7 @@ class AddressController extends BaseController
     }
 
     /**
-     * Returns all the roles
+     * Returns all the customer address
      * @param $customer_id
      * @return \Illuminate\Http\JsonResponse
      */
@@ -53,19 +52,19 @@ class AddressController extends BaseController
     }
 
     /**
-     * Get the particular role
+     * Get the particular customer address
      * @param $customer_id
      * @param $address_id
      * @return \Illuminate\Http\JsonResponse
      */
-    public function show($customer_id,$address_id)
+    public function show($customer_id, $address_id)
     {
         try {
             $customer_address = CustomerAddress::with('customer')->findOrFail($address_id);
             return $this->successResponse($customer_address);
 
         } catch (ModelNotFoundException $exception) {
-            return $this->errorResponse($exception->getMessage(), 404);
+            return $this->errorResponseForMissingModel($exception);
 
         } catch (\Exception $exception) {
             return $this->errorResponse($exception->getMessage());
@@ -74,7 +73,7 @@ class AddressController extends BaseController
 
 
     /**
-     * Stores new role
+     * Stores new customer address
      * @param Request $request
      * @param $customer_id
      * @return \Illuminate\Http\JsonResponse
@@ -95,17 +94,14 @@ class AddressController extends BaseController
                     'default_address' => 'required'
                 ]);
             $request->merge([
-               'customer_id' => $customer_id
+                'customer_id' => $customer_id
             ]);
-            $customer_address = CustomerAddress::create($request->only('customer_id','address1' ,'address2','country','state','city','postcode','phone','default_address'));
+            $customer_address = CustomerAddress::create($request->only('customer_id', 'address1', 'address2', 'country', 'state', 'city', 'postcode', 'phone', 'default_address'));
             return $this->successResponse($customer_address, trans('core::app.response.create-success', ['name' => 'Customer Address']), 201);
-
+        } catch (ModelNotFoundException $exception) {
+            return $this->errorResponseForMissingModel($exception);
         } catch (ValidationException $exception) {
             return $this->errorResponse($exception->errors(), 422);
-
-        } catch (QueryException $exception) {
-            return $this->errorResponse($exception->getMessage(),404);
-
         } catch (\Exception $exception) {
             return $this->errorResponse($exception->getMessage());
         }
@@ -113,13 +109,13 @@ class AddressController extends BaseController
 
 
     /**
-     * Updates the role details
+     * Updates the customer address
      * @param Request $request
      * @param $customer_id
      * @param $address_id
      * @return \Illuminate\Http\JsonResponse
      */
-    public function update(Request $request, $customer_id ,$address_id)
+    public function update(Request $request, $customer_id, $address_id)
     {
         try {
             $customer = Customer::findOrFail($customer_id);
@@ -139,7 +135,7 @@ class AddressController extends BaseController
             $customer_address->save();
             return $this->successResponse($customer_address, trans('core::app.response.update-success', ['name' => 'Customer Address']));
 
-        }catch (ModelNotFoundException $exception) {
+        } catch (ModelNotFoundException $exception) {
             return $this->errorResponseForMissingModel($exception);
 
         } catch (ValidationException $exception) {
@@ -154,12 +150,12 @@ class AddressController extends BaseController
     }
 
     /**
-     * Destroys the particular role
+     * Destroys the particular customer address
      * @param $customer_id
      * @param $address_id
      * @return \Illuminate\Http\JsonResponse
      */
-    public function destroy($customer_id,$address_id)
+    public function destroy($customer_id, $address_id)
     {
         try {
             $customer = Customer::findOrFail($customer_id);
@@ -168,7 +164,7 @@ class AddressController extends BaseController
             return $this->successResponseWithMessage(trans('core::app.response.deleted-success', ['name' => 'Customer Address']));
 
         } catch (ModelNotFoundException $exception) {
-            return $this->errorResponse($exception->getMessage(), 404);
+            return $this->errorResponseForMissingModel($exception);
 
         } catch (\Exception $exception) {
             return $this->errorResponse($exception->getMessage());
