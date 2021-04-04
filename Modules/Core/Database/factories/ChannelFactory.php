020f7790c1c812b34dc3e441c5f1afe92a9ddfee
Bikram Lama <bikram.lama@hazesoft.co>
2021-04-04 16:37:23 +0545

    Shortned Str

--- Modules/Core/Database/factories/ChannelFactory.php
+++ Modules/Core/Database/factories/ChannelFactory.php
@@ -6,11 +6,11 @@ use Faker\Generator as Faker;
 
 $factory->define(\Modules\Core\Entities\Channel::class, function (Faker $faker) {
     return [
-        'code' => \Illuminate\Support\Str::random(16),
+        'code' => \Str::random(16),
         'name' => $faker->company,
         'description' => $faker->paragraph,
         'timezone' => $faker->timezone,
-        'theme' => \Illuminate\Support\Str::random(16),
+        'theme' => \Str::random(16),
         'default_locale_id' => factory(\Modules\Core\Entities\Locale::class)->create()->id,
         'base_currency_id' => factory(\Modules\Core\Entities\Currency::class)->create()->id,
     ];
