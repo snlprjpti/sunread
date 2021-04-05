871601c259f00089c0e80b36c48d61182f04f98c
Bikram Lama <bikram.lama@hazesoft.co>
2021-04-05 15:38:19 +0545

    Activity Log helper refacrtored

--- Modules/Core/Services/ActivityLogHelper.php
+++ Modules/Core/Services/ActivityLogHelper.php
@@ -7,50 +7,54 @@ use Modules\Core\Entities\ActivityLog;
 
 class ActivityLogHelper {
 
-    private $activityLog;
-    private $user;
-    private $log;
+    private $activityLog, $user, $log;
 
     public function __construct(ActivityLog $activityLog)
     {
         $this->activityLog = $activityLog;
-        $this->user = Auth::user() ? Auth::user() : null;
+        $this->user = Auth::user() ?? null;
 
-        if($this->user != null) {
-            $this->log['causer_id'] = $this->user->id;
-            $this->log['causer_type'] = get_class($this->user);
+        if($this->user) {
+            $this->log = [
+                "causer_id" => $this->user->id,
+                "causer_type" => get_class($this->user)
+            ];
         }
-
     }
 
     public function log($model, $event, $action = null, $activity = null) {
         $model_name = class_basename($model);
-        $default_action = $model_name." ". $event;
-        $this->log['log_name'] = 'default';
-        $this->log['description'] = $event;
-        $this->log['subject_id'] = $model->id;
-        $this->log['subject_type'] = get_class($model);
-        $this->log['action'] = isset($action)? $action: $default_action;
-        $this->log['activity'] = isset($activity)? $activity:$default_action;
+        $default_action = $model_name." ".$event;
+        $properties = [];
 
-        if($event == 'updated') {
+        if ( $event == "updated" ) {
             $newValues = $model->getChanges();
-            $oldValues = [];
-            foreach($newValues as $key=>$value) {
-                $oldValues[$key] = $model->getOriginal($key) ;
-            }
-            $this->log['properties'] = [
+            $oldValues = collect($newValues)->mapWithKeys(function($value, $key) use ($model) {
+                return [$key => $model->getOriginal($key)];
+            })->toArray();
+
+            $properties = [
                 'from' => $oldValues,
                 'to' => $newValues
             ];
-            $this->log['activity'] = $default_action. " for properties: ". implode(',', array_keys($oldValues));
+            $activity = "{$default_action} for properties: ".implode(',', array_keys($oldValues));
         }
-        elseif($event == 'created') {
+
+        if( $event == "created" ) {
             $newValues = $model->toArray();
-            $this->log['properties'] = $newValues;
+            $properties = $newValues;
         }
-        $this->activityLog->create($this->log);
 
+        $log = [
+            "log_name" => 'default',
+            "description" => $event,
+            "subject_id" => $model->id,
+            "subject_type" => get_class($model),
+            "action" => $action ?? $default_action,
+            "activity" => $activity ?? $default_action,
+            "properties" => $properties
+        ];
+
+        $this->activityLog->create( array_merge($this->log, $log) );
     }
-
 }
