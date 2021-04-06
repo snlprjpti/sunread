0bb7a51bdba603c0315c5aa3aaac7868af955bf6
Bikram Lama <bikram.lama@hazesoft.co>
2021-04-06 15:34:51 +0545

    Resources refactored

--- Modules/Attribute/Transformers/AttributeResource.php
+++ Modules/Attribute/Transformers/AttributeResource.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Attribute\Transformers;
 
+use Illuminate\Support\Carbon;
 use Illuminate\Http\Resources\Json\Resource;
 
 class AttributeResource extends Resource
@@ -14,6 +15,23 @@ class AttributeResource extends Resource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            "id" => $this->id,
+            "slug" => $this->slug,
+            "name" => $this->name,
+            "type" => $this->type,
+            "attribute_group" => $this->attribute_group ?? null,
+            "validation" => $this->validation,
+            "position" => $this->position,
+            "is_required" => $this->is_required,
+            "is_unique" => $this->is_unique,
+            "is_filterable" => $this->is_filterable,
+            "is_user_defined" => $this->is_user_defined,
+            "use_in_flat" => $this->use_in_flat,
+            "is_visible_on_front" => $this->is_visible_on_front,
+            "created_at" => Carbon::parse($this->created_at)->format('M j\\,Y H:i A'),
+            "translations" => $this->translations,
+            "options" => $this->attribute_options ?? null
+        ];
     }
 }
