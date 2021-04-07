a48249d51d8e7107761f68de3662ff4c39b1721d
Bikram Lama <bikram.lama@hazesoft.co>
2021-04-07 15:32:38 +0545

    Added Resources

---
+++ Modules/Customer/Transformers/CustomerAddressResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Modules\Customer\Transformers;
+
+use Illuminate\Support\Carbon;
+use Illuminate\Http\Resources\Json\Resource;
+
+class CustomerAddressResource extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            "id" => $this->id,
+            "address1" => $this->address1,
+            "address2" => $this->address2,
+            "country" => $this->country,
+            "state" => $this->state,
+            "city" => $this->city,
+            "postcode" => $this->postcode,
+            "phone" => $this->phone,
+            "default_address" => $this->default_address,
+            "created_at" => Carbon::parse($this->created_at)->format('M j\\,Y H:i A')
+        ];
+    }
+}
