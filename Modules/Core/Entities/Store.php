b1c647632f8f1573c774dd16a92e8aeafe73df22
Rupess <rupeshshrestha9818@gmail.com>
2021-04-21 16:30:50 +0545

    added slugable

--- Modules/Core/Entities/Store.php
+++ Modules/Core/Entities/Store.php
@@ -4,12 +4,14 @@ namespace Modules\Core\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Modules\Core\Traits\Sluggable;
 
 class Store extends Model
 {
+    use Sluggable;
 
     protected $fillable = ["name","currency","locale","image","slug"];
 
-    
+
 
 }
