51301c8a06380d790a1254971b9868994b3aa128
Bikram Lama <bikram.lama@hazesoft.co>
2021-04-21 11:56:13 +0545

    Category Model refactored + added channels relationship

--- Modules/Category/Entities/Category.php
+++ Modules/Category/Entities/Category.php
@@ -3,9 +3,13 @@
 namespace Modules\Category\Entities;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Storage;
 use Kalnoy\Nestedset\NodeTrait;
+use Modules\Core\Entities\Channel;
 use Modules\Core\Traits\Sluggable;
 
 class Category extends Model
@@ -16,79 +20,38 @@ class Category extends Model
     protected $fillable = [ "position", "status", "parent_id", "image", "slug" ];
     protected $with = [ "translations" ];
 
-    public static function rules($id=0,$merge = [])
+    public function image_url(): ?string
     {
-        return  array_merge([
-            'slug' => 'nullable |unique:categories,slug'.($id ? ",$id" : ''),
-            'name' => 'required',
-            'image' => 'sometimes|mimes:jpeg,jpg,bmp,png',
-            'position'=>'sometimes|numeric',
-            'status'=> 'sometimes|boolean',
-
-        ],$merge);
-
-    }
-
-    /**
-     * Get image url for the category image.
-     * 
-     * @return string
-     */
-    public function image_url()
-    {
-        if (! $this->image)
-            return;
-
+        if (!$this->image) return null;
         return Storage::url($this->image);
     }
 
-    /**
-     * Get image url for the category image.
-     * 
-     * @return string
-     */
-    public function getImageUrlAttribute()
+    public function getImageUrlAttribute(): string
     {
         return $this->image_url();
     }
 
-    /**
-     * Getting the root category of a category
-     *
-     * @return Category
-     */
     public function getRootCategories(): Collection
     {
-        return Category::where('parent_id', '=', null)->get();
+        return Category::where('parent_id', null)->get();
     }
 
-    /**
-     * Getting the translations
-     * 
-     * @return CategoryTranslation
-     */
-    public function translations()
+    public function translations(): HasMany
     {
         return $this->hasMany(CategoryTranslation::class, 'category_id');
     }
 
-    /**
-     * Getting parent category
-     * 
-     * @return Category
-     */
-    public function parent()
+    public function parent(): BelongsTo
     {
         return $this->belongsTo(Category::class, 'parent_id');
     }
 
-    /**
-     * Specify category tree
-     *
-     * @param int $id
-     * @return mixed
-     */
-    public function getCategoryTree()
+    public function channels(): BelongsToMany
+    {
+        return $this->belongsToMany(Channel::class);
+    }
+
+    public function getCategoryTree(): Collection
     {
         return $this->id
             ? $this::orderBy('position', 'ASC')->where('id', '!=', $this->id)->get()->toTree()
