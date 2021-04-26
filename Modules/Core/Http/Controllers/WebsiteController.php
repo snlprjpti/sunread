9db98e5c16d1132728f4560056582070ace60118
Anisha Khadka <aneesha,khadka@hazesoft.co>
2021-04-26 14:32:54 -0700

    website repository,resource and controller done

---
+++ Modules/Core/Http/Controllers/WebsiteController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Modules\Core\Http\Controllers;
+
+use Exception;
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Routing\Controller;
+use Modules\Core\Entities\Website;
+use Modules\Core\Repositories\WebsiteRepository;
+use Modules\Core\Transformers\WebsiteResource;
+
+class WebsiteController extends BaseController
+{
+    protected $repository;
+
+    public function __construct(Website $website, WebsiteRepository $websiteRepository)
+    {
+        $this->model = $website;
+        $this->model_name = "Website";
+        $this->repository = $websiteRepository;
+
+        parent::__construct($this->model, $this->model_name);
+    }
+    public function collection(object $data): ResourceCollection
+    {
+        return WebsiteResource::collection($data);
+    }
+    public function resource(object $data): JsonResource
+    {
+        return new WebsiteResource($data);
+    }
+    public function index(Request $request): JsonResponse
+    {
+        try
+        {
+            $this->validateListFiltering($request);
+            $fetched = $this->getFilteredList($request);
+        }
+        catch( Exception $exception )
+        {
+            return $this->handleException($exception);
+        }
+
+        return $this->successResponse($this->collection($fetched), $this->lang('fetch-list-success'));
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        try
+        {
+            $data = $this->repository->validateData($request);
+
+            $created = $this->repository->create($data, function($created) use ($request) {
+            });
+        }
+        catch( Exception $exception )
+        {
+            return $this->handleException($exception);
+        }
+
+        return $this->successResponse($this->resource($created), $this->lang('create-success'), 201);
+    }
+
+    public function show(int $id): JsonResponse
+    {
+        try
+        {
+            $fetched = $this->model->findOrFail($id);
+        }
+        catch( Exception $exception )
+        {
+            return $this->handleException($exception);
+        }
+
+        return $this->successResponse($this->resource($fetched), $this->lang('fetch-success'));
+    }
+
+    public function update(Request $request, int $id): JsonResponse
+    {
+        try
+        {
+            $data = $this->repository->validateData($request);
+
+          
+
+            $updated = $this->repository->update($data, $id, function($updated) use ($request) {
+              
+            });
+        }
+        catch( Exception $exception )
+        {
+            return $this->handleException($exception);
+        }
+
+        return $this->successResponse($this->resource($updated), $this->lang('update-success'));
+    }
+
+    public function destroy(int $id): JsonResponse
+    {
+        try
+        {
+            $this->repository->delete($id, function($deleted) {
+               
+            });
+        }
+        catch( Exception $exception )
+        {
+            return $this->handleException($exception);
+        }
+
+        return $this->successResponseWithMessage($this->lang('delete-success'));
+    }
+
+}
