edd8b78f3e52bbc9c1659852c14ef491888e0a50
Anisha khadka <aneesha.khadka@hazesoft.co>
2021-04-27 11:09:42 +0545

    Repository and Resource Of Configuration was created

---
+++ Modules/Core/Repositories/ConfigurationRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Modules\Core\Repositories;
+
+use Modules\Core\Entities\Configuration;
+use Modules\Core\Repositories\BaseRepository;
+
+class ConfigurationRepository extends BaseRepository
+{
+    public function __construct(Configuration $configuration)
+    {
+        $this->model = $configuration;
+        $this->model_key = "core.configuration";
+        $this->rules = [
+           /* General */
+            "scope" => "required",
+            "scope_id" => "required",
+            "path" => "required",
+            "value" => "required"
+        ];
+    }
+}
