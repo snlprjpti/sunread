18ee3ce7d0241f611a178fffa1718e3916bfb6e0
Anisha khadka <aneesha.khadka@hazesoft.co>
2021-04-27 11:41:43 +0545

    seeder and factory was created

---
+++ Modules/Core/Database/factories/ConfigurationFactory.php
@@ -0,0 +1,34 @@
+<?php
+namespace Modules\Core\Database\factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
+
+class ConfigurationFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = \Modules\Core\Entities\Configuration::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $scope=["default", "website", "channel", "store"];
+
+         return [
+            'scope' => Arr::random($scope),
+            'scope_id' => rand(0,100),
+            'path' => $this->faker->name,
+            'value' => $this->faker->name
+        ];
+    }
+}
+
+
