8ce26d8bacadbbaeefe18bf3ffddf94c6fdf5cee
Bikram Lama <bikram.lama@hazesoft.co>
2021-04-29 12:26:50 +0545

    Added role factory

--- Modules/User/Entities/Role.php
+++ Modules/User/Entities/Role.php
@@ -4,11 +4,12 @@ namespace Modules\User\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Modules\Core\Traits\HasFactory;
 use Modules\Core\Traits\Sluggable;
 
 class Role extends Model
 {
-    use Sluggable;
+    use Sluggable, HasFactory;
 
     public static $SEARCHABLE = [ "name", "description", "permissions", "permission_type", "slug" ];
     protected $fillable = [ "name", "description", "permission_type", "permissions", "slug" ];
