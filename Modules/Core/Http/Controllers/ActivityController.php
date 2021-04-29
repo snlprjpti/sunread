47696ef020140ed12eebaad8ea0bea9053f76b64
Bikram Lama <bikram.lama@hazesoft.co>
2021-04-29 18:03:01 +0545

    ActivityLogController refactored

--- Modules/Core/Http/Controllers/ActivityController.php
+++ Modules/Core/Http/Controllers/ActivityController.php
@@ -3,27 +3,25 @@
 namespace Modules\Core\Http\Controllers;
 
 use Exception;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Database\QueryException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Http\Resources\Json\ResourceCollection;
-use Illuminate\Validation\ValidationException;
 use Modules\Core\Entities\ActivityLog;
 use Modules\Core\Repositories\ActivityRepository;
 use Modules\Core\Transformers\ActivityResource;
 
 class ActivityController extends BaseController
 {
-    private $activityRepository;
+    private $repository;
 
-    public function __construct(ActivityLog $activity_log, ActivityRepository $activityRepository)
+    public function __construct(ActivityLog $activityLog, ActivityRepository $activityRepository)
     {
-        $this->model = $activity_log;
+        $this->model = $activityLog;
         $this->model_name = "Activity Log";
+        $this->repository = $activityRepository;
+
         parent::__construct($this->model, $this->model_name);
-        $this->activityRepository = $activityRepository;
     }
 
     public function collection(object $data): ResourceCollection
@@ -36,72 +34,60 @@ class ActivityController extends BaseController
         return new ActivityResource($data);
     }
 
-    public function index(Request $request)
+    public function index(Request $request): JsonResponse
     {
-        try {
+        try
+        {
             $this->validateListFiltering($request);
-            $activity_logs = $this->getFilteredList($request);
-        } catch (Exception $exception) {
+            $fetched = $this->getFilteredList($request);
+        }
+        catch( Exception $exception )
+        {
             return $this->handleException($exception);
         }
 
-        return $this->successResponse($this->collection($activity_logs), $this->lang('fetch-list-success'));
+        return $this->successResponse($this->collection($fetched), $this->lang('fetch-list-success'));
     }
 
-    /**
-     * Show the specified resource.
-     *
-     * @param int $id
-     * @return JsonResponse
-     */
     public function show(int $id): JsonResponse
     {
-        try {
-            $activity_log = $this->model->findOrFail($id);
-        } catch (Exception $exception) {
+        try
+        {
+            $fetched = $this->model->with(["subject", "causer"])->findOrFail($id);
+        }
+        catch( Exception $exception )
+        {
             return $this->handleException($exception);
         }
 
-        return $this->successResponse($this->collection($activity_log), $this->lang('fetch-success'));
+        return $this->successResponse($this->resource($fetched), $this->lang('fetch-success'));
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return JsonResponse
-     */
-    public function destroy($id)
+    public function destroy(int $id): JsonResponse
     {
-        try {
-            $activity_log = $this->model->findOrFail($id);
-            $activity_log->delete();
-        } catch (Exception $exception) {
+        try
+        {
+            $this->repository->delete($id);
+        }
+        catch( Exception $exception )
+        {
             return $this->handleException($exception);
         }
 
         return $this->successResponseWithMessage($this->lang('delete-success'), 204);
     }
 
-    /**
-     * Remove the specified resource in bullk.
-     *
-     * @param Request $request
-     * @return JsonResponse
-     */
-    public function bulkDelete(Request $request)
+    public function bulkDelete(Request $request): JsonResponse
     {
-        try {
-            $this->validate($request, [
-                'ids' => 'array|required',
-                'ids.*' => 'required|exists:activity_logs,id',
-            ]);
-
-            $activity_logs = $this->model->whereIn('id', $request->ids);
-            $activity_logs->delete();
-        } catch (Exception $exception) {
+        try
+        {
+            $this->repository->bulkDelete($request);
+        }
+        catch (Exception $exception)
+        {
             return $this->handleException($exception);
         }
-        return $this->successResponseWithMessage($this->lang('delete-success'));
+
+        return $this->successResponseWithMessage($this->lang('delete-success'), 204);
     }
 }
