eaf9866ae0c708000b72826acf87d035dee46d54
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-04-29 17:26:24 +0545

    activity log test working progress

--- Modules/Core/Tests/Feature/ActivityTest.php
+++ Modules/Core/Tests/Feature/ActivityTest.php
@@ -2,23 +2,33 @@
 
 namespace Modules\Core\Tests\Feature;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schema;
 use Modules\Core\Entities\ActivityLog;
-use Modules\Core\Tests\BaseTestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Modules\User\Entities\Admin;
+use Modules\User\Entities\Role;
+use Tests\TestCase;
 
-class ActivityTest extends BaseTestCase
+class ActivityTest extends TestCase
 {
+    use RefreshDatabase;
+
     protected object $admin;
     protected array $headers;
 
     public function setUp(): void
     {
         parent::setUp();
+        Schema::disableForeignKeyConstraints();
+        $this->artisan("db:seed", ["--force" => true]);
+
         $this->admin = $this->createAdmin();
 
         $this->model = ActivityLog::class;
-        $this->model_name = "ActivityLog";
+        $this->model_name = "Activity Log";
         $this->route_prefix = "admin.activities";
         $this->default_resource_id = ActivityLog::latest()->first()->id;
         $this->fake_resource_id = 0;
@@ -29,12 +39,42 @@ class ActivityTest extends BaseTestCase
         ];
     }
 
+    public function createAdmin(array $attributes = []): object
+    {
+        $password = $attributes["password"] ?? "password";
+        $role_slug = $attributes["role_slug"] ?? "super-admin";
+        $role = Role::where("slug", $role_slug)->firstOrFail();
+
+        $data = [
+            "password" => Hash::make($password),
+            "role_id" => $role->id
+        ];
+
+        $admin = Admin::factory()->create($data);
+        $token = $this->createToken($admin->email, $password);
+        $this->headers["Authorization"] = "Bearer {$token}";
+
+        return $admin;
+    }
+
+
+    public function createToken(string $admin_email, string $password): ?string
+    {
+        $jwtToken = Auth::guard("admin")
+            ->setTTL( config("jwt.admin_jwt_ttl") )
+            ->attempt([
+                "email" => $admin_email,
+                "password" => $password
+            ]);
+        return $jwtToken ?? null;
+    }
+
     public function testFetchActivityList()
     {
         $response = $this->get(route("{$this->route_prefix}.index"));
 
         $response->assertStatus(200);
-        $response->assertJson([
+        $response->assertJsonFragment([
             "status" => "success",
             "message" => __("core::app.response.fetch-list-success", ["name" => $this->model_name])
         ]);
@@ -42,6 +82,7 @@ class ActivityTest extends BaseTestCase
 
     public function testFetchActivity()
     {
+        dd($this->default_resource_id);
         $response = $this->get(route("{$this->route_prefix}.show", $this->default_resource_id));
 
         $response->assertStatus(200);
