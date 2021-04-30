c141d3868cbf67bc8210fe416165796a40dda639
Anisha khadka <aneesha.khadka@hazesoft.co>
2021-04-30 21:15:40 +0545

    Configuration Factory was updated

--- Modules/Core/Database/factories/ConfigurationFactory.php
+++ Modules/Core/Database/factories/ConfigurationFactory.php
@@ -3,6 +3,10 @@ namespace Modules\Core\Database\factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Arr;
+use Modules\Core\Entities\Channel;
+use Modules\Core\Entities\Configuration;
+use Modules\Core\Entities\Store;
+use Modules\Core\Entities\Website;
 
 class ConfigurationFactory extends Factory
 {
@@ -20,12 +24,35 @@ class ConfigurationFactory extends Factory
      */
     public function definition()
     {
-        $scope=[ "default", "website", "channel", "store" ];
+        $website = Website::factory()->create();
+        $channel = Channel::factory()->create(); 
+        $store = Store::factory()->create();
+
+
+        $scopes = [ "default", "website", "channel", "store" ];
+        $scope = Arr::random($scopes);
+
+        if($scope == "default") $scope_id = 0;
+        if($scope == "website") $scope_id = $website->id;
+        if($scope == "channel") $scope_id = $channel->id;
+        if($scope == "store") $scope_id = $store->id;
+
+        $paths = [ "\Modules\Core\Entities\Currency", "\Modules\User\Entities\Admin", "\Modules\Core\Entities\Website", ];
+        $path = Arr::random($paths);
+
+        while(true) {
+            $exist_configuration = Configuration::where([
+                ['scope', $scope],
+                ['scope_id', $scope_id],
+                ['path', $path]
+            ])->first();
+            if (!$exist_configuration) break;
+        }
 
          return [
-            'scope' => Arr::random($scope),
-            'scope_id' => rand(0,100),
-            'path' => $this->faker->name,
+            'scope' => $scope,
+            'scope_id' => $scope_id,
+            'path' => $path,
             'value' => $this->faker->name
         ];
     }
