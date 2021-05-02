fc4eb16f6011eff0a9e102d2ab84701606351a01
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-05-02 17:42:42 +0545

    customer address refactored

--- Modules/Customer/Repositories/CustomerAddressRepository.php
+++ Modules/Customer/Repositories/CustomerAddressRepository.php
@@ -2,15 +2,10 @@
 
 namespace Modules\Customer\Repositories;
 
-use Exception;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Event;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Request;
+use Modules\Core\Repositories\BaseRepository;
 use Modules\Customer\Entities\CustomerAddress;
-use Modules\Customer\Contracts\CustomerAddressInterface;
 
-class CustomerAddressRepository implements CustomerAddressInterface
+class CustomerAddressRepository extends BaseRepository
 {
     protected $model, $model_key;
 
@@ -18,148 +13,8 @@ class CustomerAddressRepository implements CustomerAddressInterface
     {
         $this->model = $customer_address;
         $this->model_key = "customers.addresses";
-    }
 
-    /**
-     * Get current Model
-     * 
-     * @return Model
-     */
-    public function model()
-    {
-        return $this->model;
-    }
-
-    /**
-     * Create a new resource
-     * 
-     * @param array $data
-     * @param array $translation_data
-     * @return Model
-     */
-    public function create($data)
-    {
-        DB::beginTransaction();
-        Event::dispatch("{$this->model_key}.create.before");
-
-        try
-        {
-            $created = $this->model->create($data);
-        }
-        catch (Exception $exception)
-        {
-            DB::rollBack();
-            throw $exception;
-        }
-
-        Event::dispatch("{$this->model_key}.create.after", $created);
-        DB::commit();
-
-        return $created;
-    }
-
-    /**
-     * Update requested resource
-     * 
-     * @param array $data
-     * @param int $id
-     * @return Model
-     */
-    public function update($data, $id)
-    {
-        DB::beginTransaction();
-        Event::dispatch("{$this->model_key}.update.before");
-
-        try
-        {
-            $updated = $this->model->findOrFail($id);
-            $updated->fill($data);
-            $updated->save();
-        }
-        catch (Exception $exception)
-        {
-            throw $exception;
-        }
-
-        Event::dispatch("{$this->model_key}.update.after", $updated);
-        DB::commit();
-
-        return $updated;
-    }
-
-    /**
-     * Delete requested resource
-     * 
-     * @param int $id
-     * @return Model
-     */
-    public function delete($id)
-    {
-        DB::beginTransaction();
-        Event::dispatch("{$this->model_key}.delete.before");
-
-        try
-        {
-            $deleted = $this->model->findOrFail($id);
-
-            if ($deleted->customers()->count() > 0) throw new CustomersPresentInGroup("Customers are still present in group.");
-            $deleted->delete();
-        }
-        catch (Exception $exception)
-        {
-            throw $exception;
-        }
-
-        Event::dispatch("{$this->model_key}.delete.after", $deleted);
-        DB::commit();
-
-        return $deleted;
-    }
-
-    /**
-     * Delete requested resources in bulk
-     * 
-     * @param Request $request
-     * @return Model
-     */
-    public function bulkDelete($request)
-    {
-        DB::beginTransaction();
-        Event::dispatch("{$this->model_key}.delete.before");
-
-        try
-        {
-            $request->validate([
-                'ids' => 'array|required',
-                'ids.*' => 'required|exists:activity_logs,id',
-            ]);
-
-            $deleted = $this->model->whereIn('id', $request->ids);
-            $deleted->delete();
-        }
-        catch (Exception $exception)
-        {
-            throw $exception;
-        }
-
-        Event::dispatch("{$this->model_key}.delete.after", $deleted);
-        DB::commit();
-
-        return $deleted;
-    }
-
-    /**
-     * Returns validation rules
-     * 
-     * @param int $id
-     * @param array $merge
-     * @return array
-     */
-    public function rules($id, $merge = [])
-    {
-        $id = $id ? ",{$id}" : null;
-
-        return array_merge([
+        $this->rules = [
             "address1" => "required|min:2|max:500",
             "address2" => "sometimes",
             "country" => "required",
@@ -168,20 +23,7 @@ class CustomerAddressRepository implements CustomerAddressInterface
             "postcode" => "required",
             "phone" => "required",
             "default_address" => "required"
-        ], $merge);
+        ];
     }
 
-    /**
-     * Validates form request
-     * 
-     * @param Request $request
-     * @param int $id
-     * @return array
-     */
-    public function validateData($request, $id=null, $merge = [])
-    {
-        $data = $request->validate($this->rules($id, $merge));
-
-        return $data;
-    }
 }
