f0e9394518fc6e3387ed4c2be745c4756a1246c1
Anisha khadka <aneesha.khadka@hazesoft.co>
2021-05-02 11:35:51 +0545

    All the logic code of index method of controller are moved to repository in configuration

--- Modules/Core/Http/Controllers/ConfigurationController.php
+++ Modules/Core/Http/Controllers/ConfigurationController.php
@@ -35,37 +35,7 @@ class ConfigurationController extends BaseController
     {
         try
         {
-            $fetched = $this->config_fields;
-            $checkKey = [ 'scope' => $request->scope, 'scope_id' => $request->scope_id ];
-            foreach($fetched as $key => $data)
-            {
-                if(!isset($data['children'])) continue;
-                foreach($data['children'] as $i => $children)
-                {
-                    if(!isset($children['subChildren'])) continue;
-                    foreach($children['subChildren'] as $j => &$subchildren)
-                    {
-                        if(!isset($subchildren['elements'])) continue;
-                        foreach($subchildren['elements'] as $k => &$element)
-                        {
-                            if(!in_array($request->scope, $element['showIn']))
-                            {
-                                unset($subchildren['elements'][$k]);
-                                continue;
-                            }
-                            $checkKey["path"] = $element['path'];
-                            $element['default'] = $this->has((object) $checkKey) ? $this->getDefaultValues((object) $checkKey) : "" ;
-                            $element['value'] = $this->cacheQuery((object) $checkKey, $element['pluck']);
-                            $element['absolute_path'] = $key.'.children.'.$i.'.subChildren.'.$j.'.elements.'.$k;
-                            $subchildren['elements'][$k] = $element;
-                            
-                        }
-                        $children['subChildren'][$j] = $subchildren;
-                    }
-                    $data['children'][$i] = $children;
-                }
-                $fetched[$key] = $data;
-            }
+            $fetched = $this->repository->getConfigData($request);
         }
         catch( Exception $exception )
         {
@@ -79,7 +49,6 @@ class ConfigurationController extends BaseController
     {
         try
         {
-
             $rules = isset($request->absolute_path) ? config('configuration.'.$request->absolute_path.'.rules') : "";
             $data = $this->repository->validateData($request, [
                 "scope_id" => ["required", "integer", "min:0", new ConfigurationRule($request->scope)],
--- Modules/Core/Repositories/ConfigurationRepository.php
+++ Modules/Core/Repositories/ConfigurationRepository.php
@@ -4,9 +4,13 @@ namespace Modules\Core\Repositories;
 
 use Modules\Core\Entities\Configuration;
 use Modules\Core\Repositories\BaseRepository;
+use Modules\Core\Traits\Configuration as TraitsConfiguration;
 
 class ConfigurationRepository extends BaseRepository
 {
+    protected $config_fields;
+    use TraitsConfiguration;
+
     public function __construct(Configuration $configuration)
     {
         $this->model = $configuration;
@@ -18,5 +22,43 @@ class ConfigurationRepository extends BaseRepository
             "path" => "required",
             "value" => "nullable"
         ];
+        $this->config_fields = config("configuration");
+        $this->createModel();
+    }
+
+    public function getConfigData(object $request): array
+    {
+        $fetched = $this->config_fields;
+        $checkKey = [ 'scope' => $request->scope, 'scope_id' => $request->scope_id ];
+        foreach($fetched as $key => $data)
+        {
+            if(!isset($data['children'])) continue;
+            foreach($data['children'] as $i => $children)
+            {
+                if(!isset($children['subChildren'])) continue;
+                foreach($children['subChildren'] as $j => &$subchildren)
+                {
+                    if(!isset($subchildren['elements'])) continue;
+                    foreach($subchildren['elements'] as $k => &$element)
+                    {
+                        if(!in_array($request->scope, $element['showIn']))
+                        {
+                            unset($subchildren['elements'][$k]);
+                            continue;
+                        }
+                        $checkKey["path"] = $element['path'];
+                        $element['default'] = $this->has((object) $checkKey) ? $this->getDefaultValues((object) $checkKey) : "" ;
+                        $element['value'] = $this->cacheQuery((object) $checkKey, $element['pluck']);
+                        $element['absolute_path'] = $key.'.children.'.$i.'.subChildren.'.$j.'.elements.'.$k;
+                        $subchildren['elements'][$k] = $element;
+                        
+                    }
+                    $children['subChildren'][$j] = $subchildren;
+                }
+                $data['children'][$i] = $children;
+            }
+            $fetched[$key] = $data;
+        }
+        return $fetched;
     }
 }
