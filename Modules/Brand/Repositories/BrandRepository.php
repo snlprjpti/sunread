3b8d5148a30f1d20c5c379baf06585a5bbb2e2ef
Rupess <rupeshshrestha9818@gmail.com>
2021-05-04 17:41:18 +0545

    added setup and rules

--- Modules/Brand/Repositories/BrandRepository.php
+++ Modules/Brand/Repositories/BrandRepository.php
@@ -2,9 +2,23 @@
 
 namespace Modules\Brand\Repositories;
 
+use Modules\Brand\Entities\Brand;
 use Modules\Core\Repositories\BaseRepository;
 
 class BrandRepository extends BaseRepository
 {
-	
+	public function __construct(Brand $brand)
+	{
+		$this->model = $brand;
+        $this->model_key = "brands";
+        $this->rules = [
+            "name" => "required",
+            "slug" => "nullable|unique:brands,slug",
+            "description" => "required",
+            "image" => "required|mimes:bmp,jpeg,jpg,png,webp",
+			"meta_title" => "sometimes|nullable",
+			"meta_description" => "sometimes|nullable",
+			"meta_keywords" => "sometimes|nullable"    
+        ];
+	}
 }
