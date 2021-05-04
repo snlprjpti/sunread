15aa6f086f0055c26505ee42007062e283f9d275
Rupess <rupeshshrestha9818@gmail.com>
2021-05-04 17:41:39 +0545

    updated brand resource

--- Modules/Brand/Transformers/BrandResource.php
+++ Modules/Brand/Transformers/BrandResource.php
@@ -14,6 +14,16 @@ class BrandResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "slug" => $this->slug,
+            "image" => $this->image_url,
+            "description" => $this->description,
+            "meta_title" => $this->meta_title,
+            "meta_description" => $this->meta_description,
+            "meta_keywords" => $this->meta_keywords,
+            "created_at" => $this->created_at->format("M d, Y H:i A")
+        ];
     }
 }
