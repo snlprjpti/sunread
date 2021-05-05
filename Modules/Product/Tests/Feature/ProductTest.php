cd8af330bab6178330e0117219d07cd3255fc947
Bikram Lama <bikram.lama@hazesoft.co>
2021-05-05 12:09:48 +0545

    Added product test case

---
+++ Modules/Product/Tests/Feature/ProductTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Modules\Product\Tests\Feature;
+
+use Modules\Core\Tests\BaseTestCase;
+use Modules\Product\Entities\Product;
+
+class ProductTest extends BaseTestCase
+{
+    public function setUp(): void
+    {
+        $this->model = Product::class;
+
+        parent::setUp();
+        $this->admin = $this->createAdmin();
+
+        $this->model_name = "Product";
+        $this->route_prefix = "admin.products";
+    }
+
+    public function getNonMandodtaryCreateData(): array
+    {
+        return array_merge($this->getCreate(), [
+            "parent_id" => null,
+            "brand_id" => null
+        ]);
+    }
+
+    public function getInvalidCreateData(): array
+    {
+        return array_merge($this->getCreateData(), [
+            "sku" => null
+        ]);
+    }
+
+    public function getNonMandodtaryUpdateData(): array
+    {
+        return array_merge($this->getUpdateData(), [
+            "logo" => null,
+            "favicon" => null
+        ]);
+    }
+}
