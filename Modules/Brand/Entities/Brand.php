c618ab0ac1fd801733e05950d2ccdfda62f95eea
Rupess <rupeshshrestha9818@gmail.com>
2021-05-05 10:09:36 +0545

    updated use hasfactory

--- Modules/Brand/Entities/Brand.php
+++ Modules/Brand/Entities/Brand.php
@@ -3,7 +3,7 @@
 namespace Modules\Brand\Entities;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Modules\Core\Traits\HasFactory;
 use Illuminate\Support\Facades\Storage;
 use Modules\Core\Traits\Sluggable;
 
