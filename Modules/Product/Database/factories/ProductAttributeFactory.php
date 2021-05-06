cb557ac72cfb08b186262fc42bee7c3d55c98982
Bikram Lama <bikram.lama@hazesoft.co>
2021-05-06 11:15:02 +0545

    Added Factories

---
+++ Modules/Product/Database/factories/ProductAttributeFactory.php
@@ -0,0 +1,28 @@
+<?php
+namespace Modules\Product\Database\factories;
+
+use Modules\Core\Entities\Store;
+use Modules\Product\Entities\Product;
+use Modules\Attribute\Entities\Attribute;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductAttributeFactory extends Factory
+{
+    protected $model = \Modules\Product\Entities\ProductAttribute::class;
+
+    public function definition(): array
+    {
+        $attribute_types = config("attribute_types");
+        $attribute_type = array_rand($attribute_types);
+        $attribute_model = $attribute_types[$attribute_type];
+
+        return [
+            "attribute_id" => Attribute::factory()->create(["type" => $attribute_type])->id,
+            "channel_id" => null,
+            "product_id" => Product::factory()->create()->id,
+            "store_id" => Store::factory()->create()->id,
+            "value_type" => $attribute_model,
+            "value_id" => $attribute_model::factory()->create()->id
+        ];
+    }
+}
