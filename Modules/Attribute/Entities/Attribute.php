ec4746037e1bc011c3144fb27a76049e5c4ea527
Bikram Lama <bikram.lama@hazesoft.co>
2021-05-09 19:16:28 +0545

    Added validation generator

--- Modules/Attribute/Entities/Attribute.php
+++ Modules/Attribute/Entities/Attribute.php
@@ -29,4 +29,15 @@ class Attribute extends Model
     {
         return $this->hasMany(AttributeTranslation::class);
     }
+
+    public function generateValidation(): array
+    {
+        $validation = $this->is_required ? ["required"] : ["sometimes", "nullable"];
+        return array_merge($validation, [config("attribute_types")[$this->type]::$type]);
+    }
+
+    public function getValidationAttribute(): ?array
+    {
+        return $this->validation ?? $this->generateValidation();
+    }
 }
