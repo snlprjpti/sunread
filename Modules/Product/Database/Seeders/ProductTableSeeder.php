21d4778c861c71704fb84d62810172f2962dde65
Bikram Lama <bikram.lama@hazesoft.co>
2021-05-10 12:04:35 +0545

    removed unused dependency

--- Modules/Product/Database/Seeders/ProductTableSeeder.php
+++ Modules/Product/Database/Seeders/ProductTableSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Modules\Product\Database\Seeders;
 
-use Attribute;
 use Illuminate\Database\Seeder;
 use Modules\Product\Entities\Product;
 use Modules\Product\Entities\ProductAttribute;
