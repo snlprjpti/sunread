c0b0dea11ca24e4857da1a35086e229893dbf998
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-05-13 16:30:26 +0545

    coupon migration

--- Modules/Coupon/Database/Migrations/2021_05_13_103444_create_coupons_table.php
+++ Modules/Coupon/Database/Migrations/2021_05_13_103444_create_coupons_table.php
@@ -15,7 +15,25 @@ class CreateCouponsTable extends Migration
     {
         Schema::create('coupons', function (Blueprint $table) {
             $table->id();
+            $table->string("code")->unique();
+            $table->string("name");
+            $table->text("description")->nullable();
 
+            $table->date('valid_from');
+            $table->date('valid_to');
+
+            $table->decimal("flat_discount_amount")->nullable();
+            $table->decimal("min_discount_amount")->nullable();
+            $table->decimal("max_discount_amount")->nullable();
+            $table->decimal("min_purchase_amount")->nullable();
+
+            $table->float("discount_percent")->nullable();
+            $table->integer("max_uses");
+            $table->integer("single_user_uses")->default(1);
+
+            $table->boolean("only_new_user")->default(0);
+            $table->boolean("scope_public")->default(0);
+            $table->boolean("status")->default(1);
             $table->timestamps();
         });
     }
