35f9c8c5fb86659aee1be4d37ac27093e7a93abc
Anisha Khadka <aneesha,khadka@hazesoft.co>
2021-05-13 12:51:32 +0545

    Url main repository is created and url rewrite CRUD is completed

---
+++ Modules/UrlRewrite/Repositories/UrlRewriteMainRepository.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Modules\UrlRewrite\Repositories;
+
+
+use Modules\Core\Repositories\BaseRepository;
+use Modules\UrlRewrite\Entities\UrlRewrite;
+
+class UrlRewriteMainRepository extends BaseRepository
+{
+    protected $repository;
+
+    public function __construct(UrlRewrite $url_rewrite)
+    {
+        $this->model = $url_rewrite;
+        $this->model_key = "url_rewrites";
+        $this->rules = [
+            "type" => "required",
+            "type_attributes" =>"required|array",
+            "request_path" => "required",
+            "target_path" => "required",
+            "redirect_type" => "sometimes",
+        ];
+    }
+
+    public function geturlRewriteData(array $item): array
+    {
+        $urlRewrite = [];
+        $config_base_name = "url-rewrite.types.".$item['type'];
+        $urlRewrite['type'] = config("{$config_base_name}.route");
+
+        switch ($item['type']) {
+            case "product":
+                $urlRewrite['type_attributes']["parameter"]["product"] = $item['parameter_id'];
+                break;
+
+            case "category":
+                $urlRewrite['type_attributes']["parameter"]["category"] = $item['parameter_id'];
+                break;
+        }
+        
+        $urlRewrite['request_path'] = $item['request_path'];
+
+        if($item['store_id']) $urlRewrite['type_attributes']["extra_fields"]["store_id"] = $item['store_id'];
+        
+        $urlRewrite['target_path'] = route($urlRewrite['type'],  $urlRewrite['type_attributes']["parameter"], false);
+        
+
+        return $urlRewrite;
+    }
+
+    public function ValidateUrlRewrite(object $request): array
+    {
+        $data = $request->validate([
+            "type" => "required|in:product, category",
+            "parameter_id" => "required",
+            "store_id" => "sometimes|exists:stores,id",
+            "request_path" => "required" 
+        ]);
+        return $data;
+    }
+}
