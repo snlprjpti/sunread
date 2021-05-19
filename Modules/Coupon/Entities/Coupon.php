b7de997398d43d1d2acd6212cdb9e2bce2743f7c
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-05-19 15:59:34 +0545

    relation define in coupon

--- Modules/Coupon/Entities/Coupon.php
+++ Modules/Coupon/Entities/Coupon.php
@@ -3,6 +3,7 @@
 namespace Modules\Coupon\Entities;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Modules\Core\Traits\HasFactory;
 
 class Coupon extends Model
@@ -12,4 +13,8 @@ class Coupon extends Model
     protected $fillable = ["code","name","description","valid_from","valid_to","flat_discount_amount","min_discount_amount", "max_discount_amount",
         "discount_percent","max_uses","single_user_uses","only_new_user","min_purchase_amount","scope_public","status"];
 
+    public function allowCoupon(): HasMany
+    {
+        return $this->hasMany(AllowCoupon::class);
+    }
 }
