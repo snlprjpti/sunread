50c146921c40be742d510e3f6f0b10434a387691
Rupess <rupeshshrestha9818@gmail.com>
2021-05-20 15:32:34 +0545

    removed unnessary code

--- Modules/Product/Entities/ProductAttributeString.php
+++ Modules/Product/Entities/ProductAttributeString.php
@@ -4,18 +4,13 @@ namespace Modules\Product\Entities;
 
 use Modules\Core\Traits\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Modules\UrlRewrite\Traits\HasUrlRewrite;
 
 class ProductAttributeString extends Model
 {
-    use HasFactory, HasUrlRewrite;
+    use HasFactory;
 
     public static $type = "string";
     protected $fillable = [ "value" ];
     protected $table = "product_attribute_string";
 
-    public function product_attribute()
-    {
-        return $this->morphOne(ProductAttribute::class, "value");
-    }
 }
