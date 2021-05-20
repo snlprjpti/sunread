278dc773494bdacf65ca21146edd3122c78f08d9
Rupess <rupeshshrestha9818@gmail.com>
2021-05-20 15:19:39 +0545

    updated format and whenloaded fn

--- Modules/Category/Transformers/CategoryTranslationResource.php
+++ Modules/Category/Transformers/CategoryTranslationResource.php
@@ -2,7 +2,6 @@
 
 namespace Modules\Category\Transformers;
 
-use Illuminate\Support\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Modules\Core\Transformers\StoreResource;
 
@@ -15,13 +14,11 @@ class CategoryTranslationResource extends JsonResource
             "name" => $this->name,
             "description" => $this->description,
             "url" => $this->url,
-            "store" => new StoreResource($this->store),
-
+            "store" => new StoreResource($this->whenLoaded("store")),
             "meta_title" => $this->meta_title,
             "meta_description" => $this->meta_description,
             "meta_keywords" => $this->meta_keywords,
-
-            "created_at" => Carbon::parse($this->created_at)->format('M j\\,Y H:i A'),
+            "created_at" => $this->created_at->format('M d, Y H:i A')
         ];
     }
 }
