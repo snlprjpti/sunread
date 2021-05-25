b085dd6b76e783f699a69a713a690a0d0555779d
Anisha khadka <aneesha.khadka@hazesoft.co>
2021-05-25 19:02:51 +0545

    test code

--- Modules/Product/Http/Controllers/ProductController.php
+++ Modules/Product/Http/Controllers/ProductController.php
@@ -39,8 +39,45 @@ class ProductController extends BaseController
     {
         try
         {
-            $this->validateListFiltering($request);
-            $fetched = $this->getFilteredList($request, ["product_attributes", "images"]);
+        $product = Product::find(10);
+        $array = $product->toArray();
+
+        $array['categories'] = $product->categories;
+
+        $array['channels'] = $product->channels;
+
+        $stores = $product->channels->map(function($channel){
+            dd($channel);
+            return[
+                $channel->stores->id
+            ];
+        })->toArray();
+
+
+        $array['product_attributes'] = [
+            'global' => [],
+            'channel' => [],
+            'store' => []
+        ];
+        foreach($product->product_attributes as $data){
+            $attribute_value = [
+                $data->attribute->slug => isset($data->value->value) ? $data->value->value : ""
+            ];
+
+            if(!isset($data->store_id) && !isset($data->channel_id)) $array['product_attributes']['global'] = array_merge($array['product_attributes']['global'], $attribute_value);
+            
+            if(!isset($data->store_id) && isset($data->channel_id))
+            {
+                if(in_array($data->channel_id, $product->channels->pluck('id')->toArray())) $array['product_attributes']['channel'][$data->channel_id] = (array_key_exists ($data->channel_id, $array['product_attributes']['channel'])) ? array_merge($array['product_attributes'] ['channel'][$data->channel_id], $attribute_value) : $attribute_value;
+            }
+
+            if(isset($data->store_id) && !isset($data->channel_id)) 
+            {
+                if(in_array($data->store_id, $stores)) $array['product_attributes'] ['store'][$data->store_id] = (array_key_exists ($data->store_id, $array['product_attributes']['store'])) ? array_merge($array['product_attributes'] ['store'][$data->store_id], $attribute_value) : $attribute_value;
+            }
+        }
+
+        dd($array);
         }
         catch( Exception $exception )
         {
--- Modules/Product/Traits/IndexFormat.php
+++ Modules/Product/Traits/IndexFormat.php
@@ -12,6 +12,14 @@ trait IndexFormat
 
         $array['channels'] = $this->channels;
 
+        $stores = $this->channels->map(function($channel){
+            $channel->stores->map(function($store){
+                return [
+                    $store->id
+                ];
+            });
+        })->toArray();
+
         $array['product_attributes'] = [
             'global' => [],
             'channel' => [],
@@ -25,9 +33,15 @@ trait IndexFormat
 
             if(!isset($data->store_id) && !isset($data->channel_id)) $array['product_attributes']['global'] = array_merge($array['product_attributes']['global'], $attribute_value);
             
-            if(!isset($data->store_id) && isset($data->channel_id)) $array['product_attributes']['channel'][$data->channel_id] = (array_key_exists ($data->channel_id, $array['product_attributes']['channel'])) ? array_merge($array['product_attributes'] ['channel'][$data->channel_id], $attribute_value) : $attribute_value;
+            if(!isset($data->store_id) && isset($data->channel_id))
+            {
+                if(in_array($data->channel_id, $this->channels->pluck('id')->toArray())) $array['product_attributes']['channel'][$data->channel_id] = (array_key_exists ($data->channel_id, $array['product_attributes']['channel'])) ? array_merge($array['product_attributes'] ['channel'][$data->channel_id], $attribute_value) : $attribute_value;
+            }
 
-            if(isset($data->store_id) && !isset($data->channel_id)) $array['product_attributes'] ['store'][$data->store_id] = (array_key_exists ($data->store_id, $array['product_attributes']['store'])) ? array_merge($array['product_attributes'] ['store'][$data->store_id], $attribute_value) : $attribute_value;
+            if(isset($data->store_id) && !isset($data->channel_id)) 
+            {
+                if(in_array($data->store_id, $stores)) $array['product_attributes'] ['store'][$data->store_id] = (array_key_exists ($data->store_id, $array['product_attributes']['store'])) ? array_merge($array['product_attributes'] ['store'][$data->store_id], $attribute_value) : $attribute_value;
+            }
         }
 
         return $array;
