ae02ccc7185fa7eb4a5c1c1a680338e1bcef9d89
Rupesh <rupeshshrestha9818@gmail.com>
2021-06-01 09:28:26 +0545

    added translation trait

--- Modules/Attribute/Entities/Attribute.php
+++ Modules/Attribute/Entities/Attribute.php
@@ -6,14 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Modules\Core\Traits\HasFactory;
+use Modules\Core\Traits\HasTranslation;
 use Modules\Core\Traits\Sluggable;
 
 class Attribute extends Model
 {
-    use Sluggable, HasFactory;
+    use Sluggable, HasFactory, HasTranslation;
 
     public static $SEARCHABLE = [ "name", "type" ];
     protected $fillable = [ "attribute_group_id", "slug", "name", "type", "position", "validation", "is_required", "is_unique", "is_filterable", "is_user_defined", "is_visible_on_front" ];
+
+    public $translatedAttributes = ["name"];
 
     public function attribute_group(): BelongsTo
     {
