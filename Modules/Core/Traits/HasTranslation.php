4ecc8641634ef2a513d9abac4996cbf04e2c9f37
Rupesh <rupeshshrestha9818@gmail.com>
2021-06-02 09:50:02 +0545

    updated traits

--- Modules/Core/Traits/HasTranslation.php
+++ Modules/Core/Traits/HasTranslation.php
@@ -2,22 +2,36 @@
 
 namespace Modules\Core\Traits;
 
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
+
 trait HasTranslation 
 {
     protected $translationStoreID;
 
-	public function allTranslations($storeID = null)
+	public function allTranslations(?int $storeID = null, ?array $with = [])
 	{
 		$this->translationStoreID = (!$storeID) ? $this->getStore() : $storeID;
-        $model = $this->all();
-        
-		if (!$this->translationStoreID) return $model;
+        if ($with != []) {   
+            $model = $this->with($with)->get();
+        }
+        else {
+            $model = $this->all();
+        }
+        if (!$this->translationStoreID) return $model;
 
 		$translated_data = [];
 		foreach($model as $data) $translated_data[] = $this->translation($data);
-
-		return collect($translated_data);
+		return new Collection($translated_data);
 	}
+
+    public function paginate(collection $items, int $perPage = 15, string $page = null, array $options = [])
+    {
+        $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
+        $items = $items instanceof Collection ? $items : Collection::make($items);
+        return new LengthAwarePaginator($items->forPage($page, $perPage), $items->count(), $perPage, $page, $options);
+    }
 
 	public function translation($data)
     {
@@ -33,7 +47,7 @@ trait HasTranslation
  
     public function getTranslateData($data)
     {
-        return  $data->translations()->where('store_id', $this->translationStoreID)->first(); 
+        return $data->translations()->where('store_id', $this->translationStoreID)->first(); 
     }
 
     public function firstTranslation($storeID = null)
