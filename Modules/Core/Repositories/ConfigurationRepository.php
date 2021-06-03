700563454c17fc6f0c89069c4663c0504ec435c8
Anisha khadka <aneesha.khadka@hazesoft.co>
2021-06-03 10:27:06 +0545

    line break

--- Modules/Core/Repositories/ConfigurationRepository.php
+++ Modules/Core/Repositories/ConfigurationRepository.php
@@ -47,9 +47,11 @@ class ConfigurationRepository extends BaseRepository
                         }
                         $checkKey["path"] = $element['path'];
                         $checkKey["provider"] = $element['provider'];
+
                         $element['default'] = $this->has((object) $checkKey) ? $this->getDefaultValues((object) $checkKey) : $element['default'];
                         if( $element['provider'] !== "") $element['value'] = ( $element['values'] === "") ? $this->cacheQuery((object) $checkKey, $element['pluck']) : $element['values'];
                         // $element['absolute_path'] = $key.'.children.'.$i.'.subChildren.'.$j.'.elements.'.$k;
+                        
                         unset($element['values'], $element['pluck'], $element['provider'], $element['rules'], $element['showIn']);
                         $subchildren['elements'][$k] = $element;
                         }
