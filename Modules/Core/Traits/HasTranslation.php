ba1e147fc93c5917677a879c6799117e19593843
Anisha khadka <aneesha.khadka@hazesoft.co>
2021-06-03 18:22:27 +0545

    firstTranslation method added in trait

--- Modules/Core/Traits/HasTranslation.php
+++ Modules/Core/Traits/HasTranslation.php
@@ -35,4 +35,16 @@ trait HasTranslation
 		return array_key_exists("store_id", getallheaders()) ? (int) getallheaders()["store_id"] : 0;
 	}
 
+    public function firstTranslation($store_id)
+    {
+        $translation = $this->translations()->where('store_id', $store_id)->first(); 
+        if($translation) 
+        {
+            $item = $this->toArray();
+            foreach($this->translatedAttributes as $attribute) $item[$attribute] = $translation->$attribute;
+            return $item;
+        }
+        return $this->toArray();
+    }
+
 }
