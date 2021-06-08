dc12c91d1f2b2f45bb04c541f8a85d1b2ce4fc70
Anisha khadka <aneesha.khadka@hazesoft.co>
2021-06-08 16:02:11 +0545

    product attributes data added in product seeder

--- Modules/Product/Database/Seeders/ProductTableSeeder.php
+++ Modules/Product/Database/Seeders/ProductTableSeeder.php
@@ -4,6 +4,8 @@ namespace Modules\Product\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Modules\Attribute\Entities\Attribute;
+use Modules\Core\Entities\Store;
 use Modules\Product\Entities\Product;
 use Modules\Product\Entities\ProductAttribute;
 
@@ -16,15 +18,23 @@ class ProductTableSeeder extends Seeder
                 "parent_id" => Product::factory()->configurable()->create()->id,
                 "attribute_group_id" => 1
             ]);
-            DB::table('product_attributes')->insert([
-                [
+
+            $attribute_types = config("attribute_types");
+            unset($attribute_types["image"], $attribute_types["file"]);
+ 
+            for($i = 0; $i < 4; $i++)
+            {
+                $attribute_type = array_rand($attribute_types);
+                $attribute_model = $attribute_types[$attribute_type];
+                ProductAttribute::create([
                     "product_id" => $product->id,
-                    "attribute_id" => 1,
-                    "value_type" => "Modules\Product\Entities\ProductAttributeString",
-                    "value_id" => null,
-                    "store_id" => 1,
+                    "attribute_id" => Attribute::factory()->create(["type" => $attribute_type])->id,
+                    "value_type" => $attribute_model,
+                    "value_id" => $attribute_model::factory()->create()->id,
+                    "store_id" => Store::factory()->create()->id,
                     "channel_id" => null
-                ]
-            ]);
+                ]);
+            }
+
     }
 }
--- Modules/Product/Routes/api.php
+++ Modules/Product/Routes/api.php
@@ -5,6 +5,9 @@ Route::group(["middleware" => ["api"]], function() {
     // ADMIN PRODUCT ROUTES
     Route::group(["prefix" => "admin/catalog", "as" => "admin.catalog.", "middleware" => ["admin", "language"]], function() {
 
+        //Product Attribute Routes 
+        Route::post('products/attributes', [\Modules\Product\Http\Controllers\ProductAttributeController::class, "store"])->name("products.attributes.store");
+        
         Route::get('products/search', [\Modules\Product\Http\Controllers\ProductSearchController::class, "index"]);
 
         Route::post('products/reindex/bulk', [\Modules\Product\Http\Controllers\ProductSearchController::class, "bulkReIndex"]);
@@ -14,9 +17,6 @@ Route::group(["middleware" => ["api"]], function() {
         Route::put("/products/{product_id}/update-status", [\Modules\Product\Http\Controllers\ProductController::class, "updateStatus"])->name("products.status");
         Route::resource("products", ProductController::class)->except(["create", "edit"]);
 
-        //Product Attribute Routes 
-        Route::post('products/attributes', [\Modules\Product\Http\Controllers\ProductAttributeController::class, "store"])->name("products.attributes.store");
-        
         // Product Images Routes
         Route::group(['prefix' => 'product', 'as' => 'products.'], function() {
             Route::put('image/{id}/change-main-image', [\Modules\Product\Http\Controllers\ProductImageController::class,"changeMainImage"])->name("image.change_main_image");
