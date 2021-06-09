b4e51081e00d82f6126be17345353679df5cbd67
Bikram Lama <thebikramlama1123@gmail.com>
2021-06-09 13:50:09 +0545

    Refactored table seeder

--- Modules/Product/Database/Seeders/ProductTableSeeder.php
+++ Modules/Product/Database/Seeders/ProductTableSeeder.php
@@ -3,9 +3,6 @@
 namespace Modules\Product\Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Modules\Attribute\Entities\Attribute;
-use Modules\Core\Entities\Store;
 use Modules\Product\Entities\Product;
 use Modules\Product\Entities\ProductAttribute;
 
@@ -13,25 +10,11 @@ class ProductTableSeeder extends Seeder
 {
     public function run(): void
     {
-        $product = Product::factory()
+        Product::factory()
+            ->has(ProductAttribute::factory(), 'product_attributes')
             ->create([
                 "parent_id" => Product::factory()->configurable()->create()->id,
                 "attribute_group_id" => 1
             ]);
-
-            $attribute_types = config("attribute_types");
-            unset($attribute_types["image"], $attribute_types["file"]);
-            $attribute_type = array_rand($attribute_types);
-            $attribute_model = $attribute_types[$attribute_type];
-            
-            ProductAttribute::create([
-                "product_id" => $product->id,
-                "attribute_id" => Attribute::factory()->create(["type" => $attribute_type])->id,
-                "value_type" => $attribute_model,
-                "value_id" => $attribute_model::factory()->create()->id,
-                "store_id" => null,
-                "channel_id" => null
-            ]);
-
     }
 }
