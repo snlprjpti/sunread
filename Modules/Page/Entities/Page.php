9fcc1394f1a8d2a6dd964226fcdd53f031c42bf3
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-06-09 17:42:47 +0545

    page translation implemented

--- Modules/Page/Entities/Page.php
+++ Modules/Page/Entities/Page.php
@@ -6,14 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Modules\Core\Traits\HasFactory;
+use Modules\Core\Traits\HasTranslation;
 use Modules\Core\Traits\Sluggable;
 
 class Page extends Model
 {
-    use HasFactory, Sluggable;
+    use HasFactory, Sluggable, HasTranslation;
 
     protected $fillable = [ "parent_id", "slug", "title", "description", "position", "status", "meta_title", "meta_description", "meta_keywords" ];
     protected $with = [ "translations" ];
+
+    public $translatedAttributes = ["title", "description", "meta_title", "meta_description", "meta_keywords"];
+    public $translatedModels = [ PageTranslation::class, "page_id" ];
 
     public function translations(): HasMany
     {
