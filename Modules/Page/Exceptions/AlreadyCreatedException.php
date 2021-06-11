3f62b9ec5a52b1057b80f77d3957424772a66bbd
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-06-11 12:38:01 +0545

    replace tab to space

--- Modules/Page/Exceptions/AlreadyCreatedException.php
+++ Modules/Page/Exceptions/AlreadyCreatedException.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace  Modules\Page\Exceptions;
+namespace Modules\Page\Exceptions;
 
 class AlreadyCreatedException extends \Exception
 {
-	public function __construct()
-	{
-		parent::__construct("Requested data already created.");
-	}
+    public function __construct()
+    {
+        parent::__construct("Requested data already created.");
+    }
 }
