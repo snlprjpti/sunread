33e254f45b28179bc5b998685ba8c04ff7fedbe5
Anisha Khadka <aneesha,khadka@hazesoft.co>
2021-06-16 14:39:29 +0545

    database seeder sequence is updated

--- Modules/Core/Database/Seeders/CoreDatabaseSeeder.php
+++ Modules/Core/Database/Seeders/CoreDatabaseSeeder.php
@@ -17,9 +17,9 @@ class CoreDatabaseSeeder extends Seeder
         Model::unguard();
         $this->call(CurrencyTableSeeder::class);
         $this->call(ExchangeRateTableSeeder::class);
-        $this->call(StoreTableSeeder::class);
         $this->call(WebsiteTableSeeder::class);
         $this->call(ChannelTableSeeder::class);
+        $this->call(StoreTableSeeder::class);
         $this->call(ConfigurationTableSeeder::class);
     }
 }
