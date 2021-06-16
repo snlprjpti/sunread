d4fa364af5f1befe2d2620465430a1d3a1044f43
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-06-16 14:31:21 +0545

    return relation resource

--- Modules/Coupon/Transformers/AllowCouponResource.php
+++ Modules/Coupon/Transformers/AllowCouponResource.php
@@ -12,7 +12,7 @@ class AllowCouponResource extends JsonResource
             "id" => $this->id,
             "coupon" => new CouponResource($this->whenLoaded("coupon")),
             "model_type" => $this->model_type,
-            "model_id" => $this->model_id,
+            "model_id" => $this->whenLoaded("model"),
             "status" => $this->status,
             "created_at" => $this->created_at->format("M d, Y H:i A")
         ];
