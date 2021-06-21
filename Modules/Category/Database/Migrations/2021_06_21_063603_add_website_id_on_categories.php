835a441fec1bd9de6a5c12046647ddac6e711b89
Anisha Khadka <aneesha,khadka@hazesoft.co>
2021-06-21 15:34:38 +0545

    website_id and include_in_menu is added

---
+++ Modules/Category/Database/Migrations/2021_06_21_063603_add_website_id_on_categories.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddWebsiteIdOnCategories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->unsignedBigInteger('website_id');
+            $table->unique(['website_id', 'slug', 'parent_id']);
+            $table->foreign('website_id')->references('id')->on('websites')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropColumn('website_id');
+            $table->dropForeign('website_id');
+        });
+    }
+}
