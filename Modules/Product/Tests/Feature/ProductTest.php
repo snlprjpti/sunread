912675d972f19ea3ddad75e6cf9b6f914a06aee7
Rupesh Shrestha <rupesh.shrestha@hazesoft.co>
2021-06-22 07:12:19 +0000

    updated product test

--- Modules/Product/Tests/Feature/ProductTest.php
+++ Modules/Product/Tests/Feature/ProductTest.php
@@ -8,11 +8,16 @@ use Modules\Core\Tests\BaseTestCase;
 use Modules\Product\Entities\Product;
 use Illuminate\Support\Facades\Storage;
 use Modules\Attribute\Entities\Attribute;
+use Modules\Attribute\Entities\AttributeSet;
+use Modules\Attribute\Entities\AttributeGroup;
 use Modules\Category\Entities\Category;
 use Modules\Product\Entities\ProductImage;
+use Modules\Core\Entities\Website;
 
 class ProductTest extends BaseTestCase
 {
+    protected $attribute_group, $attribute_set_id;
+
     public function setUp(): void
     {
         $this->model = Product::class;
@@ -23,22 +28,113 @@ class ProductTest extends BaseTestCase
         $this->model_name = "Product";
         $this->route_prefix = "admin.catalog.products";
         $this->hasStatusTest = true;
+
+        $this->attribute_set_id = AttributeSet::factory()->create()->id;
+        $this->attribute_group = AttributeGroup::factory(1)
+        ->create(["attribute_set_id" => $this->attribute_set_id])
+        ->each(function ($attr_group){
+            $attr_group->attributes()->attach(Attribute::factory(1)->create());
+        })->first();
+
+        // $this->hasUpdateTest = false;
+
     }
 
     public function getCreateData(): array
     {
-        $attribute = Attribute::whereType("text")->inRandomOrder()->first();
         $category = Category::inRandomOrder()->first();
-
+        $attribute = $this->attribute_group->attributes[0];
+        
         return $this->model::factory()->make([
+            "website_id" => Website::factory()->create()->id,
+            "attribute_set_id" => $this->attribute_group->attribute_set_id,
             "attributes" => [
                 [
                     "attribute_id" => $attribute->id,
-                    "value" => Str::random(10)
+                    "value" => $this->value($attribute)
                 ]
             ],
             "categories" => [$category->id]
         ])->toArray();
+    }
+
+    public function testAdminCanUpdateResourceWithNonMandatoryData()
+    {
+        // $product = $this->model::find($this->default_resource_id);
+        
+        // $attribute = $product->attribute_set->attribute_groups[0]->attributes[0];
+
+        // $category = Category::inRandomOrder()->first();
+        // $post_data = $this->model::factory()->make([
+        //     "website_id" => Website::factory()->create()->id,
+        //     "attribute_set_id" => $product->attribute_set_id,
+        //     "attributes" => [
+        //         [
+        //             "attribute_id" => $attribute->id,
+        //             "value" => $this->value($attribute)
+        //         ]
+        //     ],
+        //     "categories" => [$category->id]
+        // ])->toArray();
+
+        $post_data = $this->getUpdateData();
+
+        $response = $this->withHeaders($this->headers)->put($this->getRoute("update", [$this->default_resource_id]), $post_data);
+
+        $response->assertOk();
+        $response->assertJsonFragment([
+            "status" => "success",
+            "message" => __("core::app.response.update-success", ["name" => $this->model_name])
+        ]);
+    }
+
+    public function testAdminCanUpdateResource()
+    {
+        $product = $this->model::find($this->default_resource_id);
+        $attribute = $product->attribute_set->attribute_groups[0]->attributes[0];
+
+        $category = Category::inRandomOrder()->first();
+        $post_data = $this->model::factory()->make([
+            "website_id" => Website::factory()->create()->id,
+            "attribute_set_id" => $product->attribute_set_id,
+            "attributes" => [
+                [
+                    "attribute_id" => $attribute->id,
+                    "value" => $this->value($attribute)
+                ]
+            ],
+            "categories" => [$category->id]
+        ])->toArray();
+
+        $response = $this->withHeaders($this->headers)->put($this->getRoute("update", [$this->default_resource_id]), $post_data);
+
+        $response->assertOk();
+        $response->assertJsonFragment([
+            "status" => "success",
+            "message" => __("core::app.response.update-success", ["name" => $this->model_name])
+        ]);
+    }
+
+    public function value(object $attribute): mixed
+    {
+        switch($attribute->type)
+        {
+            case "price" : 
+                $value = 1000.1;
+                break;
+
+            case "boolean" : 
+                $value = 1;
+                break;
+
+            case "timestamp" : 
+                $value = now();
+                break;
+
+            default:
+                $value = Str::random(10);
+        }
+        return $value;
     }
 
     public function getNonMandodtaryCreateData(): array
