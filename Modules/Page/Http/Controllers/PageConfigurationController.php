f9f5d292989f05592f7755bf64b236cc90569f61
Bikram Lama <thebikramlama1123@gmail.com>
2021-06-24 14:34:09 +0545

    Revert "create or udpate configure"
    This reverts commit 2a897a6655251e38522cccf6142377fe4b793a45.

--- Modules/Page/Http/Controllers/PageConfigurationController.php
+++ Modules/Page/Http/Controllers/PageConfigurationController.php
@@ -2,45 +2,17 @@
 
 namespace Modules\Page\Http\Controllers;
 
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Modules\Core\Http\Controllers\BaseController;
 use Modules\Page\Entities\PageConfiguration;
-use Exception;
-use Modules\Page\Repositories\PageConfigurationRepository;
 
 class PageConfigurationController extends BaseController
 {
-    private $repository;
 
-    public function __construct(PageConfiguration $pageConfiguration, PageConfigurationRepository $pageConfigurationRepository)
+    public function __construct(PageConfiguration $pageConfiguration)
     {
         $this->model = $pageConfiguration;
         $this->model_name = "Page Configuration";
-        $this->repository = $pageConfigurationRepository;
 
         parent::__construct($this->model, $this->model_name);
-    }
-
-    public function createOrUpdate(Request $request): JsonResponse
-    {
-        try
-        {
-            $scope_rules = $this->repository->scopeValidation($request);
-            $data = $this->repository->validateData($request, $scope_rules, function ($current_data) {
-                return $current_data->all();
-            });
-
-            if(!$request->scope) $data["scope"] = "website";
-            if(!$request->scope_id) $data["scope_id"] = 0;
-
-            $created_data = $this->repository->add((object) $data);
-        }
-        catch( Exception $exception )
-        {
-            return $this->handleException($exception);
-        }
-
-        return $this->successResponse($created_data->data ?? [], $this->lang($created_data->message), $created_data->code);
     }
 }
