4d7a2fc7ec0462799c83f30880a0d0bb2fd860cb
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-06-28 17:44:52 +0545

    remove status code in page image destroy

--- Modules/Page/Http/Controllers/PageImageController.php
+++ Modules/Page/Http/Controllers/PageImageController.php
@@ -69,6 +69,6 @@ class PageImageController extends BaseController
             return $this->handleException($exception);
         }
 
-        return $this->successResponseWithMessage($this->lang('delete-success'), 204);
+        return $this->successResponseWithMessage($this->lang('delete-success'));
     }
 }
