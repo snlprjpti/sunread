43364bfbc55a061f22b785499fa2cd247a250b88
Anisha Khadka <aneesha,khadka@hazesoft.co>
2021-06-30 00:48:57 +0545

    store image method is added

--- Modules/Category/Repositories/CategoryValueRepository.php
+++ Modules/Category/Repositories/CategoryValueRepository.php
@@ -7,18 +7,21 @@ use Exception;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\ValidationException;
 use Modules\Category\Entities\CategoryValue;
 
 class CategoryValueRepository
 {
-    protected $model, $model_key, $repository;
+    protected $model, $model_key, $repository, $model_name;
 
     public function __construct(CategoryValue $category_value, CategoryRepository $category_repository)
     {
         $this->model = $category_value;
         $this->model_key = "catalog.category.values";
         $this->repository = $category_repository;
+        $this->model_name = "Category";
     }
 
     public function createOrUpdate(array $data, Model $parent): void
@@ -35,18 +38,24 @@ class CategoryValueRepository
                 "scope" => $data["scope"],
                 "scope_id" => $data["scope_id"]
             ];
-            unset($data["position"], $data["website_id"], $data["parent_id"], $data["slug"]);
-            $config_arrays = config("category.attributes");
+
+            $config_arrays = collect(config('category.attributes'))->pluck('elements')->flatten(1)->toArray();
 
             foreach($config_arrays as $config_array){
                 $key = $config_array["title"];
 
-                if($this->repository->scopeFilter($data['scope'], $config_array["scope"])) $input[$key] = $this->repository->getDefaultValues($match)->toArray()[$key];
-                else{
-                    $val = isset($data["attributes"][0][$key]) ? $data["attributes"][0][$key] : null;
-                    if($val && isset($val["use_default_value"]) && $val["use_default_value"] != 1) throw ValidationException::withMessages([ "use_default_value" => __("core::app.response.use_default_value") ]);
-                    if($val) $input[$key] = (isset($val["use_default_value"]) && $val["use_default_value"] == 1) ? $this->repository->getDefaultValues($match)->toArray()[$key] : $val["value"];
+                if($this->repository->scopeFilter($data['scope'], $config_array["scope"])) {
+                    $input[$key] = $this->repository->getDefaultValues($match)->$key;
+                    continue;
                 }
+                $val = isset($data["attributes"][0][$key]) ? $data["attributes"][0][$key] : null;
+                if($key == "image" && isset($val["value"]))
+                {
+                    $input[$key] = $this->storeImage($val["value"], strtolower($this->model_name));
+                    continue;
+                }
+                if($val && isset($val["use_default_value"]) && $val["use_default_value"] != 1) throw ValidationException::withMessages([ "use_default_value" => __("core::app.response.use_default_value") ]);
+                if($val) $input[$key] = (isset($val["use_default_value"]) && $val["use_default_value"] == 1) ? $this->repository->getDefaultValues($match)->toArray()[$key] : $val["value"];            
             }
             $created = $this->model->updateOrCreate($match, $input);
         }
@@ -58,5 +67,26 @@ class CategoryValueRepository
 
         Event::dispatch("{$this->model_key}.create.after", $created);
         DB::commit();
+    }
+
+    public function storeImage(object $request, ?string $folder = null, ?string $delete_url = null): string
+    {
+        try
+        {
+            // Store File
+            $file = $request;
+            $key = Str::random(6);
+            $folder = $folder ?? "default";
+            $file_path = $file->storeAs("images/{$folder}/{$key}", (string) $file->getClientOriginalName());
+
+            // Delete old file if requested
+            if ( $delete_url !== null ) Storage::delete($delete_url);
+        }
+        catch (\Exception $exception)
+        {
+            throw $exception;
+        }
+
+        return $file_path;
     }
 }
