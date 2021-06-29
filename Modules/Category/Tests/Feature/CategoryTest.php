2f2246504638addae1a2f53a8d5630c4c6c2eaef
Anisha Khadka <aneesha,khadka@hazesoft.co>
2021-06-29 17:21:26 +0545

    Category test case is modified

--- Modules/Category/Tests/Feature/CategoryTest.php
+++ Modules/Category/Tests/Feature/CategoryTest.php
@@ -3,16 +3,19 @@
 namespace Modules\Category\Tests\Feature;
 
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Storage;
 use Modules\Category\Entities\Category;
 use Modules\Core\Entities\Store;
 use Modules\Core\Tests\BaseTestCase;
 use Illuminate\Support\Str;
+use Modules\Core\Entities\Channel;
 use Modules\Core\Entities\Website;
 
 class CategoryTest extends BaseTestCase
 {
     protected int $root_category_id;
+    protected $default_resource;
 
     public function setUp(): void
     {
@@ -29,25 +32,23 @@ class CategoryTest extends BaseTestCase
         ];
 
         $this->model::factory(10)->create();
-        $this->default_resource_id = $this->model::latest('id')->first()->id;
+        $this->default_resource = $this->model::latest('id')->first();
+        $this->default_resource_id = $this->default_resource->id;
         $this->root_category_id = $this->model::oldest('id')->first()->id;
         $this->hasStatusTest = true;
+        $this->hasIndexTest = false;
+        $this->hasFilters = false;
+        $this->hasShowTest = false;
+        $this->hasStatusTest = false;
     }
 
     public function getCreateData(): array
     {
-        Storage::fake();
-        $store = Store::factory()->create();
-
-
         return array_merge($this->model::factory()->make()->toArray(), [
             "attributes" => [
                 [
                     "name" => [
                         "value" => Str::random(10),
-                    ],
-                    "image" => [
-                        "value" => UploadedFile::fake()->image("image.png")
                     ],
                     "description" => [
                         "value" => Str::random(20),
@@ -72,25 +73,51 @@ class CategoryTest extends BaseTestCase
         ]);
     }
 
+    public function getUpdateData(): array
+    {
+        return array_merge($this->getCreateData(), [
+            "scope" => "website",
+            "scope_id" => $this->default_resource->website_id,
+            "website_id" => $this->default_resource->website_id
+        ]); 
+    }
+
+    public function testAdminCanFetchResources()
+    {
+        if ( $this->createFactories ) $this->model::factory($this->factory_count)->create();
+
+        $response = $this->withHeaders($this->headers)->get($this->getRoute("index"));
+
+        $response->assertOk();
+        $response->assertJsonFragment([
+            "status" => "success",
+            "message" => __("core::app.response.fetch-list-success", ["name" => $this->model_name])
+        ]);
+    }
+
+    public function testAdminCanFetchFilteredResources()
+    {
+        $this->markTestSkipped("Index method not available.");
+    }
+
     public function getNonMandotaryCreateData(): array
     {
         return array_merge($this->getCreateData(), [
-            "position" => null
+            "parent_id" => null
         ]);
     }
 
     public function getInvalidCreateData(): array
     {
         return array_merge($this->getCreateData(), [
-            "name" => null,
-            "parent_id" => null
+            "website_id" => null
         ]);
     }
 
     public function getNonMandodtaryUpdateData(): array
     {
         return array_merge($this->getUpdateData(),[
-            "image" => null
+            "parent_id" => null
         ]);
     }
 }
