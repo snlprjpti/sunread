065830bb0d9721cd83b0c202b98e519c591427cf
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-06-30 17:40:39 +0545

    add relation to region

--- Modules/Country/Entities/Country.php
+++ Modules/Country/Entities/Country.php
@@ -3,8 +3,15 @@
 namespace Modules\Country\Entities;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Country extends Model
 {
     protected $fillable = [ "iso_2_code", "iso_3_code", "numeric_code", "dialing_code", "name" ];
+
+    public function regions(): HasMany
+    {
+        return $this->hasMany(Region::class);
+    }
+
 }
