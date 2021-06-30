bd7e439aa6ea476a1e43114722dc0a4917982f0f
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-06-30 15:16:57 +0545

    registered country seeder

--- Modules/Country/Database/Seeders/CountryDatabaseSeeder.php
+++ Modules/Country/Database/Seeders/CountryDatabaseSeeder.php
@@ -7,15 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class CountryDatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
+    public function run(): void
     {
         Model::unguard();
-
-        // $this->call("OthersTableSeeder");
+         $this->call(CountryTableSeeder::class);
     }
 }
