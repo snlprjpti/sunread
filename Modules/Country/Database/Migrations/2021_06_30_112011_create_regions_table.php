1c8025cc85d5c3d27d32291c7c4c3a1fa72d899f
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-06-30 17:39:42 +0545

    region migration added

--- Modules/Country/Database/Migrations/2021_06_30_112011_create_regions_table.php
+++ Modules/Country/Database/Migrations/2021_06_30_112011_create_regions_table.php
@@ -18,7 +18,7 @@ class CreateRegionsTable extends Migration
             $table->unsignedBigInteger("country_id");
             $table->foreign("country_id")->references("id")->on("countries")->onDelete("cascade");
             $table->string('code')->unique();
-            $table->string('name')->unique();
+            $table->string('name');
             $table->timestamps();
         });
     }
