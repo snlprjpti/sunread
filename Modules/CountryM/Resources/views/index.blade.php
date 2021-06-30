26f8efcc3f4bd5f4b4046033d21aa99be1705ff3
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-06-30 15:01:44 +0545

    country model created

--- Modules/CountryM/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('countrym::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('countrym.name') !!}
-    </p>
-@endsection
