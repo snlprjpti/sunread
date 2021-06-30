fd63428846ad7dfa798c4024a0c9add15cd75d11
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-06-30 15:36:49 +0545

    list and show method added

---
+++ Modules/Country/Transformers/CountryResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Modules\Country\Transformers;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CountryResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            "id" => $this->id,
+            "alpha_2_code" => $this->alpha_2_code,
+            "alpha_3_code" => $this->alpha_3_code,
+            "numeric_code" => $this->numeric_code,
+            "iso_2_code" => $this->iso_2_code,
+            "iso_3_code" => $this->iso_3_code,
+            "dialing_code" => $this->dialing_code,
+            "name" => $this->name,
+            "created_at" => $this->created_at->format('M d, Y H:i A'),
+        ];
+    }
+}
