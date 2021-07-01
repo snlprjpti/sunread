b9cbea760288d550f5ed84cf7e6e854944de68c0
Bikram Lama <thebikramlama1123@gmail.com>
2021-07-01 14:25:14 +0545

    Added tax-rate database

---
+++ Modules/Tax/Database/factories/TaxRateFactory.php
@@ -0,0 +1,30 @@
+<?php
+namespace Modules\Tax\Database\factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TaxRateFactory extends Factory
+{
+    protected $model = \Modules\Tax\Entities\TaxRate::class;
+
+    public function definition(): array
+    {
+        // $country = Country::inRandomOrder()->first();
+        // $country_id = $country->id;
+        // $region_id = $country->regions()->inRandomOrder()->first()->id;
+        $country_id = null;
+        $region_id = null;
+
+        return [
+            "country_id" => $country_id,
+            "region_id" => $region_id,
+            "identifier" => $this->faker->unique()->slug(),
+            "use_zip_range" => 0,
+            "zip_code" => "*",
+            "postal_code_from" => null,
+            "postal_code_to" => null,
+            "tax_rate" => $this->faker->randomFloat(min: 5, max: 20)
+        ];
+    }
+}
+
