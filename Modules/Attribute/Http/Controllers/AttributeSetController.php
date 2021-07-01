64ab81daa0b93b3ee22d467bd25d1be63933bc3b
Rupesh Shrestha <rupesh.shrestha@hazesoft.co>
2021-07-01 10:10:15 +0000

    removed huge logic to repo

--- Modules/Attribute/Http/Controllers/AttributeSetController.php
+++ Modules/Attribute/Http/Controllers/AttributeSetController.php
@@ -9,7 +9,6 @@ use Illuminate\Http\Resources\Json\JsonResource;
 use Modules\Core\Http\Controllers\BaseController;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 use Modules\Attribute\Entities\Attribute;
-use Modules\Product\Entities\Product;
 use Modules\Attribute\Entities\AttributeSet;
 use Modules\Attribute\Exceptions\AttributeGroupsPresent;
 use Modules\Attribute\Exceptions\DefaultFamilyCanNotBeDeleted;
@@ -203,71 +202,13 @@ class AttributeSetController extends BaseController
         try
         {
             $this->repository->validateAttributeSetListing($request); 
-
-            $product = Product::find($request->product);
-            $data = $this->model->findOrFail($product ? $product->attribute_set_id : $request->attribute_set);
-            $groups = [];
-
-            $attribute_groups = $data->attribute_groups->sortBy("position")->map(function ($attribute_group) use (&$groups, $product) {                
-                $groups[] = [
-                    "group_id" => $attribute_group->id,
-                    "title" => $attribute_group->name,
-                    "position" => $attribute_group->position,
-                    "elements" => $attribute_group->attributes->sortBy("position")->map(function ($attribute) use ($product){
-                        return [
-                            "attribute_id" => $attribute->id,
-                            "name" => $attribute->name,
-                            "scope" => $attribute->scope,
-                            "position" => $attribute->position,
-                            "is_required" => $attribute->is_required,
-                            "type" => $attribute->type,
-                            "value" => $product ? $attribute->product_attributes->where("product_id", $product->id)->first()->value_data ?? '' : '' 
-                        ];
-                    })->toArray()
-                ];
-            });
-
-            $default = [
-                "general" => [
-                    [
-                        "title" => "General Details",
-                        "position" => 1,
-                        "elements" => [
-                            [
-                                "title" => "SKU",
-                                "name" => "sku",
-                                "is_required" => 1,
-                                "position" => 1,
-                                "type" => "text",
-                                "value" => $product->sku ?? '' 
-                            ],
-                            [
-                                "title" => "Type",
-                                "name" => "type",
-                                "is_required" => 1,
-                                "position" => 2,
-                                "type" => "hidden",
-                                "value" => $product->type ?? ''
-                            ],
-                            [
-                                "title" => "Attribute Set",
-                                "name" => "attribute_set_id",
-                                "is_required" => 1,
-                                "position" => 3,
-                                "type" => "select",
-                                "value" => $product ? $product->attribute_set_id : $request->attribute_set_id
-                            ]
-                        ]
-                    ]
-                ],
-                "attribute_groups" => $groups,
-            ];
+            $fetched = $this->repository->generateFormat($request);
         }
         catch( Exception $exception )
         {
             return $this->handleException($exception);
         }
 
-        return $this->successResponse($default, $this->lang("fetch-success"));
+        return $this->successResponse($fetched, $this->lang("fetch-success"));
     }
 }
