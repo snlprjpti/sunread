5a9ea1d57499e6068bf98d7258b42dcad2e2ee1b
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-07-02 15:53:34 +0545

    route status update

--- Modules/Tax/Routes/api.php
+++ Modules/Tax/Routes/api.php
@@ -7,7 +7,7 @@ Route::group(["middleware" => ["api"]], function () {
         Route::resource("customer-tax-groups", CustomerTaxGroupController::class)->except(["create", "edit"]);
         Route::resource("product-tax-groups", ProductTaxGroupController::class)->except(["create", "edit"]);
 
-        Route::put("/{tax_rule_id}/status", [\Modules\Tax\Http\Controllers\TaxRuleController::class, "updateStatus"])->name("status");
+        Route::put("/tax-rules/{tax_rule_id}/status", [\Modules\Tax\Http\Controllers\TaxRuleController::class, "updateStatus"])->name("status");
         Route::resource('tax-rules', TaxRuleController::class)->except(['create', 'edit']);
     });
 });
