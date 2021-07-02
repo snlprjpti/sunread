77b65f4fc30b30524b0fb6777adf53a43a422795
Bikram Lama <thebikramlama1123@gmail.com>
2021-07-02 16:03:25 +0545

    Optimized testing, reduced data on seeds

---
+++ Modules/Country/Database/Seeders/data/testing/regions.php
@@ -0,0 +1,70 @@
+<?php
+
+return [
+	[
+		'name' => 'Sidi Bel Abbès',
+		'country_id' => 4,
+		'country_code' => 'DZ',
+		'code' => '22'
+	],
+	[
+		'name' => 'El M\'ghair',
+		'country_id' => 4,
+		'country_code' => 'DZ',
+		'code' => '49'
+	],
+	[
+		'name' => 'El Menia',
+		'country_id' => 4,
+		'country_code' => 'DZ',
+		'code' => '50'
+	],
+	[
+		'name' => 'Ouled Djellal',
+		'country_id' => 4,
+		'country_code' => 'DZ',
+		'code' => '51'
+	],
+	[
+		'name' => 'Bordj Baji Mokhtar',
+		'country_id' => 4,
+		'country_code' => 'DZ',
+		'code' => '52'
+	],
+	[
+		'name' => 'Béni Abbès',
+		'country_id' => 4,
+		'country_code' => 'DZ',
+		'code' => '53'
+	],
+	[
+		'name' => 'Timimoun',
+		'country_id' => 4,
+		'country_code' => 'DZ',
+		'code' => '54'
+	],
+	[
+		'name' => 'Touggourt',
+		'country_id' => 4,
+		'country_code' => 'DZ',
+		'code' => '55'
+	],
+	[
+		'name' => 'Djanet',
+		'country_id' => 4,
+		'country_code' => 'DZ',
+		'code' => '56'
+	],
+	[
+		'name' => 'In Salah',
+		'country_id' => 4,
+		'country_code' => 'DZ',
+		'code' => '57'
+	],
+	[
+		'name' => 'In Guezzam',
+		'country_id' => 4,
+		'country_code' => 'DZ',
+		'code' => '58'
+	],
+];
