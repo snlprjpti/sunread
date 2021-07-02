da67d591d797ec4f148373e9e5f0bc680bb2f4a7
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-07-02 22:40:18 +0545

    tax rule tax rate migration renamed

--- Modules/Tax/Database/Migrations/2021_07_02_104353_create_tax_rate_tax_rule_table.php
+++ Modules/Tax/Database/Migrations/2021_07_02_104353_create_tax_rate_tax_rule_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTaxRateTaxRulesTable extends Migration
+class CreateTaxRateTaxRuleTable extends Migration
 {
     /**
      * Run the migrations.
