75a760e7e08598ab059025b7a1c4be076367dacb
Rupesh Shrestha <rupesh.shrestha@hazesoft.co>
2021-07-06 20:00:22 +0545

    updated spaces

--- Modules/Inventory/Exceptions/InventoryCannotBeLessThanZero.php
+++ Modules/Inventory/Exceptions/InventoryCannotBeLessThanZero.php
@@ -4,8 +4,8 @@ namespace Modules\Inventory\Exceptions;
 
 class InventoryCannotBeLessThanZero extends \Exception
 {
-	public function __construct()
-	{
-		parent::__construct(__("core::app.response.inventory_cannot_be_zero"));
-	}
+    public function __construct()
+    {
+        parent::__construct(__("core::app.response.inventory_cannot_be_zero"));
+    }
 }
