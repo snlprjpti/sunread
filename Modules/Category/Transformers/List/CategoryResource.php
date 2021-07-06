ea62d162ebdd89319532f4c0ad948f8b63d1e04f
Anisha Khadka <aneesha,khadka@hazesoft.co>
2021-07-06 10:29:43 +0545

    list category resource is modified

--- Modules/Category/Transformers/List/CategoryResource.php
+++ Modules/Category/Transformers/List/CategoryResource.php
@@ -15,12 +15,15 @@ class CategoryResource extends JsonResource
             "scope_id" => $request->scope_id ?? $request->website_id,
             "category_id" => $this->id
         ];
-        $scopeValue = $this->checkCondition($data) ? $this->getValues($data) : $this->getDefaultValues($data);
+        $name_data = array_merge($data, ["attribute" => "name"]);
+        $nameValue = $this->has($name_data) ? $this->getValues($name_data) : $this->getDefaultValues($name_data);
+        $slug_data = array_merge($data, ["attribute" => "slug"]);
+        $slugValue = $this->has($slug_data) ? $this->getValues($slug_data) : $this->getDefaultValues($slug_data);
 
         return [
             "id" => $this->id,
-            "slug" => $this->slug,
-            "name" => $scopeValue->name,
+            "slug" => $slugValue ? $slugValue->value : null,
+            "name" => $nameValue ? $nameValue->value : null,
             "children" => CategoryResource::collection($this->children)
         ];
     }
