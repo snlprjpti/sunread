817ebef0ffe9f18b8b4e93e3dc1999d534bbc557
Rupesh Shrestha <rupesh.shrestha@hazesoft.co>
2021-07-06 10:10:39 +0545

    updated without events

--- Modules/Inventory/Database/Seeders/CatalogInventoryTableSeeder.php
+++ Modules/Inventory/Database/Seeders/CatalogInventoryTableSeeder.php
@@ -12,10 +12,13 @@ class CatalogInventoryTableSeeder extends Seeder
 {
     public function run(): void
     {
-        CatalogInventory::factory()
-        ->create()
-        ->each(function ($catalog_inventory){
-            $catalog_inventory->catalog_inventory_items()->attach(CatalogInventoryItem::factory(2)->create());
+        CatalogInventory::withoutEvents(function () {
+            CatalogInventory::factory()
+            ->create()
+            ->each(function ($catalog_inventory){
+                $catalog_inventory->catalog_inventory_items()->attach(CatalogInventoryItem::factory(2)->create());
+            });
         });
+
     }
 }
