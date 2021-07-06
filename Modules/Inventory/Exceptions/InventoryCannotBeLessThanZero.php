071ee8baa9b633b75451212c1c6c4e694dc10df1
Rupesh Shrestha <rupesh.shrestha@hazesoft.co>
2021-07-06 17:53:38 +0545

    added exception

---
+++ Modules/Inventory/Exceptions/InventoryCannotBeLessThanZero.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Modules\Inventory\Exceptions;
+
+class InventoryCannotBeLessThanZero extends \Exception
+{
+	public function __construct()
+	{
+		parent::__construct(__("core::app.response.inventory_cannot_be_zero"));
+	}
+}
