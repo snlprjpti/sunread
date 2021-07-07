076408c7ddc44ec1ac2dbb26988d8a30434d87ce
Anisha Khadka <aneesha,khadka@hazesoft.co>
2021-07-07 19:00:32 +0545

    indenattion fix and bool return issue solved

--- Modules/Attribute/Traits/HasMapper.php
+++ Modules/Attribute/Traits/HasMapper.php
@@ -15,27 +15,27 @@ trait HasMapper
 
     public function getConfigOption(): ?object
     {
-            $fetched = null;
+        $fetched = null;
             
-            if($this->checkOption())
-            {
-                $configSlug = $this->mapper[$this->slug];
-                $model = new $configSlug["module"];
-                $fetched =  $model->select($configSlug["pluck"][0], $configSlug["pluck"][1])->get();
-            }
+        if($this->checkOption())
+        {
+            $configSlug = $this->mapper[$this->slug];
+            $model = new $configSlug["module"];
+            $fetched =  $model->select($configSlug["pluck"][0], $configSlug["pluck"][1])->get();
+        }
         return $fetched;
     }
 
     public function checkMapper(): bool
     {
         $this->getMapperData();
-        return in_array($this->slug, array_keys($this->mapper)) ? true : false; 
+        return in_array($this->slug, array_keys($this->mapper)); 
     }
 
     public function checkOption(): bool
     {
         if($this->checkMapper()){
-            return ($this->mapper[$this->slug]["options"] == 1) ? true : false;
+            return ($this->mapper[$this->slug]["options"] == 1);
         }
         return false;
     }
