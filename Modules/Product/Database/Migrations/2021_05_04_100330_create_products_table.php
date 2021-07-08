dd82fdf3f365500f4f85abfd4a23d6d3de0cc81d
Rupesh Shrestha <rupesh.shrestha@hazesoft.co>
2021-07-08 23:11:09 +0545

    updated sku to null

--- Modules/Product/Database/Migrations/2021_05_04_100330_create_products_table.php
+++ Modules/Product/Database/Migrations/2021_05_04_100330_create_products_table.php
@@ -19,7 +19,7 @@ class CreateProductsTable extends Migration
             $table->unsignedBigInteger("attribute_set_id")->nullable();
             $table->foreign("attribute_set_id")->references("id")->on("attribute_sets")->onDelete("cascade");
 
-            $table->string("sku")->unique();
+            $table->string("sku")->unique()->nullable();
             $table->string("type")->default("simple");
             $table->boolean("status")->default(1);
 
