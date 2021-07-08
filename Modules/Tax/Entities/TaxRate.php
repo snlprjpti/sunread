60a4514d0982d20ad5a0651b80235c4c9afce1ca
Bikram Lama <thebikramlama1123@gmail.com>
2021-07-08 11:44:48 +0545

    Added realtionships

--- Modules/Tax/Entities/TaxRate.php
+++ Modules/Tax/Entities/TaxRate.php
@@ -2,8 +2,11 @@
 
 namespace Modules\Tax\Entities;
 
-use Illuminate\Database\Eloquent\Model;
 use Modules\Core\Traits\HasFactory;
+use Modules\Country\Entities\Region;
+use Modules\Country\Entities\Country;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class TaxRate extends Model
 {
@@ -11,4 +14,14 @@ class TaxRate extends Model
 
     public static $SEARCHABLE = [ "identifier" ];
     protected $fillable = [ "country_id", "region_id", "identifier", "use_zip_range", "zip_code", "postal_code_from", "postal_code_to", "tax_rate" ];
+
+    public function country(): BelongsTo
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function region(): BelongsTo
+    {
+        return $this->belongsTo(Region::class);
+    }
 }
