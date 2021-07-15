5435838346ba1aac7282b7398f208460df2becae
Anisha Khadka <aneesha,khadka@hazesoft.co>
2021-07-15 13:20:50 +0545

    data fetched from factory

--- Modules/Customer/Database/Seeders/CustomerTableSeeder.php
+++ Modules/Customer/Database/Seeders/CustomerTableSeeder.php
@@ -4,25 +4,12 @@ namespace Modules\Customer\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Modules\Customer\Entities\Customer;
 
 class CustomerTableSeeder extends Seeder
 {
     public function run(): void
     {
-        DB::table("customers")->insert([
-            "first_name" => "John",
-            "middle_name" => "Nic",
-            "last_name" => "Doe",
-            "email" => "customer@example.net",
-            "customer_group_id" => 1,
-            "website_id" => 1,
-            "store_id" => 1,
-            "gender" => "male",
-            "date_of_birth" => "1950-01-01",
-            "password" => bcrypt("password"),
-            "profile_image" => null,
-            "created_at" => now(),
-            "updated_at" => now()
-        ]);
+        Customer::factory()->create();
     }
 }
