32a6bd92fd123620c09bb95727193970647d5178
Rupesh Shrestha <rupesh.shrestha@hazesoft.co>
2021-07-15 17:12:45 +0545

    updated values

--- Modules/Product/Transformers/List/ProductResource.php
+++ Modules/Product/Transformers/List/ProductResource.php
@@ -3,13 +3,9 @@
 namespace Modules\Product\Transformers\List;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 use Modules\Attribute\Entities\Attribute;
-use Modules\Attribute\Transformers\AttributeGroupResource;
-use Modules\Brand\Transformers\BrandResource;
 use Modules\Category\Transformers\CategoryResource;
-use Modules\Core\Transformers\WebsiteResource;
-use Modules\Inventory\Transformers\CatalogInventoryResource;
-use Modules\Product\Transformers\ProductImageResource;
 
 class ProductResource extends JsonResource
 {
@@ -19,19 +15,19 @@ class ProductResource extends JsonResource
         $attributeIdforVisibility = Attribute::whereSlug("visibility")->first()->id;
         $product_attributes = $this->product_attributes()->whereScope($request->scope ?? "website")->whereScopeId($request->scope_id ?? $this->website_id);
         $name = $product_attributes->whereAttributeId($attributeIdforName)->first();
-        $visibility = $product_attributes->whereAttributeId($attributeIdforVisibility)->first();
+        $visibility = $this->product_attributes()->whereScope($request->scope ?? "website")->whereScopeId($request->scope_id ?? $this->website_id)->whereAttributeId($attributeIdforVisibility)->first();
         $images = $this->images()->where('main_image', 1)->first();
         $stock = $this->catalog_inventories()->first();
 
         return [
             "id" => $this->id,
-            "name" => $name ? $name->value->value : null,
+            "name" => $name->value_data,
             "sku" => $this->sku,           
             "stock" => $stock ? $stock->quantity : null,
             "status" => (bool) $this->status,
             "categories" => CategoryResource::collection($this->whenLoaded("categories")),
-            "visibility" => $visibility ? $visibility->value->value : null,
-            "images" => $images ? $images->path : null,
+            "visibility" => $visibility->value_data,
+            "images" => $images ? Storage::url($images->path) : null,
             "created_at" => $this->created_at->format("M d, Y H:i A")
         ];
     }
