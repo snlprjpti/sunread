787966fdd06ac20cdc47a036a3dabb4d6b54ff5a
Rupesh Shrestha <rupesh.shrestha@hazesoft.co>
2021-07-16 10:44:59 +0545

    removed variable

--- Modules/Product/Database/factories/ProductFactory.php
+++ Modules/Product/Database/factories/ProductFactory.php
@@ -20,14 +20,14 @@ class ProductFactory extends Factory
         $system_defined_attributes = Attribute::whereIsUserDefined(0)->pluck('id')->toArray();
         $user_defined_attributes = Attribute::factory()->create()->id;
 
-        $attribute_group = AttributeGroup::factory()
-            ->create([
-                "attribute_set_id" => $attribute_set_id,
-                "position" => 1
-            ])
-            ->each(function ($attr_group) use ( $system_defined_attributes, $user_defined_attributes ) {
-                $attr_group->attributes()->sync(array_merge($system_defined_attributes, [ $user_defined_attributes ]));
-            });
+        AttributeGroup::factory()
+        ->create([
+            "attribute_set_id" => $attribute_set_id,
+            "position" => 1
+        ])
+        ->each(function ($attr_group) use ( $system_defined_attributes, $user_defined_attributes ) {
+            $attr_group->attributes()->sync(array_merge($system_defined_attributes, [ $user_defined_attributes ]));
+        });
 
         return [
             "parent_id" => null,
