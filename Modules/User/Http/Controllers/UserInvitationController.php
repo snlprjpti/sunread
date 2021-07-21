85f7dbd6f2c4388df5a95e06ae3031675cbaa458
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-07-21 15:24:45 +0545

    replace modelnot found exception

--- Modules/User/Http/Controllers/UserInvitationController.php
+++ Modules/User/Http/Controllers/UserInvitationController.php
@@ -2,13 +2,13 @@
 
 namespace Modules\User\Http\Controllers;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Hash;
 use Modules\Core\Http\Controllers\BaseController;
 use Modules\User\Entities\Admin;
+use Modules\User\Exceptions\AdminNotFoundException;
 use Modules\User\Repositories\AdminRepository;
 use Modules\User\Transformers\AdminResource;
 use Exception;
@@ -25,7 +25,7 @@ class UserInvitationController extends BaseController
         $this->model_name = "Admin account";
         $this->repository = $adminRepository;
         $exception_statuses = [
-            ModelNotFoundException::class => 403
+            AdminNotFoundException::class => 403
         ];
         parent::__construct($this->model, $this->model_name, $exception_statuses);
     }
@@ -58,8 +58,9 @@ class UserInvitationController extends BaseController
                 'invitation_token' => 'required',
                 'password' => 'required|confirmed|min:6',
             ]);
-            $user = $this->model->whereInvitationToken($request->invitation_token)->firstOrFail();
+            $user = $this->model->whereInvitationToken($request->invitation_token)->first();
 
+            if (! $user ) throw new AdminNotFoundException(__("core::app.response.not-found"));
             $data = [
                 "invitation_token" => null,
                 "password" => Hash::make($data["password"])
