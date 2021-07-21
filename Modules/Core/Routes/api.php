d6b5a1ddc4b64985bf1afb0a514c771dc8e9c08d
Bikram Lama <bikram.lama@hazesoft.co>
2021-07-21 11:35:05 +0545

    updated route name

--- Modules/Core/Routes/api.php
+++ Modules/Core/Routes/api.php
@@ -2,7 +2,7 @@
 
 Route::group(["middleware" => ["api"]], function () {
     // Resolver
-    Route::get("resolver/{website?}", [Modules\Core\Http\Controllers\ResolverController::class, "resolve"])->name("resolver");
+    Route::get("resolver/{website?}", [Modules\Core\Http\Controllers\ResolverController::class, "resolve"])->name("resolver.resolve");
 
     // Visitor channel stores
     Route::get("channels/{id}/stores", [Modules\Core\Http\Controllers\Visitors\ChannelController::class, "stores"])->name("channels.stores.index");
