b6bb18047d294f4c3811042f196c11ffc8cf7634
Anisha Khadka <aneesha,khadka@hazesoft.co>
2021-07-22 19:36:24 +0545

    update status route added

--- Modules/Page/Routes/api.php
+++ Modules/Page/Routes/api.php
@@ -7,6 +7,7 @@ Route::group(['middleware' => ['api']], function () {
     Route::group(['prefix'=>'admin', 'as' => 'admin.', 'middleware' => ['admin', 'language']], function () {
         Route::get('pages/components', [\Modules\Page\Http\Controllers\PageAttributeController::class, "index"])->name("components.index");
         Route::get('pages/components/{component_slug}', [\Modules\Page\Http\Controllers\PageAttributeController::class, "show"])->name("components.show");
+        Route::put("pages/{page_id}/status", [\Modules\Page\Http\Controllers\PageController::class, "updateStatus"])->name("pages.status");
         Route::resource('pages', PageController::class)->except(['create', 'edit']);
     });
 });
