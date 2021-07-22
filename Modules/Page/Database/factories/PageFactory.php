2543912a7e6486e209538266112af4b14085d7d5
Anisha Khadka <aneesha,khadka@hazesoft.co>
2021-07-22 19:36:37 +0545

    page factory is modified

--- Modules/Page/Database/factories/PageFactory.php
+++ Modules/Page/Database/factories/PageFactory.php
@@ -11,23 +11,7 @@ class PageFactory extends Factory
     protected $model = \Modules\Page\Entities\Page::class;
 
     public function definition(): array
-    {
-        for($i = 0; $i < rand(1,2); $i++)
-        {
-            $data["scope"] = Arr::random([ "website", "store" ]);
-
-            switch ($data["scope"]) {
-                case "website":
-                    $data["scope_id"] = Website::factory()->create()->id;
-                    break;
-    
-                case "store":
-                    $data["scope_id"] = Store::factory()->create()->id;
-                    break;
-            }
-            $scopes[] = $data;
-        }      
-
+    {      
         return [
             "title" => $this->faker->name(),
             "slug" => $this->faker->unique()->slug(),
@@ -35,9 +19,7 @@ class PageFactory extends Factory
             "meta_keywords" => $this->faker->name(),
             "meta_description" => $this->faker->paragraph(),
             "position" => $this->faker->randomDigit(),
-            "status" => 1,
-            "scopes" => $scopes,
-            "attributes" => []
+            "status" => 1
         ];
     }
 }
