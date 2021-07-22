fdc373fd99c68c35e7f6a24c80cb42b63458d147
Bikram Lama <bikram.lama@hazesoft.co>
2021-07-22 15:52:03 +0545

    Added resources

---
+++ Modules/Core/Transformers/ResolveChannelResource.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Modules\Core\Transformers;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ResolveChannelResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "code" => $this->code,
+            "stores" => ResolveStore♦Resource::collection($this->whenLoaded("stores")),
+        ];
+    }
+}
