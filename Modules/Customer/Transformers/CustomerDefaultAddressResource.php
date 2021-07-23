96a1dd1ef03d814ccfa70404709067bdcc43d576
Anisha Khadka <aneesha,khadka@hazesoft.co>
2021-07-23 12:53:26 +0545

    Default addresss resource is created

--- Modules/Customer/Transformers/CustomerDefaultAddressResource.php
+++ Modules/Customer/Transformers/CustomerDefaultAddressResource.php
@@ -9,19 +9,8 @@ class CustomerDefaultAddressResource extends JsonResource
     public function toArray($request): array
     {
         return [
-            "id" => $this->id,
-            "last_logged_in" => $this->created_at->format('M d, Y H:i A'),
-            "is_lock" => (bool) $this->is_lock,	
-            "confirmed_email" => "",
-            "created_at" => $this->created_at->format('M d, Y H:i A'),
-            "customer_store" =>	$this->when($this->relationLoaded("store"), $this->store?->name),
-            "customer_group" => $this->when($this->relationLoaded("group"), $this->group?->name),
-            "default_addresses" => $this->when($this->relationLoaded("addresses"), function() {
-                return [
-                    "billing" => new CustomerAddressResource($this->default_billing_address),
-                    "shipping" => new CustomerAddressResource($this->default_shipping_address)
-                ];
-            })
+            "default_billing_address" => $this->when($this->default_billing_address, new CustomerAddressResource($this->default_billing_address)),
+            "default_shipping_address" => $this->when($this->default_shipping_address, new CustomerAddressResource($this->default_shipping_address))
         ];
     }
 }
