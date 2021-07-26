95e7d8daa3fca34b897a7e5617558d0e6fab428e
Rupesh Shrestha <rupesh.shrestha@hazesoft.co>
2021-07-26 10:19:26 +0545

    updated for now

--- Modules/Erp/Traits/HasErpMapper.php
+++ Modules/Erp/Traits/HasErpMapper.php
@@ -8,31 +8,60 @@ use Illuminate\Support\Facades\Http;
 
 trait HasErpMapper
 {
-
-	public array $credentials = [
-		"Username" => "SPORTMANSHIP\exthbg",
-		"Password" => "uFsR+z2862hZSWqcKt3ehPWpbakTSJ+OxQaFW/+MTUc="
-	];
+	protected $url = "https://bc.sportmanship.se:7148/sportmanshipbctestapi/api/NaviproAB/web/beta/";
 
 	public function getList()
 	{
-		$url = "https://bc.sportmanship.se:7148/sportmanshipbctestapi/api/NaviproAB/web/beta/webItems";
-		// $response = Http::get("https://bc.sportmanship.se:7148/sportmanshipbctestapi/api/NaviproAB/web/beta/webItems", $this->credentials);
-		// dd("asd");
-		$client = new Client();
-// 		$response = $client->get("https://bc.sportmanship.se:7148/sportmanshipbctestapi/api/NaviproAB/web/beta/webItems",
-// $this->credentials);
-		// $response
-		$credentials = base64_encode("SPORTMANSHIP\exthbg:uFsR+z2862hZSWqcKt3ehPWpbakTSJ+OxQaFW/+MTUc=");
-		$asd = "U1BPUlRNQU5TSElQG3h0aGJnOnVGc1IrejI4NjJoWlNXcWNLdDNlaFBXcGJha1RTSitPeFFhRlcvK01UVWM9";
-		// dd($credentials);
-		$auth = "U1BPUlRNQU5TSElQXGV4dGhiZzp1RnNSK3oyODYyaFpTV3FjS3QzZWhQV3BiYWtUU0orT3hRYUZXLytNVFVjPQ==";
-		$response = $client->request("GET", $url, [
-			// "auth" => ["SPORTMANSHIP\exthbg","uFsR+z2862hZSWqcKt3ehPWpbakTSJ+OxQaFW/+MTUc="],
-			"headers" => [
-				"Authorization" => "Basic {$auth}"
-			]
-		]);
-		dd($response->getBody());
+		dd($this->getProducts());
+
+		$response = Http::withBasicAuth(env("ERP_API_USERNAME"), env("ERP_API_PASSWORD"))->get($this->url."webAssortments");
+		$values = [];
+		$values = $response->json()["value"];
+		$last = [];
+		$last[] = end($values);
+		
+		while ( true )
+		{
+			$last_value = end($last);
+			$skip = '$skiptoken='."'{$last_value['itemNo']}',".'SR'.",'{$last_value['colorCode']}'";
+			$dimensional = Http::withBasicAuth(env("ERP_API_USERNAME"), env("ERP_API_PASSWORD"))->get($this->url."webAssortments",$skip);
+			if (empty($dimensional->json()["value"])) break;
+			$last[] = end($dimensional->json()["value"]);
+			$values = array_merge($dimensional->json()["value"], $values);
+		}
+
+		$collection =  collect($values)
+			->where("webActive", true)
+			->where("webSetup", "SR")
+			->chunk(50)
+			->flatten(1);
+		
+		dd($collection);
+		
+
+
+
+
+	}
+
+	public function getProducts()
+	{
+		$response = Http::withBasicAuth(env("ERP_API_USERNAME"), env("ERP_API_PASSWORD"))->get($this->url."webItems");
+
+		$values = [];
+		$values = $response->json()["value"];
+		$last = [];
+		$last[] = end($values);
+		
+		while ( true )
+		{
+			$last_value = end($last);
+			$skip = '$skiptoken='."'{$last_value['no']}',"."'{$last_value['webAssortmentWeb_Setup']}',"."'{$last_value['webAssortmentColor_Code']}'".",'{$last_value['languageCode']}'".",'{$last_value['auxiliaryIndex1']}'".",'{$last_value['auxiliaryIndex2']}'".",'{$last_value['auxiliaryIndex3']}'".",'{$last_value['auxiliaryIndex4']}'";
+			$dimensional = Http::withBasicAuth(env("ERP_API_USERNAME"), env("ERP_API_PASSWORD"))->get($this->url."webItems",$skip);
+			if (empty($dimensional->json()["value"])) break;
+			$last[] = end($dimensional->json()["value"]);
+			$values = array_merge($dimensional->json()["value"], $values);
+		}
+		dd($values);		
 	}
 }
