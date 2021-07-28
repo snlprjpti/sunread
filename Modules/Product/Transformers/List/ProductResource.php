280a73f6a2356dd3419f9e2cd4737680f8c753f1
Anisha Khadka <aneesha,khadka@hazesoft.co>
2021-07-28 18:49:26 +0545

    product listing issue solved

--- Modules/Product/Transformers/List/ProductResource.php
+++ Modules/Product/Transformers/List/ProductResource.php
@@ -5,6 +5,7 @@ namespace Modules\Product\Transformers\List;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Storage;
 use Modules\Attribute\Entities\Attribute;
+use Modules\Attribute\Entities\AttributeOption;
 use Modules\Category\Transformers\CategoryResource;
 
 class ProductResource extends JsonResource
@@ -13,21 +14,23 @@ class ProductResource extends JsonResource
     {
         $attributeIdforName = Attribute::whereSlug("name")->first()->id;
         $attributeIdforVisibility = Attribute::whereSlug("visibility")->first()->id;
+
         $product_attributes = $this->product_attributes()->whereScope($request->scope ?? "website")->whereScopeId($request->scope_id ?? $this->website_id);
-        $name = $product_attributes->whereAttributeId($attributeIdforName)->first();
-        $visibility = $this->product_attributes()->whereScope($request->scope ?? "website")->whereScopeId($request->scope_id ?? $this->website_id)->whereAttributeId($attributeIdforVisibility)->first();
+        $name = (clone $product_attributes)->whereAttributeId($attributeIdforName)->first();
+        $visibility = $product_attributes->whereAttributeId($attributeIdforVisibility)->first();
+        
         $images = $this->images()->where('main_image', 1)->first();
         $stock = $this->catalog_inventories()->first();
 
         return [
             "id" => $this->id,
-            "name" => $name->value_data,
+            "name" => $name?->value_data,
             "type" => $this->type,
             "sku" => $this->sku,           
-            "stock" => $stock ? $stock->quantity : null,
+            "stock" => (int) $stock?->quantity,
             "status" => (bool) $this->status,
             "categories" => CategoryResource::collection($this->whenLoaded("categories")),
-            "visibility" => $visibility->value_data,
+            "visibility" => AttributeOption::find($visibility?->value_data)?->name,
             "images" => $images ? Storage::url($images->path) : null,
             "created_at" => $this->created_at->format("M d, Y H:i A")
         ];
