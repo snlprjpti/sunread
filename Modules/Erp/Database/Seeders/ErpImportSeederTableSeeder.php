ee47bec8de9860ce1c7747e99607ceac6d301e20
Rupesh Shrestha <rupesh.shrestha@hazesoft.co>
2021-07-29 13:12:09 +0545

    updated seeder

---
+++ Modules/Erp/Database/Seeders/ErpImportSeederTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Modules\Erp\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class ErpImportSeederTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            "webAssortments",
+            "listProducts",
+            "attributeGroups",
+            "salePrices",
+            "eanCodes",
+            "webInventories",
+            "productDescriptions",
+            "productVariants"
+        ];
+
+        foreach ( $data as $type )
+        {
+            DB::table("erp_imports")->insert(["type" => $type, "created_at" => now(), "updated_at" => now()]);
+        }
+    }
+}
