db6a2ec3e981939e78122b493d17b13481e3d984
Rupesh Shrestha <rupesh.shrestha@hazesoft.co>
2021-07-30 18:15:50 +0545

    work on progress

--- Modules/Erp/Traits/HasErpValueMapper.php
+++ Modules/Erp/Traits/HasErpValueMapper.php
@@ -8,6 +8,9 @@ use Modules\Attribute\Entities\AttributeGroup;
 use Modules\Attribute\Entities\AttributeSet;
 use Modules\Attribute\Entities\AttributeTranslation;
 use Modules\Erp\Entities\ErpImport;
+use Modules\Inventory\Entities\CatalogInventory;
+use Modules\Inventory\Jobs\LogCatalogInventoryItem;
+use Modules\Product\Entities\Product;
 
 trait HasErpValueMapper
 {
@@ -22,144 +25,146 @@ trait HasErpValueMapper
 		"productVariants"
 	];
 
+	protected array $erp_attributes = [
+		"Features",
+		"Size and Care",
+		"Ean Code"
+	];
+
 	public function importAll()
 	{
-		$erp = ErpImport::query();
-		$erp_details = $erp->whereType("listProducts")->first()->erp_import_details;
-		dd($erp_details);
-		//1111703, OR161610
-		// dd($erp_details->last());
-		// dd($erp_details->where("sku","OR161610")->map( function ($data) {
-		// 	return json_decode($data->value, true);
-		// }));
-		// dd("asd");
-		$attributes = $this->getAttributes();
+		$erp_details = ErpImport::where("type","listProducts")->first()->erp_import_details;
 
-
+		$erp = ErpImport::all();
+		$count = 0;
 		foreach ( $erp_details as $detail )
 		{
-			$type = ($erp_details->where("sku", "OR161610")->count() > 1) ? "configurable" : "simple";
-
-			// product updateOrCreate data
-			$data = [
+			$product_data = [
 				"attribute_set_id" => 1,
 				"website_id" => 1,
-				"sku" => $erp_details->sku,
-				"type" => $type,
-				"created_at" => now(),
-				"updated_at" => now()
+				"sku" => $detail->sku,
+				"type" => "simple",
 			];
 
+			$match = [
+				"website_id" => 1,
+				"sku" => $detail->sku,
+			];
+
+			$product = Product::updateOrCreate($match, $product_data);
+
+			$desc = $erp->where("type", "productDescriptions")->first()->erp_import_details()->where("sku", $detail->sku)->get();
+			$price_detail = $erp->where("type", "salePrices")->first()->erp_import_details()->where("sku", $detail->sku)->get();
+
+			$inventory = $erp->where("type", "webInventories")->first()->erp_import_details()->where("sku", $detail->sku)->get(); 
+
+			$variants = $erp->where("type", "productVariants")->first()->erp_import_details()->where("sku", $detail->sku)->get(); 
+
+			$attr_groups = $erp->where("type", "attributeGroups")->first()->erp_import_details()->where("sku", $detail->sku)->get(); 
 			
 
-			$erp->whereType("eanCodes")->first()->erp_import_details->where("value->code");
+			$count ++;
+		
+			// $this->createInventory($product, $inventory);
+			if ($count > 50) $this->createAttributeValue($attr_groups, $product, $detail);
 
 
 		}
-		dd($attributes);
-
-		
-
-		dd(json_decode($erp_details->value, true));
-
-		dd($erp_details->first());
-
+		// $this->createVariant($product, $variants);
 		dd("asd");
 	}
 
-	public function getAttributes(): array
+	private function createAttributeValue(mixed $attr_groups, object $product, object $erp_product_iteration)
 	{
 		try
 		{
-			$attributes = [
+			$attribute_data = [
 				[
-					"name" => "Features",
-					"type" => "texteditor",
-					"scope" => "store",
-					"is_searchable" => 1,
-					"search_weight" => 1
+					"attribute_id" => 1,
+					"value" => $erp_product_iteration->value["description"]
 				],
 				[
-					"name" => "Size and Care",
-					"type" => "texteditor",
-					"scope" => "store",
-					"is_searchable" => 1,
-					"search_weight" => 1
+					"attribute_id" => 28,
+					"value" => $this->getAttributeValue( $attr_groups, "Features" ), 
 				],
 				[
-					"name" => "EAN Code",
-					"type" => "text",
-					"is_unique" => 1,
-					"scope" => "website"
+					"attribute_id" => 29,
+					"value" => $this->getAttributeValue( $attr_groups, "Size and care" ), 
+				],
+				[
+					"attribute_id" => 29,
+					"value" => $this->getAttributeValue( $attr_groups, "Size and care" ), 
 				]
 			];
-
-			$attribute_arr = [];
-
-			foreach ( $attributes as $attribute )
-			{
-				$attribute_array = [
-					"slug" => \Str::slug($attribute["name"]),
-					"name" => $attribute["name"],
-					"type" => $attribute["type"],
-					"scope" => $attribute["scope"] ?? "website",
-					"validation" => $attribute["validation"] ?? null,
-					"is_required" => $attribute["is_required"] ?? 0,
-					"is_unique" => $attribute["is_unique"] ?? 0,
-					"use_in_layered_navigation" => $attribute["use_in_layered_navigation"] ?? 0,
-					"comparable_on_storefront" => $attribute["comparable_on_storefront"] ?? 0,
-					"is_searchable" => $attribute["is_searchable"] ?? 0,
-					"search_weight" => $attribute["search_weight"] ?? null,
-					"is_user_defined" => $attribute["is_user_defined"] ?? 0,
-					"is_visible_on_storefront" => $attribute["is_visible_on_storefront"] ?? 0,
-					"default_value" => null
-				];
-
-				$attribute_data = Attribute::withoutEvents( function () use ($attribute_array) {
-					return Attribute::updateOrCreate($attribute_array);
-				});
-
-				AttributeTranslation::updateOrCreate([
-					"store_id" => 1,
-					"name" => $attribute["name"],
-					"attribute_id" => $attribute_data->id
-				]);
-
-				$attribute_arr[] = [
-					"id" => $attribute_data->id,
-					"slug" => \Str::slug($attribute["name"]),
-				];
-			}
-
-			$attribute_set_data = [
-				"name" => "Import",
-				"is_user_defined" => 0,
-				"created_at" => now(),
-				"updated_at" => now()
-			];
-
-			$attribute_set = AttributeSet::withoutEvents( function () use ($attribute_set_data) {
-				return AttributeSet::updateOrCreate($attribute_set_data);
-			});
-
-			$attribute_group_data = [
-                "name" => "Extra",
-                "position" => 5,
-                "attribute_set_id" => $attribute_set->id,
-                "created_at" => now(),
-                "updated_at" => now()
-            ];
-
-			$attribute_group = AttributeGroup::create($attribute_group_data);
-
-			//add default attributes
-            $attribute_group->attributes()->sync(collect($attribute_arr)->pluck("id")->toArray());
+			// dd($attribute_data);
 		}
 		catch ( Exception $exception )
 		{
 			throw $exception;
 		}
 
-		return $attribute_arr;
 	}
+
+	private function getAttributeValue(mixed $attr_groups, string $attribute_name): string
+	{
+		try
+		{
+			$attach_value = "<ul><li>";
+			$this->getValue($attr_groups, function ($value) use (&$attach_value, $attribute_name) {
+				if ( $value["attributetype"] == $attribute_name ) $attach_value .= \Str::start("<li>", \Str::finish($value["description"], "</li>"));
+			});
+		}
+		catch ( Exception $exception )
+		{
+			throw $exception;
+		}
+		return $attach_value."</ul>";
+	}
+
+	private function getValue(mixed $values, callable $callback = null): mixed
+	{
+		try
+		{
+			$data = $values->map( function ($value) use($callback){
+				if ( $callback ) $data = $callback($value->value);
+				return ( $callback ) ? $data : $value->value;
+			});
+		}
+		catch ( Exception $exception )
+		{
+			throw $exception;
+		}
+
+		return $data;
+	}
+
+	private function createInventory(object $product, mixed $inventory): object
+	{
+		try
+		{
+			$value = array_sum($this->getValue($inventory)->pluck("Inventory")->toArray());
+			$data = [
+				"quantity" => $value,
+				"use_config_manage_stock" => 1,
+				"product_id" => $product->id,
+				"website_id" => $product->website_id,
+				"manage_stock" =>  0,
+				"is_in_stock" => (bool) ($value > 0) ? 1 : 0,
+			];
+	
+			$match = [
+				"product_id" => $product->id,
+				"website_id" => $product->website_id
+			];
+
+			$catalog_inventory = CatalogInventory::updateOrCreate($match, $data);
+		}
+		catch ( Exception $exception )
+		{
+			throw $exception;
+		}
+
+		return $catalog_inventory;
+	}
+
 }
