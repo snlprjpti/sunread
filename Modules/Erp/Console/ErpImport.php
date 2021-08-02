533c0dc0f9233c1ba27f07ea0eedb614f650e869
Rupesh Shrestha <rupesh.shrestha@hazesoft.co>
2021-08-02 16:20:01 +0545

    made artisan commands

---
+++ Modules/Erp/Console/ErpImport.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Modules\Erp\Console;
+
+use Exception;
+use Illuminate\Console\Command;
+use Modules\Erp\Jobs\EanCodes;
+use Modules\Erp\Jobs\ErpAttributeGroups;
+use Modules\Erp\Jobs\ListProducts;
+use Modules\Erp\Jobs\ProductImages;
+use Modules\Erp\Jobs\ProductVariants;
+use Modules\Erp\Jobs\SalePrices;
+use Modules\Erp\Jobs\WebInventories;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class ErpImport extends Command
+{
+    public $signature = 'erp:import';
+
+    protected $description = 'This command will import all data from Erp API.';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle(): void
+    {
+        ProductImages::dispatch();
+        EanCodes::dispatch();
+        ErpAttributeGroups::dispatch();
+        ListProducts::dispatch();
+        ProductVariants::dispatch();
+        SalePrices::dispatch();
+        WebInventories::dispatch();
+    }
+
+    protected function getArguments(): array
+    {
+        return [
+            ['erp:import', InputArgument::REQUIRED, 'An example argument.'],
+        ];
+    }
+
+    protected function getOptions(): array
+    {
+        return [
+            ['erp:import', null, InputOption::VALUE_OPTIONAL, 'An example option.', null],
+        ];
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Modules\Erp\Console\ErpImport;
+use Modules\Erp\Console\ErpMigrate;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,7 +15,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        ErpImport::class,
+        ErpMigrate::class,
     ];
 
     /**
@@ -25,6 +28,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command("erp:import");
+        $schedule->command("erp:migrate");
     }
 
     /**
