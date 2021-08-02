7165b59f9c3bde14b67fbd96927972cb14cfeff2
Rupesh Shrestha <rupesh.shrestha@hazesoft.co>
2021-08-02 11:02:12 +0545

    updated group job

--- Modules/Erp/Jobs/ErpAttributeGroups.php
+++ Modules/Erp/Jobs/ErpAttributeGroups.php
@@ -13,13 +13,15 @@ class ErpAttributeGroups implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, HasErpMapper;
 
-    public function __construct()
+    public $skip_token;
+
+    public function __construct(?string $skip_token = null)
     {
-        //
+        $this->skip_token = $skip_token;
     }
 
     public function handle(): void
     {
-        $this->erpImport("attributeGroups", $this->url."webItemAttributeGroups");
+        $this->erpImport("attributeGroups", $this->url."webItemAttributeGroups", $this->skip_token);
     }
 }
