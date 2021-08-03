34595f3bc7c237cf8534131f4de5984945dfc971
Rupesh Shrestha <rupesh.shrestha@hazesoft.co>
2021-08-03 09:42:24 +0545

    removed unnecessary codes

--- Modules/Erp/Routes/api.php
+++ Modules/Erp/Routes/api.php
@@ -11,10 +11,3 @@
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::group(['middleware' => ['api']], function () {
-    Route::group(["prefix" => "admin/erp", "middleware" => ["admin", "language"], "as" => "admin.erp."], function () {        
-        Route::resource("erp", ErpController::class)->except(["create","edit"]);
-    });
-    
-});
