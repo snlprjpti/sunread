90efc0084f5e45af8d3885c6e125f106cd7135f8
Anisha Khadka <aneesha,khadka@hazesoft.co>
2021-08-11 09:35:46 +0545

    commented test code

--- Modules/Core/Listeners/StoreListener.php
+++ Modules/Core/Listeners/StoreListener.php
@@ -10,30 +10,30 @@ class StoreListener
 {
     public function indexing($store)
     {
-        $hosts = [
-            "localhost:9200" 
-        ];
+        // $hosts = [
+        //     "localhost:9200" 
+        // ];
         
-        $client = ClientBuilder::create()->setHosts($hosts)->build();
+        // $client = ClientBuilder::create()->setHosts($hosts)->build();
 
-        $website = $store?->channel?->website;
-        $products = Product::whereWebsiteId($website->id)->get();
-        foreach($products as $product)
-        {
-            $data = $product->toArray();
-            foreach($product->product_attributes()->with("attribute")->get() as $product_attribute)
-            {
-                $data[$product_attribute->attribute->slug] = $product_attribute->value?->value;
-            }
-            $data["categories"] = $product->categories()->pluck("id")->toArray();
-            $data["catalog_inventories"] = $product->catalog_inventories()->get();
+        // $website = $store?->channel?->website;
+        // $products = Product::whereWebsiteId($website->id)->get();
+        // foreach($products as $product)
+        // {
+        //     $data = $product->toArray();
+        //     foreach($product->product_attributes()->with("attribute")->get() as $product_attribute)
+        //     {
+        //         $data[$product_attribute->attribute->slug] = $product_attribute->value?->value;
+        //     }
+        //     $data["categories"] = $product->categories()->pluck("id")->toArray();
+        //     $data["catalog_inventories"] = $product->catalog_inventories()->get();
 
-            $params = [
-                "index" => "sail_racing_store_{$store->id}",
-                "id" => $product->id,
-                "body" => $data
-            ];
-            $client->index($params);
-        }
+        //     $params = [
+        //         "index" => "sail_racing_store_{$store->id}",
+        //         "id" => $product->id,
+        //         "body" => $data
+        //     ];
+        //     $client->index($params);
+        // }
     }
 }
