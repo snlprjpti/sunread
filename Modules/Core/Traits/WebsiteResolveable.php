80c1f6e2f9d6a4a0a7960653124485756842f5d2
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-08-16 15:47:55 +0545

    cache implement

--- Modules/Core/Traits/WebsiteResolveable.php
+++ Modules/Core/Traits/WebsiteResolveable.php
@@ -3,8 +3,8 @@
 namespace Modules\Core\Traits;
 
 use Exception;
+use Illuminate\Support\Facades\Cache;
 use Modules\Core\Entities\Channel;
-use Modules\Core\Entities\Configuration;
 use Modules\Core\Entities\Store;
 use Modules\Core\Entities\Website;
 use Illuminate\Support\Facades\Request;
@@ -53,18 +53,22 @@ trait WebsiteResolveable
     {
         try
         {
-            $domain = $this->getDomain();
+            $host = $request->header('hc-host');
+            $website = Cache::rememberForever("website_{$host}", function() use($request){
+                $domain = $this->getDomain();
+                $fallback_id = config("website.fallback_id");
+                if ($request->hasHeader('hc-host')) {
+                    $fallback_id = Website::whereHostname($request->header("hc-host"))->firstOrFail()?->id;
+                }
 
-            $fallback_id = config("website.fallback_id");
-            if ($request->hasHeader('hc-host')) {
-                $fallback_id = Website::whereHostname($request->header("hc-host"))->firstOrFail()?->id;
-            }
+                $website = Website::whereHostname($domain);
+                if ( !$website->exists() && config("website.environment") == "local" ) {
+                    $website = Website::whereId($fallback_id);
+                }
+                $website = $website->firstOrFail();
+                return $website;
+            });
 
-            $website = Website::whereHostname($domain);
-            if ( !$website->exists() && config("website.environment") == "local" ) {
-                $website = Website::whereId($fallback_id);
-            }
-            $website = $website->select(["id","name","code"])->setEagerLoads([])->firstOrFail();
             $website->channel = $this->getChannel($request, $website);
             $website->store = $this->getStore($request, $website);
             $website->pages = $this->getPages($website);
@@ -83,13 +87,15 @@ trait WebsiteResolveable
     {
         try
         {
-            if($request->hasHeader("hc-channel")){
-                $channel_code = $request->header("hc-channel");
-                $channel = Channel::whereCode($channel_code)->select(["id","name","code"])->setEagerLoads([])->firstOrFail();
-            }
-            else {
-                $channel = ($channel = $this->checkConditionChannel($website)) ? $channel->firstOrFail() : null;
-            }
+            $channel_code = $request->header("hc-channel");
+            $channel = Cache::rememberForever("channel_{$channel_code}", function() use($channel_code, $website) {
+                if ($channel_code) {
+                    $channel = Channel::whereCode($channel_code)->firstOrFail();
+                } else {
+                    $channel = $this->checkConditionChannel($website) ?? null;
+                }
+                return $channel;
+            });
         }
         catch( Exception $exception )
         {
@@ -103,13 +109,15 @@ trait WebsiteResolveable
     {
         try
         {
-            if($request->hasHeader("hc-store")){
-                $store_code = $request->header("hc-store");
-                $store = Store::whereCode($store_code)->select(["id","name","code"])->setEagerLoads([])->firstOrFail();
-            }
-            else {
-                $store = ($store = $this->checkConditionStore($website)) ? $store->firstOrFail() : null;
-            }
+            $store_code = $request->header("hc-store");
+            $store = Cache::rememberForever("store_{$store_code}", function() use($store_code, $website) {
+                if ($store_code) {
+                    $store = Store::whereCode($store_code)->firstOrFail();
+                } else {
+                    $store = $this->checkConditionStore($website) ?? null;
+                }
+                return $store;
+            });
         }
         catch( Exception $exception )
         {
