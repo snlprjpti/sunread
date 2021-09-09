3d9e5400fe8a57c11e4f0589f653d4f7360994fd
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-09-09 15:33:11 +0545

    api update

--- Modules/Country/Routes/api.php
+++ Modules/Country/Routes/api.php
@@ -12,7 +12,7 @@ Route::group(['middleware' => ['api']], function () {
 //    PUBLIC COUNTRY ROUTES
     Route::group(['prefix'=> 'public', 'as' => 'public.'], function () {
 
-        Route::get('country', [ \Modules\Country\Http\Controllers\StoreFront\CountryController::class, "index" ]);
+        Route::get('countries', [ \Modules\Country\Http\Controllers\StoreFront\CountryController::class, "index" ]);
         Route::get('regions', [ \Modules\Country\Http\Controllers\StoreFront\RegionController::class, "index" ]);
         Route::get('cities', [ \Modules\Country\Http\Controllers\StoreFront\CityController::class, "index" ]);
     });
