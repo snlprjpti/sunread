0044c814e634f9b13648e481a4ed4ca3b6efa240
Anisha Khadka <aneesha,khadka@hazesoft.co>
2021-09-10 10:55:51 +0545

    category update in elasticsearch

--- Modules/Category/Observers/CategoryValueObserver.php
+++ Modules/Category/Observers/CategoryValueObserver.php
@@ -16,17 +16,27 @@ class CategoryValueObserver
 
     public function created(CategoryValue $category_value)
     {
+        if(($category_value->attribute == "name" || $category_value->attribute == "slug") && $category_value->scope != "website") 
+        $this->preparingIndexData($category_value->category->products);
         // UrlRewrite::handleUrlRewrite($category_translation, __FUNCTION__);
     }
 
     public function updated(CategoryValue $category_value)
     {
-        if($category_value->attribute == "name" || $category_value->attribute == "slug") $this->preparingIndexData($category_value->category->products);
+        if($category_value->attribute == "name" || $category_value->attribute == "slug") 
+        $this->preparingIndexData($category_value->category->products);
         // UrlRewrite::handleUrlRewrite($category_translation, __FUNCTION__);
     }
 
     public function deleted(CategoryValue $category_value)
     {
+        // UrlRewrite::handleUrlRewrite($category_translation, __FUNCTION__);
+    }
+
+    public function deleting(CategoryValue $category_value)
+    {
+        if($category_value->attribute == "name" || $category_value->attribute == "slug") 
+        $this->preparingIndexData($category_value->category->products);
         // UrlRewrite::handleUrlRewrite($category_translation, __FUNCTION__);
     }
 }
