f6e1ae61a6948210bd73cf038dc2713b6a7bd0a9
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-09-13 14:01:41 +0545

    remove variable

--- Modules/EmailTemplate/Routes/api.php
+++ Modules/EmailTemplate/Routes/api.php
@@ -2,6 +2,5 @@
 
 Route::group(["prefix" => "admin", "as" => "admin.", "middleware" => ["api", "admin", "language"]], function () {
 
-    Route::get("/email-templates/variables", [\Modules\EmailTemplate\Http\Controllers\EmailTemplateController::class, "getVariables"]);
     Route::resource("/email-templates", EmailTemplateController::class);
 });
