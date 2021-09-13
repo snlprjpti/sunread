95f5400aea9d7260f2df005c4d41b04f432d0312
Rupesh Shrestha <rupesh.shrestha@hazesoft.co>
2021-09-13 17:04:40 +0545

    added inventory queue

--- Modules/Core/Services/ActivityLogHelper.php
+++ Modules/Core/Services/ActivityLogHelper.php
@@ -9,6 +9,7 @@ use Modules\Attribute\Entities\Attribute;
 use Modules\Attribute\Entities\AttributeOption;
 use Modules\Attribute\Entities\AttributeSet;
 use Modules\Core\Entities\ActivityLog;
+use Modules\Product\Jobs\UpdateProductInventoryJob;
 use Modules\Review\Entities\ReviewVote;
 
 class ActivityLogHelper {
@@ -37,6 +38,8 @@ class ActivityLogHelper {
         if($model_name == "ReviewVote") $this->reviewVoteCache($model);
 
         if ($model_name == "Attribute" || $model_name == "AttributeSet" || $model_name == "AttributeOption") $this->attributeCache($model);
+
+        if ($model_name == "Product") UpdateProductInventoryJob::dispatch($model, $event);
 
         if(Cache::get($model::class)) $this->modelCache($model);
 
