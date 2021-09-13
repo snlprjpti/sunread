80b50aaefa1bff99e7d1b0e4f66d4d256f657993
Anisha Khadka <aneesha,khadka@hazesoft.co>
2021-09-13 16:50:17 +0545

    tab issue fixed

--- Modules/Category/Exceptions/CategoryNotFoundException.php
+++ Modules/Category/Exceptions/CategoryNotFoundException.php
@@ -5,7 +5,7 @@ namespace  Modules\Category\Exceptions;
 
 class CategoryNotFoundException extends \Exception
 {
-    public function __construct()
+	public function __construct()
 	{
 		parent::__construct(__("core::app.response.not-found", ["name" => "Category"]));
 	}
