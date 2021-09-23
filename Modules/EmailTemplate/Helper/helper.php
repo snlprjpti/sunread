aba307578ddc3b6db9073419f8511f346bdf0d4c
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-09-23 22:03:08 +0545

    helper method updated

--- Modules/EmailTemplate/Helper/helper.php
+++ Modules/EmailTemplate/Helper/helper.php
@@ -2,9 +2,11 @@
 
 use \Modules\EmailTemplate\Entities\EmailTemplate;
 
-function hc_include_email_template($path, $scope, $scope_id)
-{
-    $template = EmailTemplate::findOrFail(1);
-    return $template->content;
+if (!function_exists('hc_include_email_template')) {
+    function hc_include_email_template($path, $scope, $scope_id)
+    {
+        $template = EmailTemplate::findOrFail(1);
+        return $template->content;
+    }
 }
 
