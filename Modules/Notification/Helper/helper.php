4ae4f0f16cad17e12a432d180bcf493b22c9fb50
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-09-27 17:17:02 +0545

    add helper function

---
+++ Modules/Notification/Helper/helper.php
@@ -0,0 +1,15 @@
+<?php
+
+use Modules\Core\Facades\SiteConfig;
+
+/**
+ * function to get content of email template from configuration
+ */
+if (!function_exists('hc_include_email_template'))
+{
+    function hc_include_email_template($path)
+    {
+        $template = SiteConfig::fetch($path, "store", config('store'));
+        return $template->content;
+    }
+}
