9c2f7ccb2d337ae3c97fc1d8af283e83da55eb13
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-09-27 17:31:21 +0545

    revert customer factory and seeder

--- Modules/Customer/Database/factories/CustomerFactory.php
+++ Modules/Customer/Database/factories/CustomerFactory.php
@@ -5,7 +5,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
-use Modules\Core\Entities\Store;
 use Modules\Core\Entities\Website;
 use Modules\Customer\Entities\CustomerGroup;
 
@@ -16,8 +15,6 @@ class CustomerFactory extends Factory
     public function definition(): array
     {
         $gender = Arr::random([ "male", "female", "other" ]);
-        $store_id = Store::first()->id;
-
         return [
             "customer_group_id" => CustomerGroup::factory()->create()->id,
             "website_id" => Website::factory()->create()->id,
@@ -30,8 +27,7 @@ class CustomerFactory extends Factory
             "password" => Hash::make("password"),
             "subscribed_to_news_letter" => 0,
             "remember_token" => Str::random(10),
-            "account_type" => Arr::random(["personal", "company"]),
-            "store_id" => $store_id
+            "account_type" => Arr::random(["personal", "company"])
         ];
     }
 }
