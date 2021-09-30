d9fa0705c13b874e4c12bde551e6153ed18cfc6c
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-09-30 13:17:57 +0545

    registered event and listener

--- Modules/Notification/Providers/EventServiceProvider.php
+++ Modules/Notification/Providers/EventServiceProvider.php
@@ -3,7 +3,13 @@
 namespace Modules\Notification\Providers;
 
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Modules\Notification\Events\ConfirmEmail;
+use Modules\Notification\Events\ForgotPassword;
 use Modules\Notification\Events\RegistrationSuccess;
+use Modules\Notification\Events\ResetPassword;
+use Modules\Notification\Listeners\SendAccountConfirmation;
+use Modules\Notification\Listeners\SendPasswordChangeEmail;
+use Modules\Notification\Listeners\SendPasswordResetLink;
 use Modules\Notification\Listeners\SendWelcomeEmail;
 
 class EventServiceProvider extends ServiceProvider
@@ -11,6 +17,15 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         RegistrationSuccess::class => [
             SendWelcomeEmail::class,
+        ],
+        ConfirmEmail::class => [
+            SendAccountConfirmation::class,
+        ],
+        ForgotPassword::class => [
+            SendPasswordResetLink::class,
+        ],
+        ResetPassword::class => [
+            SendPasswordChangeEmail::class,
         ],
     ];
 
