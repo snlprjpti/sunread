25f3447fb30d5e1247aaa328d5ea41ceda9d674a
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-09-30 16:12:19 +0545

    email template seeder update

--- Modules/EmailTemplate/Database/Seeders/EmailTemplateSeeder.php
+++ Modules/EmailTemplate/Database/Seeders/EmailTemplateSeeder.php
@@ -25,7 +25,28 @@ class EmailTemplateSeeder extends Seeder
                 "email_template_code" => "footer",
                 "content" => '</td></tr><tr><td class="footer"><p class="closing">Thank you!</p></td></tr></table></td></tr></table></body></html>',
                 "style" => ""
-            ]
+            ],
+            [
+                "name" => "Welcome",
+                "subject" => "Welcome",
+                "email_template_code" => "welcome_email",
+                "content" => "<h2>Welcome Here</h2>",
+                "style" => ""
+            ],
+            [
+                "name" => "Forgot Password",
+                "subject" => "Forgot Password",
+                "email_template_code" => "forgot_password",
+                "content" => "<h2>Forgot Password</h2>",
+                "style" => ""
+            ],
+            [
+                "name" => "Reset Password",
+                "subject" => "Reset Password",
+                "email_template_code" => "Reset_password",
+                "content" => "<h2>Reset Password</h2>",
+                "style" => ""
+            ],
         ];
 
         $data = array_map(function ($template) {
