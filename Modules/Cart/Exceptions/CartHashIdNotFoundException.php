072078d2d72f78b3fccd2b18dcb65d4eb2bb13cc
Rupesh Shrestha <rupesh.shrestha@hazesoft.co>
2021-10-04 16:48:28 +0545

    updated space

--- Modules/Cart/Exceptions/CartHashIdNotFoundException.php
+++ Modules/Cart/Exceptions/CartHashIdNotFoundException.php
@@ -5,7 +5,7 @@ namespace  Modules\Cart\Exceptions;
 class CartHashIdNotFoundException extends \Exception
 {
     public function __construct()
-	{
-		parent::__construct(__("core::app.response.not-found", ["name" => "Cart"]));
-	}
+    {
+        parent::__construct(__("core::app.response.not-found", ["name" => "Cart"]));
+    }
 }
