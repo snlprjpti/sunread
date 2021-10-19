2f1cc62ec99d8bc226769844b14eaaabce9c899d
Anisha Khadka <aneesha.khadka@hazesoft.co>
2021-10-19 09:23:11 +0000

    Update ElasticSearchImport.php

--- Modules/Product/Console/ElasticSearchImport.php
+++ Modules/Product/Console/ElasticSearchImport.php
@@ -16,6 +16,7 @@ use Modules\Product\Jobs\SingleIndexing;
 use Modules\Product\Jobs\VariantIndexing;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
+use Illuminate\Bus\Batch;
 
 class ElasticSearchImport extends Command
 {
@@ -30,7 +31,34 @@ class ElasticSearchImport extends Command
 
     public function handle(): void
     {
-        ReIndexer::dispatch()->onQueue("index");
+        $batch = Bus::batch([])->onQueue("index")->dispatch();
+        
+        $products = Product::whereType("simple")->whereParentId(null)->get();
+        foreach($products as $product)
+        {
+            $stores = Website::find($product->website_id)->channels->map(function ($channel) {
+                return $channel->stores;
+            })->flatten(1);
+            
+            foreach($stores as $store) $batch->add(new SingleIndexing($product, $store));
+        }
+
+        $configurable_products = Product::whereType("configurable")->get();
+        foreach($configurable_products as $configurable_product)
+        {
+            $stores = Website::find($configurable_product->website_id)->channels->map(function ($channel) {
+                return $channel->stores;
+            })->flatten(1);
+            $variants = $configurable_product->variants()->with(["categories", "product_attributes", "catalog_inventories", "attribute_options_child_products"])->get();
+    
+            foreach( $stores as $store) {
+                $configurable_batch = Bus::batch([])->then(function (Batch $variant_batch) use ($variants, $configurable_product, $store) {
+                    $variant_batch = Bus::batch([])->allowFailures()->onQueue('index')->dispatch();
+                    foreach($variants as $variant) $variant_batch->add(new VariantIndexing($configurable_product, $variants, $variant, $store));
+                })->allowFailures()->onQueue('index')->dispatch();
+                $configurable_batch->add(new ConfigurableIndexing($configurable_product, $store));
+            }
+        }
         $this->info("All data imported successfully");
     }
 }
