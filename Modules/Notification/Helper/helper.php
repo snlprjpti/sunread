5bf245dfa8aefee1f87db3d97ec498e4a3a3c7ee
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-10-20 16:01:52 +0545

    condition optimize

--- Modules/Notification/Helper/helper.php
+++ Modules/Notification/Helper/helper.php
@@ -9,6 +9,7 @@ if (!function_exists('hc_include_email_template'))
 {
     function hc_include_email_template($path)
     {
-        return  ($template = SiteConfig::fetch($path, "store", config('store'))) ? $template->content : null;
+        $template = SiteConfig::fetch($path, "store", config('store'));
+        return ($template) ? $template->content : null;
     }
 }
