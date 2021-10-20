da2a40f6905f0b31a52badc0cd20f708b4566eb1
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-10-20 15:44:21 +0545

    remove unnecessary class

--- Modules/Notification/Emails/NotificationMail.php
+++ Modules/Notification/Emails/NotificationMail.php
@@ -5,7 +5,6 @@ namespace Modules\Notification\Emails;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Contracts\Queue\ShouldQueue;
 
 class NotificationMail extends Mailable
 {
