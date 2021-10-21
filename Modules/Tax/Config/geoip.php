496f2ae5cfbae75bda60c3f052101bc8d4184bcd
Anisha Khadka <aneesha,khadka@hazesoft.co>
2021-10-21 13:10:20 +0545

    geoip update

--- Modules/Tax/Config/geoip.php
+++ Modules/Tax/Config/geoip.php
@@ -4,7 +4,7 @@ return [
 
     "services" => [
         'maxmind_database' => [
-            'database_path' => storage_path('app\GeoIp\geoipcity.mmdb'),
+            'database_path' => storage_path('app/GeoIP/geoipcity.mmdb'),
             'update_url' => sprintf('https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=%s&suffix=tar.gz', env('MAXMIND_LICENSE_KEY')),
             'locales' => ['en'],
         ],
