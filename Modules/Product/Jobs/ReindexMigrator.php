4df853a631f208b0375594a8dd3df0b05244b2d3
Anisha Khadka <aneesha,khadka@hazesoft.co>
2021-10-21 14:26:08 +0545

    test code

--- Modules/Product/Jobs/ReindexMigrator.php
+++ Modules/Product/Jobs/ReindexMigrator.php
@@ -9,6 +9,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Support\Facades\Bus;
+use Modules\Core\Entities\Website;
 use Modules\Product\Entities\Product;
 
 class ReindexMigrator implements ShouldQueue
@@ -26,10 +27,30 @@ class ReindexMigrator implements ShouldQueue
         
         foreach ($chunk_products as $products)
         {
-            $batch = Bus::batch([])->onQueue("index")->dispatch();
             foreach ($products as $product)
             {
-                $batch->add(new ReIndexer($product));
+                $product_sku = $product->sku;
+                $product_sku = Bus::batch([])->onQueue("index")->dispatch();
+                $stores = Website::find($product->website_id)->channels->map(function ($channel) {
+                    return $channel->stores;
+                })->flatten(1);
+    
+                if ($product->type == "configurable") $chunk_variants = $product->variants()->with(["categories", "product_attributes", "catalog_inventories", "attribute_options_child_products"])->get()->chunk(100);
+    
+                foreach ($stores as $store)
+                {
+                    if ($product->type == "simple") $product_sku->add(new SingleIndexing($product, $store));
+                    elseif ($product->type == "configurable") {
+                        $product_sku->add(new ConfigurableIndexing($product, $store));
+                        foreach ( $chunk_variants as $chunk_variant_key => $variants )
+                        {
+                            $chunk_variant_key = Bus::batch([])->onQueue("index")->dispatch();
+                            foreach ($variants as $variant) {
+                                $chunk_variant_key->add(new VariantIndexing($product, $variants, $variant, $store));
+                            }
+                        }
+                    }
+                }
             }
         }
     }
