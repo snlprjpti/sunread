26aaf02464a463ee3c7a9228634dbc01f64fd373
Rupesh Shrestha <rupesh.shrestha@hazesoft.co>
2021-10-21 12:27:41 +0545

    work in progress

--- Modules/Product/Jobs/ReIndexer.php
+++ Modules/Product/Jobs/ReIndexer.php
@@ -21,42 +21,37 @@ class ReIndexer implements ShouldQueue
 {
     use Batchable, Dispatchable, InteractsWithQueue, Queueable, SerializesModels, HasIndexing, ConfigurableProductHandler;
 
-    public function __construct()
+    protected $product;
+
+    public function __construct(object $product)
     {
-        
+        $this->product = $product;
     }
 
     public function handle(): void
     {
         try
         {
-            $chunk_products = Product::with(["variants", "categories", "product_attributes", "catalog_inventories", "attribute_options_child_products"])->whereParentId(null)->get()->chunk(100);
-            foreach ($chunk_products as $products)
+            $product_name = $product->name;
+            $product_name.$key = Bus::batch([])->onQueue("index")->dispatch();
+
+            $stores = Website::find($product->website_id)->channels->map(function ($channel) {
+                return $channel->stores;
+            })->flatten(1);
+            
+            if ($product->type == "configurable") $chunk_variants = $product->variants()->with(["categories", "product_attributes", "catalog_inventories", "attribute_options_child_products"])->get()->chunk(100);
+            
+            foreach ($stores as $store)
             {
-                foreach ($products as $key => $product)
-                {
-                    $product_name = $product->name;
-                    $product_name.$key = Bus::batch([])->onQueue("index")->dispatch();
+                if ($product->type == "simple") $product_name.$key->add(new SingleIndexing($product, $store));
+                elseif ($product->type == "configurable") {
+                    $product_name.$key->add(new ConfigurableIndexing($product, $store));
 
-                    $stores = Website::find($product->website_id)->channels->map(function ($channel) {
-                        return $channel->stores;
-                    })->flatten(1);
-                    
-                    if ($product->type == "configurable") $chunk_variants = $product->variants()->with(["categories", "product_attributes", "catalog_inventories", "attribute_options_child_products"])->get()->chunk(100);
-                    
-                    foreach ($stores as $store)
+                    foreach ( $chunk_variants as $chunk_variant_key => $variants )
                     {
-                        if ($product->type == "simple") $product_name.$key->add(new SingleIndexing($product, $store));
-                        elseif ($product->type == "configurable") {
-                            $product_name.$key->add(new ConfigurableIndexing($product, $store));
-
-                            foreach ( $chunk_variants as $chunk_variant_key => $variants )
-                            {
-                                $key.$product_name.$chunk_variant_key = Bus::batch([])->onQueue("index")->dispatch();
-                                foreach ($variants as $variant) {
-                                    $key.$product_name.$chunk_variant_key->add(new VariantIndexing($product, $variants, $variant, $store));
-                                }
-                            }
+                        $key.$product_name.$chunk_variant_key = Bus::batch([])->onQueue("index")->dispatch();
+                        foreach ($variants as $variant) {
+                            $key.$product_name.$chunk_variant_key->add(new VariantIndexing($product, $variants, $variant, $store));
                         }
                     }
                 }
---
+++ Modules/Product/Jobs/ReindexMigrator.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Modules\Product\Jobs;
+
+use Illuminate\Bus\Batchable;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Bus;
+use Modules\Product\Entities\Product;
+
+class ReindexMigrator implements ShouldQueue
+{
+    use Batchable, Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct()
+    {
+        //
+    }
+
+    public function handle(): void
+    {
+        $chunk_products = Product::with(["variants", "categories", "product_attributes", "catalog_inventories", "attribute_options_child_products"])->whereParentId(null)->get()->chunk(100);
+        
+        foreach ($chunk_products as $products)
+        {
+            $batch = Bus::batch([])->onQueue("index")->dispatch();
+            foreach ($products as $product)
+            {
+                $batch->add(new ReIndexer($product));
+            }
+        }
+    }
+}
