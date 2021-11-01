a84820f58c3bd02819b86ce28973dba0781ea3e1
Anisha Khadka <aneesha.khadka@hazesoft.co>
2021-11-01 16:42:32 +0000

    Update ProductSearchRepository.php

--- Modules/Product/Repositories/ProductSearchRepository.php
+++ Modules/Product/Repositories/ProductSearchRepository.php
@@ -46,8 +46,8 @@ class ProductSearchRepository extends ElasticSearchRepository
             $search = [];
 
             if(isset($request->q)) {
-                $search[] = $this->queryString($this->searchKeys, $request->q);
-                foreach($this->searchKeys as $key) $search[] = $this->wildcard($key, $request->q);
+                //$search[] = $this->queryString($this->searchKeys, $request->q);
+                foreach($this->searchKeys as $key) $search[] = $this->match($key, $request->q);
             }
             $query = $this->orwhereQuery($search);
         }
