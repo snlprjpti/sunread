3cb7c8875265cacaeb9947eaf265fa2917b61f35
Rupesh Shrestha <rupesh.shrestha@hazesoft.co>
2021-11-08 19:02:12 +0545

    work in progress

--- Modules/Sales/Repositories/OrderRepository.php
+++ Modules/Sales/Repositories/OrderRepository.php
@@ -7,34 +7,21 @@ use Illuminate\Contracts\Validation\Rule;
 use Modules\Sales\Entities\Order;
 use Illuminate\Support\Facades\DB;
 use Modules\Core\Repositories\BaseRepository;
+use Modules\Coupon\Entities\Coupon;
+use Modules\Tax\Facades\TaxPrice;
 
 class OrderRepository extends BaseRepository
 {
+    protected $discount_percent;
+
     public function __construct(Order $order)
     {
         $this->model = $order;
         $this->model_key = "orders";
         $this->rules = [
-            //website_id
-            /**
-             * website_id
-             * store_id
-             * product_id
-             * shipping_method_id
-             * payment_method_id
-             * currency_code
-             * coupon_code
-             * qty
-             * ///is guest 
-             * customer_name
-             * customer_phone
-             * customer_taxVat
-             * // customer details
-             * 
-             */
-            "orders" => "required|array",
-            "*orders.product_id" => "required|exists:products,id",
-            "*orders.qty" => "required|decimal",
+            "orders" => "array",
+            "orders.*.product_id" => "required|exists:products,id",
+            "orders.*.qty" => "required|decimal",
             "coupon_code" => "sometimes|exists:coupons,code"
         ];
     }
@@ -44,7 +31,12 @@ class OrderRepository extends BaseRepository
         DB::beginTransaction();
         try
         {
-            dd($request);
+            $this->validateData($request);
+            $this->calculate($request);
+            
+
+            // dd($coupon);
+            dd($request->all());
             // validate params
             
             
@@ -59,6 +51,39 @@ class OrderRepository extends BaseRepository
 
         DB::commit();        
         return  '';
+    }
+
+    public function getTax(object $request): ?object
+    {
+        try
+        {
+            $data = '';
+        }
+        catch (Exception $exception)
+        {
+            throw $exception;
+        }
+
+        return $data;
+    }
+
+    public function calculate(object $request): ?object
+    {
+        // Discount 
+        if ($request->coupon_code) {
+            $coupon = Coupon::whereCode($request->coupon_code)->publiclyAvailable()->first();
+            if (!$coupon) throw new Exception("Coupon Expired");         
+            $this->discount_percent = $coupon->discount_percent;          
+        }
+
+        $customer_tax = TaxPrice::calculate($request, 1000, customer_tax_group_id:1);
+        dd($customer_tax);
+
+        
+
+
+
+        // Tax
     }
 
 
