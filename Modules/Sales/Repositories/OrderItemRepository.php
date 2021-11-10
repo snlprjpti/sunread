0cd9b03cf29013b2c8d7fecf9aa94f86dd6882ae
= <=>
2021-11-10 11:08:14 +0545

    sales wip

--- Modules/Sales/Repositories/OrderItemRepository.php
+++ Modules/Sales/Repositories/OrderItemRepository.php
@@ -28,10 +28,28 @@ class OrderItemRepository extends BaseRepository
         {
             $coreCache = $this->getCoreCache($request);
 
-            $row_total = ($order_item_details->price * $order_item_details->qty);
-            //$discount_amount_tax = 0;
-            $row_total_incl_tax = ( $row_total + $order_item_details->tax_rate_value );
-            $row_weight = ( $order_item_details->weight * $order_item_details->qty );
+            // $row_total = ($order_item_details->price * $order_item_details->qty);
+            // //$discount_amount_tax = 0;
+            // $row_total_incl_tax = ( $row_total + $order_item_details->tax_rate_value );
+            // $row_weight = ( $order_item_details->weight * $order_item_details->qty );
+            // dd($order_item_details);
+
+            $price = $order_item_details->price;
+            $qty = $order_item_details->qty;
+            $weight = $order_item_details->weight;
+
+            $tax_amount = $order_item_details->tax_rate_value;
+            $tax_percent = $order_item_details->tax_rate_percent;
+
+            $price_incl_tax = $price + $tax_amount;
+            $row_total = $price * $qty;
+            $row_total_incl_tax = $row_total + $tax_amount;
+            $row_weight = $weight * $qty;
+
+            $discount_amount_tax = 0.00;
+            $discount_amount = 0.00;
+            $discount_percent = 0.00;
+
             $data = [
                 "website_id" => $coreCache?->website->id,
                 "store_id" => $coreCache?->store->id,
@@ -40,21 +58,22 @@ class OrderItemRepository extends BaseRepository
                 "product_id" => $order_item_details->product_id,
                 "name" => $order_item_details->name,
                 "sku" => $order_item_details->sku,
-                "weight" => $order_item_details->weight,
-                "qty" => $order_item_details->qty,
-                "price" => $order_item_details->price,
+                "weight" => $weight,
+                "qty" => $qty,
+                "price" => $price,
                 "cost" => $order_item_details->cost,
                 "product_type" => $order_item_details->type,
                 "product_options" => json_encode($order_item_details->product_options),
 
-                "price_incl_tax" => $order_item_details->tax_rate_value,
-                "tax_amount" => $order_item_details->tax_rate_value,
+                "price_incl_tax" => $price_incl_tax,
+                "tax_amount" => $tax_amount,
 
-                "tax_percent" => $order_item_details->tax_rate_percent,
+                "tax_percent" => $tax_percent,
                 "row_total" => $row_total,
                 "row_total_incl_tax" => $row_total_incl_tax,
                 "row_weight" => $row_weight
             ];
+            
             $this->create($data);
 
         } 
