3b67ebb49955c69a486c04ca0754056c6b76b253
= <=>
2021-11-11 14:00:53 +0545

    order and order item trait wip

--- Modules/Sales/Repositories/OrderRepository.php
+++ Modules/Sales/Repositories/OrderRepository.php
@@ -3,20 +3,25 @@
 namespace Modules\Sales\Repositories;
 
 use Exception;
-use Modules\Sales\Entities\Order;
-use Illuminate\Support\Facades\DB;
-use Modules\Cart\Entities\CartItem;
-use Modules\Core\Facades\SiteConfig;
-use Modules\Core\Repositories\BaseRepository;
-use Modules\Coupon\Entities\Coupon;
-use Modules\Customer\Entities\Customer;
-use Modules\Tax\Facades\TaxPrice;
 use Modules\GeoIp\Facades\GeoIp;
+use Modules\Sales\Entities\Order;
+use Modules\Tax\Facades\TaxPrice;
+use Illuminate\Support\Facades\DB;
+use Modules\Sales\Traits\HasOrder;
+use Modules\Cart\Entities\CartItem;
+use Modules\Coupon\Entities\Coupon;
+use Modules\Core\Facades\SiteConfig;
 use Modules\Product\Entities\Product;
+use Modules\Customer\Entities\Customer;
+use Modules\Core\Repositories\BaseRepository;
 use Modules\Sales\Repositories\OrderItemRepository;
 
 class OrderRepository extends BaseRepository
 {
+    use HasOrder {
+        store as traitStore;
+    }
+
     protected $orderItemRepository, $orderAddressRepository;
 
     protected array $product_attribute_slug = [
@@ -46,53 +51,55 @@ class OrderRepository extends BaseRepository
         DB::beginTransaction();
         try
         {
-            // $this->validateData($request);
+            $this->validateData($request);
                 
             $coreCache = $this->getCoreCache($request);
-            $currency_code = SiteConfig::fetch('channel_currency', 'channel', $coreCache?->channel->id);
-            $data = [
-                "website_id" => $coreCache?->website->id,
-                "store_id" => $coreCache?->store->id,
-                "customer_id" => auth("customer")->id(),
-                "store_name" => $coreCache?->store->name,
-                "is_guest" => auth("customer")->id() ? 0 : 1,
-                "billing_address_id" => $request->billing_address_id,
-                "shipping_address_id" => $request->shipping_address_id,
-                "currency_code" => $currency_code->code,
-                "shipping_method" => $request->shipping_method ?? 'online-delivery',
-                "shipping_method_label" => $request->shipping_method_label ?? 'online-delivery',
-                "payment_method" => $request->payment_method ?? 'stripe',
-                "payment_method_label" => $request->payment_method_label ?? 'stripe',
-                "sub_total" => $request->sub_total ?? 0.00,
-                "sub_total_tax_amount" => $request->sub_total_tax_amount ?? 0.00,
-                "tax_amount" => $request->tax_amount ?? 0.00,
-                "grand_total" => $request->grand_total ?? 0.00,
-                "total_items_ordered" => $request->total_items_ordered ?? 0.00,
-                "total_qty_ordered" => $request->total_qty_ordered ?? 0.00
-            ];
+            // $currency_code = SiteConfig::fetch('channel_currency', 'channel', $coreCache?->channel->id);
+            // $data = [
+            //     "website_id" => $coreCache?->website->id,
+            //     "store_id" => $coreCache?->store->id,
+            //     "customer_id" => auth("customer")->id(),
+            //     "store_name" => $coreCache?->store->name,
+            //     "is_guest" => auth("customer")->id() ? 0 : 1,
+            //     "billing_address_id" => $request->billing_address_id,
+            //     "shipping_address_id" => $request->shipping_address_id,
+            //     "currency_code" => $currency_code->code,
+            //     "shipping_method" => $request->shipping_method ?? 'online-delivery',
+            //     "shipping_method_label" => $request->shipping_method_label ?? 'online-delivery',
+            //     "payment_method" => $request->payment_method ?? 'stripe',
+            //     "payment_method_label" => $request->payment_method_label ?? 'stripe',
+            //     "sub_total" => $request->sub_total ?? 0.00,
+            //     "sub_total_tax_amount" => $request->sub_total_tax_amount ?? 0.00,
+            //     "tax_amount" => $request->tax_amount ?? 0.00,
+            //     "grand_total" => $request->grand_total ?? 0.00,
+            //     "total_items_ordered" => $request->total_items_ordered ?? 0.00,
+            //     "total_qty_ordered" => $request->total_qty_ordered ?? 0.00
+            // ];
 
-            if ( $data['is_guest'] ) {
-                $customer_data = [
-                    "customer_email" => $request->customer_details['email'],
-                    "customer_first_name" => $request->customer_details['first_name'],
-                    "customer_middle_name" => $request->customer_details['middle_name'],
-                    "customer_last_name" => $request->customer_details['last_name'],
-                    "customer_phone" => $request->customer_details['phone'],
-                    "customer_taxvat" => $request->customer_details['taxvat'],
-                    "customer_ip_address" => GeoIp::requestIp(),
-                    "status" => "pending"
-                ];
-            }
-            $customer_data = isset($data['is_guest']) ? $customer_data : []; 
-            $data = array_merge($data, $customer_data);
+            // if ( $data['is_guest'] ) {
+            //     $customer_data = [
+            //         "customer_email" => $request->customer_details['email'],
+            //         "customer_first_name" => $request->customer_details['first_name'],
+            //         "customer_middle_name" => $request->customer_details['middle_name'],
+            //         "customer_last_name" => $request->customer_details['last_name'],
+            //         "customer_phone" => $request->customer_details['phone'],
+            //         "customer_taxvat" => $request->customer_details['taxvat'],
+            //         "customer_ip_address" => GeoIp::requestIp(),
+            //         "status" => "pending"
+            //     ];
+            // }
+            // $customer_data = isset($data['is_guest']) ? $customer_data : []; 
+            // $data = array_merge($data, $customer_data);
 
-            $order = $this->create($data, function ($order) use ($request) {
-                // $this->orderAddressRepository->store($request, $order);
-            });
+            // $order = $this->create($data, function ($order) use ($request) {
+            //     // $this->orderAddressRepository->store($request, $order);
+            // });
+
+           $order = $this->traitStore($request);
 
             $items = CartItem::whereCartId($request->cart_hash_id)->select("product_id", "qty")->get()->toArray();
 
-            foreach ( $items as $order_item ) 
+            foreach ( $items as $order_item )
             {
                 $order_item_details = $this->getProductDetail($request, $order_item, function ($product) use ($coreCache, &$tax, $request, $order_item) {
                     $tax = $this->calculateTax($request, $order_item)->toArray();
@@ -100,10 +107,7 @@ class OrderRepository extends BaseRepository
                     return array_merge($product_options, $tax, $order_item);
                 });
                 $this->orderItemRepository->store($request, $order, $order_item_details);
-                dd($order_item_details);
-            }  
-            // dd('asd');
-            
+            }
         } 
         catch (Exception $exception)
         {
---
+++ Modules/Sales/Traits/HasOrderItem.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Modules\Sales\Traits;
+
+use Exception;
+use Illuminate\Support\Facades\DB;
+use Modules\Sales\Entities\OrderTax;
+use Modules\Sales\Entities\OrderItem;
+use Modules\Sales\Entities\OrderTaxItem;
+
+trait HasOrderItem
+{
+	public function store(object $request, object $order, object $order_item_details): object
+	{
+		try
+		{
+            $coreCache = $this->getCoreCache($request);
+            $calculation = $this->calculateItems($order_item_details);
+            $data = array_merge($calculation, [
+                "website_id" => $coreCache?->website->id,
+                "store_id" => $coreCache?->store->id,
+                "order_id" => $order->id,
+                "product_id" => $order_item_details->product_id,
+                "name" => $order_item_details->name,
+                "sku" => $order_item_details->sku,
+                "cost" => $order_item_details->cost,
+                "product_type" => $order_item_details->type,
+                "product_options" => json_encode($order_item_details->product_options),
+           ]);
+           $order_item = OrderItem::create($data);
+           $this->storeOrderTax($order, $order_item_details, function ($order_tax, $rule) use ($order_item) {
+                $this->storeOrderTaxItem($order_tax, $order_item, $rule);
+                /**
+                 * @TODO::Addition 
+                 * Sum order tax again for additional order items 
+                 * Order Items can be added in callback 
+                 * ie. Sum order tax form relations and update order taxes fields
+                 * Order Tax array format [["tax_id" => $order_tax->id,".."]]
+                 */
+           });
+		}
+		catch (Exception $exception)
+		{
+			throw $exception;
+		}
+
+		return $order_item;
+	}
+
+    public function calculateItems(object $order_item_details): array
+    {
+        try
+        {
+            $price = $order_item_details->price;
+            $qty = $order_item_details->qty;
+            $weight = $order_item_details->weight;
+    
+            $tax_amount = $order_item_details->tax_rate_value;
+            $tax_percent = $order_item_details->tax_rate_percent;
+    
+            $price_incl_tax = $price + $tax_amount;
+            $row_total = $price * $qty;
+            $row_total_incl_tax = $row_total + $tax_amount;
+            $row_weight = $weight * $qty;
+    
+            $discount_amount_tax = 0.00;
+            $discount_amount = 0.00;
+            $discount_percent = 0.00;
+    
+            $data = [
+                "price" => $price,
+                "qty" => $qty,
+                "weight" => $weight,
+                "tax_amount" => $tax_amount,
+                "tax_percent" => $tax_percent,
+                "price_incl_tax" =>$price_incl_tax,
+                "row_total" =>$row_total,
+                "row_total_incl_tax" =>$row_total_incl_tax,
+                "row_weight" =>$row_weight,
+                "discount_amount_tax" =>$discount_amount_tax,
+                "discount_amount" =>$discount_amount,
+                "discount_percent" =>$discount_percent,
+            ];
+        }
+        catch ( Exception $exception )
+        {
+            throw $exception;
+        }
+        return $data;
+    }
+
+    public function storeOrderTax(object $order, object $order_item_details, ?callable $callback = null): object
+    {
+        DB::beginTransaction();
+        try
+        {
+            foreach ($order_item_details->rules as $rule)
+            {
+                $percent = array_sum($rule->rates->pluck("tax_rate")->toArray());
+                $amount = array_sum($rule->rates->pluck("tax_rate_value")->toArray());
+            
+                $data = [
+                    "order_id" => $order->id,
+                    "code" => $rule->name,
+                    "title" => $rule->name,
+                    "percent" => $percent,
+                    "amount" => $amount
+                ];
+
+                $order_tax = OrderTax::create($data);
+                if ($callback) $callback($order_tax, $rule);
+            }
+        }
+        catch ( Exception $exception )
+        {
+            DB::rollback();
+            throw $exception;
+        }
+
+        DB::commit();        
+        return $order;
+    }
+
+    public function storeOrderTaxItem(object $order_tax, object $order_item, mixed $rule, ?callable $callback = null): void
+    {
+        DB::beginTransaction();
+        try
+        {
+            $data = [];
+            foreach ($rule->rates as $rate) 
+            {
+                $data[] = [
+                    "tax_id" => $order_tax->id,
+                    "item_id" => $order_item->product_id,
+                    "tax_percent" => $rate->tax_rate,
+                    "amount" => $rate->tax_rate_value,
+                    "tax_item_type" => "product"
+                ];
+            }
+
+            if ($callback) $data = array_merge($data, $callback());
+            
+            OrderTaxItem::insert($data);  
+        }
+        catch ( Exception $exception )
+        {
+            DB::rollback();
+            throw $exception;
+        }
+        DB::commit(); 
+    }
+
+}
