3c47f728f6976241ab86a2723f2b53c5e76bbe50
Rupesh Shrestha <rupesh.shrestha@hazesoft.co>
2021-11-12 09:50:25 +0545

    work in progress

--- Modules/Sales/Jobs/OrderTaxesJob.php
+++ Modules/Sales/Jobs/OrderTaxesJob.php
@@ -3,6 +3,7 @@
 namespace Modules\Sales\Jobs;
 
 use Exception;
+use Illuminate\Bus\Batchable;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
@@ -15,7 +16,7 @@ use Modules\Sales\Traits\HasOrderProductDetail;
 
 class OrderTaxesJob implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, HasOrderProductDetail;
+    use Batchable, Dispatchable, InteractsWithQueue, Queueable, SerializesModels, HasOrderProductDetail;
 
     protected $order_item_details, $order;
 
@@ -30,12 +31,7 @@ class OrderTaxesJob implements ShouldQueue
         try
         {
             $this->storeOrderTax(function ($order_tax, $order_item_details, $rule) {
-
-                $order_tax_amount = $order_tax->amount;
-                $order_item_tax = $this->storeOrderTaxItem($order_tax, $order_item_details, $rule);
-                $order_item_tax_amount = $order_item_tax->amount;
-                $order_tax_amount = ($order_item_tax_amount + $order_tax_amount);
-                $order_tax->update(["amount" => $order_tax_amount]);
+                $this->storeOrderTaxItem($order_tax, $order_item_details, $rule);
             });
         }
         catch(Exception $exception)
@@ -44,20 +40,19 @@ class OrderTaxesJob implements ShouldQueue
         }
     }
 
-    public function storeOrderTax(?callable $callback = null): void
+    private function storeOrderTax(?callable $callback = null): void
     {
         DB::beginTransaction();
         try
         {
             foreach ($this->order_item_details->rules as $rule)
             {
-                $amount = (($this->order_item_details->tax_rate_percent/100) * $this->order_item_details->price * $this->order_item_details->qty);
                 $data = [
                     "order_id" => $this->order->id,
                     "code" => \Str::slug($rule->name),
                     "title" => $rule->name,
                     "percent" => $this->order_item_details->tax_rate_percent,
-                    "amount" => $amount
+                    "amount" => 0
                 ];
                 $match = $data;
                 unset($match["title"], $match["percent"], $match["amount"]);
@@ -74,17 +69,16 @@ class OrderTaxesJob implements ShouldQueue
         DB::commit();
     }
 
-    public function storeOrderTaxItem(object $order_tax, object $order_item_details, mixed $rule): object
+    private function storeOrderTaxItem(object $order_tax, object $order_item_details, mixed $rule): object
     {
         DB::beginTransaction();
         try
         {
-
             $data = [
                 "tax_id" => $order_tax->id,
                 "item_id" => $order_item_details->product_id,
                 "tax_percent" => $order_tax->percent,
-                "amount" => $rule->rates?->pluck("tax_rate_value")->first(),
+                "amount" => ($rule->rates?->pluck("tax_rate_value")->first() * $order_item_details->qty),
                 "tax_item_type" => "product"
             ];            
             $order_tax_item = OrderTaxItem::create($data);  
