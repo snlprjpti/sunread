b2ad6067bed9eb0b56dd5dd6110686b5fb973d75
Ayush Oli <ayush.oli@hazesoft.co>
2021-11-15 18:20:39 +0545

    Table changed for type in clubhouse

--- Modules/ClubHouse/Repositories/ClubHouseRepository.php
+++ Modules/ClubHouse/Repositories/ClubHouseRepository.php
@@ -33,7 +33,6 @@ class ClubHouseRepository extends BaseRepository
         $this->rules = [
             // ClubHouse validation
             "website_id" => "required|exists:websites,id",
-            "type" => "required|string|in:clubhouse,resort",
             "position" => "required|integer",
         ];
 
