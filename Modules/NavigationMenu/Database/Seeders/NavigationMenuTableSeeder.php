92105b628fc718df77c5c2e163edfd1d2e077211
Anisha Khadka <aneesha,khadka@hazesoft.co>
2021-11-16 14:33:46 +0545

    seeder for navigation

--- Modules/NavigationMenu/Database/Seeders/NavigationMenuTableSeeder.php
+++ Modules/NavigationMenu/Database/Seeders/NavigationMenuTableSeeder.php
@@ -4,14 +4,12 @@ namespace Modules\NavigationMenu\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Modules\NavigationMenu\Entities\NavigationMenu;
 
 class NavigationMenuTableSeeder extends Seeder
 {
-
     public function run(): void
     {
-        Model::unguard();
-
-        // $this->call("OthersTableSeeder");
+        NavigationMenu::factory()->create();
     }
 }
