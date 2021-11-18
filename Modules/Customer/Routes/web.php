f4707f8f35a231be2f5e72d47b817fe8ac05eadd
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-11-18 22:08:29 +0545

    route and test update

--- Modules/Customer/Routes/web.php
+++ Modules/Customer/Routes/web.php
@@ -3,6 +3,9 @@
 use Modules\Customer\Http\Controllers\ResetPasswordController;
 use Modules\Customer\Http\Controllers\VerificationController;
 
-Route::get("/{channel_code}/{store_code}/verify-account/{token}", [VerificationController::class, "verifyAccount"])->name("account-verify");
+Route::group(["as" => "customers."],function () {
 
-Route::get("/{channel_code}/{store_code}/reset-password/{token}", [ResetPasswordController::class, "create"])->name("customers.reset-password.create");
+    Route::get("/{channel_code}/{store_code}/verify-account/{token}", [VerificationController::class, "verifyAccount"])->name("account-verify");
+
+    Route::get("/{channel_code}/{store_code}/reset-password/{token}", [ResetPasswordController::class, "create"])->name("reset-password.create");
+});
