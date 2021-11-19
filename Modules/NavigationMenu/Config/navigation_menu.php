21cdb95c862e64913f39fe8196a7eb8662f39321
Ayush Oli <ayush.oli@hazesoft.co>
2021-11-19 12:02:57 +0545

    Dynamic link and Category Id fixed for nav menu items

--- Modules/NavigationMenu/Config/navigation_menu.php
+++ Modules/NavigationMenu/Config/navigation_menu.php
@@ -23,24 +23,39 @@ return [
                     "options" => [
                         [ "value" => "category", "label" => "Category" ],
                         [ "value" => "page", "label" => "Page" ],
-                        [ "value" => "custom", "label" => "Custom" ]
+                        [ "value" => "custom", "label" => "Custom" ],
+                        [ "value" => "dynamic", "label" => "Dynamic Link" ],
                     ],
                     "has_condition" => 1,
-                    "rules" => "string|in:category,page,custom",
+                    "rules" => "string|in:category,page,custom,dynamic",
                     "is_required" => 1
                 ],
                 [
-                    "title" => "Type Id",
-                    "slug" => "type_id",
+                    "title" => "Page ID",
+                    "slug" => "page_id",
                     "type" => "text",
                     "value" => "",
                     "scope" => "website",
                     "condition" => [
                         "field" => 'type',
-                        "value" => 'category,page'
+                        "value" => 'page'
                     ],
                     "options" => [],
-                    "rules" => "required_if:items.type.value,page,category|integer",
+                    "rules" => "required_if:items.type.value,page|integer",
+                    "is_required" => 0
+                ],
+                [
+                    "title" => "Category ID",
+                    "slug" => "category_id",
+                    "type" => "text",
+                    "value" => "",
+                    "scope" => "website",
+                    "condition" => [
+                        "field" => 'type',
+                        "value" => 'category'
+                    ],
+                    "options" => [],
+                    "rules" => "required_if:items.type.value,category|integer",
                     "is_required" => 0
                 ],
                 [
@@ -55,6 +70,20 @@ return [
                     ],
                     "options" => [],
                     "rules" => "required_if:items.type.value,custom|string",
+                    "is_required" => 0
+                ],
+                [
+                    "title" => "Dynamic Link",
+                    "slug" => "dynamic_link",
+                    "type" => "text",
+                    "value" => "",
+                    "scope" => "website",
+                    "condition" => [
+                        "field" => 'type',
+                        "value" => 'dynamic'
+                    ],
+                    "options" => [],
+                    "rules" => "required_if:items.type.value,dynamic|string",
                     "is_required" => 0
                 ],
                 [
@@ -78,7 +107,7 @@ return [
                         [ "value" => "0", "label" => "Disabled" ]
                     ],
                     "rules" => "in:0,1",
-                    "is_required" => 0
+                    "is_required" => 1
                 ],
             ]
         ],
@@ -162,10 +191,12 @@ return [
     "absolute_path" => [
         "title" => "general.elements.0",
         "type" => "general.elements.1",
-        "type_id" => "general.elements.2",
-        "custom_link" => "general.elements.3",
-        "order" => "general.elements.4",
-        "status" => "general.elements.5",
+        "page_id" => "general.elements.2",
+        "category_id" => "general.elements.3",
+        "custom_link" => "general.elements.4",
+        "dynamic_link" => "general.elements.5",
+        "order" => "general.elements.6",
+        "status" => "general.elements.7",
         "background_type" => "additional_data.elements.0",
         "background_image" => "additional_data.elements.1",
         "background_video_type" => "additional_data.elements.2",
