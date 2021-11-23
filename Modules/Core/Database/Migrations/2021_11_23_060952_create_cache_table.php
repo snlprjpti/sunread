027de059e4b865b694b9905ee1e4be4c1cdbb31b
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2021-11-23 13:08:48 +0545

    renamed table name

--- Modules/Core/Database/Migrations/2021_11_23_060952_create_cache_table.php
+++ Modules/Core/Database/Migrations/2021_11_23_060952_create_cache_table.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCacheManagementTable extends Migration
+class CreateCacheTable extends Migration
 {
     public function up(): void
     {
-        Schema::create('cache_management', function (Blueprint $table) {
+        Schema::create('cache', function (Blueprint $table) {
             $table->id();
             $table->string("name");
             $table->string("slug")->unique();
@@ -21,6 +21,6 @@ class CreateCacheManagementTable extends Migration
 
     public function down(): void
     {
-        Schema::dropIfExists('cache_management');
+        Schema::dropIfExists('cache');
     }
 }
