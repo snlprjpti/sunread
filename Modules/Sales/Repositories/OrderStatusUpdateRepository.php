272752d8a133c869d955f8a509d4724ca76a9ea9
= <=>
2021-11-26 18:16:10 +0545

    adjusted by added

--- Modules/Sales/Repositories/OrderStatusUpdateRepository.php
+++ Modules/Sales/Repositories/OrderStatusUpdateRepository.php
@@ -43,7 +43,7 @@ class OrderStatusUpdateRepository extends BaseRepository
                             "product_id" => $item->product_id,
                             "website_id" => $item->website_id,
                             "event" => "{$this->model_key}.order_status_updated",
-                            "adjustment_type" => "deduction",
+                            "adjustment_type" => auth("admin")->id(),
                             "order_id" => $request->order_id,
                             "quantity" => $item->qty
                         ]);
