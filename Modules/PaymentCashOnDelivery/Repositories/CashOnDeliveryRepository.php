d09c29af056f3b12337da724f90fa27ffcb80ac3
= <=>
2021-12-01 09:46:27 +0545

    review intentation updated

--- Modules/PaymentCashOnDelivery/Repositories/CashOnDeliveryRepository.php
+++ Modules/PaymentCashOnDelivery/Repositories/CashOnDeliveryRepository.php
@@ -11,50 +11,40 @@ use Modules\CheckOutMethods\Repositories\BasePaymentMethodRepository;
 
 class CashOnDeliveryRepository extends BasePaymentMethodRepository implements PaymentMethodInterface
 {
-	protected object $request;
-	protected object $parameter;
-	protected string $method_key;
+    protected object $request;
+    protected object $parameter;
+    protected string $method_key;
 
-	public function __construct(object $request, object $parameter)
-	{
-		$this->request = $request;
-		$this->method_key = "cash_on_delivery";
-		$this->parameter = $parameter;
+    public function __construct(object $request, object $parameter)
+    {
+        $this->request = $request;
+        $this->method_key = "cash_on_delivery";
+        $this->parameter = $parameter;
 
-		parent::__construct($this->request, $this->method_key);
-	}
+        parent::__construct($this->request, $this->method_key);
+    }
 
-	public function get(): mixed
-	{
-		try 
-		{
+    public function get(): mixed
+    {
+        try 
+        {
             $coreCache = $this->getCoreCache();
-			$channel_id = $coreCache?->channel->id;
-			$minimum_order_total = SiteConfig::fetch("payment_methods_{$this->method_key}_minimum_total_order", "channel", $channel_id);
+            $channel_id = $coreCache?->channel->id;
+            $minimum_order_total = SiteConfig::fetch("payment_methods_{$this->method_key}_minimum_total_order", "channel", $channel_id);
             $maximum_order_total = SiteConfig::fetch("payment_methods_{$this->method_key}_maximum_total_order", "channel", $channel_id);
-			if (($this->parameter->sub_total_tax_amount < $minimum_order_total) || ($this->parameter->sub_total_tax_amount > $maximum_order_total)) {
+            if (($this->parameter->sub_total_tax_amount < $minimum_order_total) || ($this->parameter->sub_total_tax_amount > $maximum_order_total)) {
                 throw new CashOnDeliveryNotAllowedException("Total order must be between {$minimum_order_total} & {$maximum_order_total}", 403);
-			}
+            }
 
-			$this->parameter->order->update([
-				"payment_method" => $this->method_key,
-            	"payment_method_label" => SiteConfig::fetch("payment_methods_{$this->method_key}_title", "channel", $channel_id)
-			]);
-		}
-		catch ( Exception $exception )
+            $this->parameter->order->update([
+                "payment_method" => $this->method_key,
+                "payment_method_label" => SiteConfig::fetch("payment_methods_{$this->method_key}_title", "channel", $channel_id)
+            ]);
+        }
+        catch ( Exception $exception )
         {
             throw $exception;
         }
-		return true;
-	}
-
-
-
-
-
-	
-
-
-
-
+        return true;
+    }
 }
