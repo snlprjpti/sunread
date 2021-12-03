c98d3d7fb2c39f73e940ed092919f228e4d8b94b
Anisha Khadka <aneesha,khadka@hazesoft.co>
2021-12-03 14:12:28 +0545

    extra fields added on category

--- Modules/Category/Config/category.php
+++ Modules/Category/Config/category.php
@@ -7,20 +7,26 @@ return [
                 [
                     "title" => "Name",
                     "slug" => "name",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "text",
                     "value" => "",
                     "scope" => "store",
                     "options" => [],
+                    "conditions" => [],
                     "rules" => "string",
                     "is_required" => 1
                 ],
                 [
                     "title" => "Slug",
                     "slug" => "slug",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "text",
                     "value" => "",
                     "scope" => "store",
                     "options" => [],
+                    "conditions" => [],
                     "rules" => "string",
                     "is_required" => 0
 
@@ -28,26 +34,34 @@ return [
                 [
                     "title" => "Status",
                     "slug" => "status",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "select",
+                    "multiple" => false,
                     "value" => "",
                     "scope" => "store",
                     "options" => [
                         [ "value" => "1", "label" => "Enabled" ],
                         [ "value" => "0", "label" => "Disabled" ]
                     ],
+                    "conditions" => [],
                     "rules" => "nullable|in:0,1",
                     "is_required" => 0
                 ],
                 [
                     "title" => "Include In Menu",
                     "slug" => "include_in_menu",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "select",
+                    "multiple" => false,
                     "value" => "",
                     "scope" => "store",
                     "options" => [
                         [ "value" => "1", "label" => "Yes" ],
                         [ "value" => "0", "label" => "No" ]
                     ],
+                    "conditions" => [],
                     "rules" => "nullable|in:0,1",
                     "is_required" => 0
 
@@ -60,20 +74,26 @@ return [
                 [
                     "title" => "Image",
                     "slug" => "image",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "file",
                     "value" => "",
                     "scope" => "store",
                     "options" => [],
+                    "conditions" => [],
                     "rules" => "nullable|mimes:jpeg,jpg,bmp,png",
                     "is_required" => 0
                 ],
                 [
                     "title" => "Description",
                     "slug" => "description",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "textarea",
                     "value" => "",
                     "scope" => "store",
                     "options" => [],
+                    "conditions" => [],
                     "rules" => "nullable|string",
                     "is_required" => 0
 
@@ -86,13 +106,17 @@ return [
                 [
                     "title" => "Background Type",
                     "slug" => "background_type",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "select",
+                    "multiple" => false,
                     "value" => "",
                     "scope" => "store",
                     "options" => [
                         [ "value" => "image", "label" => "Image" ],
                         [ "value" => "video", "label" => "Video" ]
                     ],
+                    "conditions" => [],
                     "rules" => "in:image,video",
                     "multiple" => false,
                     "is_required" => 0
@@ -100,70 +124,91 @@ return [
                 [
                     "title" => "Background Image",
                     "slug" => "background_image",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "file",
                     "value" => "",
                     "scope" => "store",
                     "options" => [],
+                    "conditions" => [],
                     "rules" => "mimes:jpeg,jpg,bmp,png",
                     "is_required" => 0
                 ],
                 [
                     "title" => "Youtube Link",
                     "slug" => "youtube_link",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "text",
                     "value" => "",
                     "scope" => "store",
                     "options" => [],
+                    "conditions" => [],
                     "rules" => "string",
                     "is_required" => 0
                 ],
                 [
                     "title" => "Gradient Color",
                     "slug" => "gradient_color",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "text",
                     "value" => "",
                     "scope" => "store",
                     "options" => [],
+                    "conditions" => [],
                     "rules" => "string",
                     "is_required" => 0
                 ],
                 [
                     "title" => "Title",
                     "slug" => "hero_banner_title",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "text",
                     "value" => "",
                     "scope" => "store",
                     "options" => [],
+                    "conditions" => [],
                     "rules" => "string",
                     "is_required" => 0
                 ],
                 [
                     "title" => "Content",
                     "slug" => "hero_banner_content",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "text",
                     "value" => "",
                     "scope" => "store",
                     "options" => [],
+                    "conditions" => [],
                     "rules" => "string",
                     "is_required" => 0
                 ],
                 [
                     "title" => "Readmore Label",
                     "slug" => "readmore_label",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "text",
                     "value" => "",
                     "scope" => "store",
                     "options" => [],
+                    "conditions" => [],
                     "rules" => "string",
                     "is_required" => 0
                 ],
                 [
                     "title" => "Readmore Link",
                     "slug" => "readmore_link",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "text",
                     "value" => "",
                     "scope" => "store",
                     "options" => [],
+                    "conditions" => [],
                     "rules" => "string",
                     "is_required" => 0
                 ],
@@ -175,40 +220,52 @@ return [
                 [
                     "title" => "First Image",
                     "slug" => "usp_banner_1_first_image",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "file",
                     "value" => "",
                     "scope" => "store",
                     "options" => [],
+                    "conditions" => [],
                     "rules" => "mimes:jpeg,jpg,bmp,png",
                     "is_required" => 0
                 ],
                 [
                     "title" => "Second Image",
                     "slug" => "usp_banner_1_second_image",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "file",
                     "value" => "",
                     "scope" => "store",
                     "options" => [],
+                    "conditions" => [],
                     "rules" => "mimes:jpeg,jpg,bmp,png",
                     "is_required" => 0
                 ],
                 [
                     "title" => "Third Image",
                     "slug" => "usp_banner_1_third_image",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "file",
                     "value" => "",
                     "scope" => "store",
                     "options" => [],
+                    "conditions" => [],
                     "rules" => "mimes:jpeg,jpg,bmp,png",
                     "is_required" => 0
                 ],
                 [
                     "title" => "Placement",
                     "slug" => "usp_banner_1_placement",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "number",
                     "value" => "",
                     "scope" => "store",
                     "options" => [],
+                    "conditions" => [],
                     "rules" => "integer",
                     "is_required" => 0
                 ],
@@ -220,40 +277,52 @@ return [
                 [
                     "title" => "First Image",
                     "slug" => "usp_banner_2_first_image",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "file",
                     "value" => "",
                     "scope" => "store",
                     "options" => [],
+                    "conditions" => [],
                     "rules" => "mimes:jpeg,jpg,bmp,png",
                     "is_required" => 0
                 ],
                 [
                     "title" => "Second Image",
                     "slug" => "usp_banner_2_second_image",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "file",
                     "value" => "",
                     "scope" => "store",
                     "options" => [],
+                    "conditions" => [],
                     "rules" => "mimes:jpeg,jpg,bmp,png",
                     "is_required" => 0
                 ],
                 [
                     "title" => "Third Image",
                     "slug" => "usp_banner_2_third_image",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "file",
                     "value" => "",
                     "scope" => "store",
                     "options" => [],
+                    "conditions" => [],
                     "rules" => "mimes:jpeg,jpg,bmp,png",
                     "is_required" => 0
                 ],
                 [
                     "title" => "Placement",
                     "slug" => "usp_banner_2_placement",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "number",
                     "value" => "",
                     "scope" => "store",
                     "options" => [],
+                    "conditions" => [],
                     "rules" => "integer",
                     "is_required" => 0
                 ],
@@ -265,40 +334,52 @@ return [
                 [
                     "title" => "First Image",
                     "slug" => "usp_banner_3_first_image",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "file",
                     "value" => "",
                     "scope" => "store",
                     "options" => [],
+                    "conditions" => [],
                     "rules" => "mimes:jpeg,jpg,bmp,png",
                     "is_required" => 0
                 ],
                 [
                     "title" => "Second Image",
                     "slug" => "usp_banner_3_second_image",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "file",
                     "value" => "",
                     "scope" => "store",
                     "options" => [],
+                    "conditions" => [],
                     "rules" => "mimes:jpeg,jpg,bmp,png",
                     "is_required" => 0
                 ],
                 [
                     "title" => "Third Image",
                     "slug" => "usp_banner_3_third_image",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "file",
                     "value" => "",
                     "scope" => "store",
                     "options" => [],
+                    "conditions" => [],
                     "rules" => "mimes:jpeg,jpg,bmp,png",
                     "is_required" => 0
                 ],
                 [
                     "title" => "Placement",
                     "slug" => "usp_banner_3_placement",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "number",
                     "value" => "",
                     "scope" => "store",
                     "options" => [],
+                    "conditions" => [],
                     "rules" => "integer",
                     "is_required" => 0
                 ],
@@ -310,10 +391,13 @@ return [
                 [
                     "title" => "Meta Title",
                     "slug" => "meta_title",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "text",
                     "value" => "",
                     "scope" => "store",
                     "options" => [],
+                    "conditions" => [],
                     "rules" => "nullable",
                     "is_required" => 0
 
@@ -321,10 +405,13 @@ return [
                 [
                     "title" => "Meta KeyWords",
                     "slug" => "meta_keywords",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "text",
                     "value" => "",
                     "scope" => "store",
                     "options" => [],
+                    "conditions" => [],
                     "rules" => "nullable",
                     "is_required" => 0
 
@@ -332,13 +419,104 @@ return [
                 [
                     "title" => "Meta Description",
                     "slug" => "meta_description",
+                    "provider" => "",
+                    "pluck" => [],
                     "type" => "textarea",
                     "value" => "",
                     "scope" => "store",
                     "options" => [],
+                    "conditions" => [],
                     "rules" => "nullable",
                     "is_required" => 0
 
+                ]
+            ]
+        ],
+        "layout" => [
+            "title" => "Layout",
+            "elements" => [
+                [
+                    "title" => "Layout Type",
+                    "slug" => "layout_type",
+                    "provider" => "",
+                    "pluck" => [],
+                    "type" => "select",
+                    "multiple" => false,
+                    "value" => "",
+                    "scope" => "store",
+                    "options" => [
+                        [ "value" => "single", "label" => "Single" ],
+                        [ "value" => "multiple", "label" => "Multiple" ]
+                    ],
+                    "conditions" => [],
+                    "rules" => "in:single,multiple",
+                    "is_required" => 1
+                ],
+                [
+                    "title" => "Categories",
+                    "slug" => "categories",
+                    "provider" => "Modules\Category\Entities\Category",
+                    "pluck" => [],
+                    "type" => "select",
+                    "multiple" => true,
+                    "value" => "",
+                    "scope" => "store",
+                    "options" => [],
+                    "conditions" => [
+                        "operator"	=> "AND",
+                        "condition"	=> [
+                            [
+                                "layout_type" => "multiple"
+                            ]
+                        ]
+                    ],
+                    "rules" => "exists:categories,id",
+                    "is_required" => 1
+                ],
+                [
+                    "title" => "No of Items",
+                    "slug" => "no_of_items",
+                    "provider" => "",
+                    "pluck" => [],
+                    "type" => "text",
+                    "value" => "",
+                    "scope" => "store",
+                    "options" => [],
+                    "conditions" => [
+                        "operator"	=> "AND",
+                        "condition"	=> [
+                            [
+                                "layout_type" => "multiple"
+                            ]
+                        ]
+                    ],
+                    "rules" => "integer",
+                    "is_required" => 1
+
+                ],
+                [
+                    "title" => "Pagination",
+                    "slug" => "pagination",
+                    "provider" => "",
+                    "pluck" => [],
+                    "type" => "select",
+                    "multiple" => false,
+                    "value" => "",
+                    "scope" => "store",
+                    "options" => [
+                        [ "value" => "1", "label" => "Yes" ],
+                        [ "value" => "0", "label" => "No" ]
+                    ],
+                    "conditions" => [
+                        "operator"	=> "AND",
+                        "condition"	=> [
+                            [
+                                "layout_type" => "multiple"
+                            ]
+                        ]
+                    ],
+                    "rules" => "in:1,0",
+                    "is_required" => 1
                 ]
             ]
         ],
@@ -372,6 +550,10 @@ return [
         "usp_banner_3_placement" => "usp_banner_3.elements.3",
         "meta_title" => "search_engine_optimization.elements.0",
         "meta_keywords" => "search_engine_optimization.elements.1",
-        "meta_description" => "search_engine_optimization.elements.2"
+        "meta_description" => "search_engine_optimization.elements.2",
+        "layout_type" => "layout.elements.0",
+        "categories" => "layout.elements.1",
+        "no_of_items" => "layout.elements.2",   
+        "pagination" => "layout.elements.3"
     ]
 ];
