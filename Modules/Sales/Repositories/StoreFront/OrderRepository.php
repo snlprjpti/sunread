c30f384389d428e27d3de17ff69bb4653a19fb68
Rupesh <rupesh.shrestha@hazesoft.co>
2021-12-17 19:03:33 +0545

    added prams to resolver fn

--- Modules/Sales/Repositories/StoreFront/OrderRepository.php
+++ Modules/Sales/Repositories/StoreFront/OrderRepository.php
@@ -178,7 +178,7 @@ class OrderRepository extends BaseRepository
         $check_out_resolver = new CheckOutProcessResolver($request);
         $check_out_methods = $check_out_resolver->getCheckOutMethods(callback:function ($methods, $check_out_method) use ($check_out_resolver) {
             if ($check_out_method == "delivery_methods") {
-                $check_custom_handler = $check_out_resolver->allow_custom_checkout();
+                $check_custom_handler = $check_out_resolver->allow_custom_checkout("delivery_methods");
                 if ($check_custom_handler) $methods = [ [ "slug" => "proxy_checkout_method", "title" => "Proxy Checkout Method", "visible" => false ] ];
             }
             return $methods;
