4ba9371cf64d507407e0bcfdee516b99e358bded
Rupesh <rupesh.shrestha@hazesoft.co>
2021-12-20 16:21:56 +0545

    added try catch

--- Modules/Sales/Repositories/StoreFront/OrderRepository.php
+++ Modules/Sales/Repositories/StoreFront/OrderRepository.php
@@ -165,12 +165,19 @@ class OrderRepository extends BaseRepository
 
     public function updateOrderTax(object $order, object $request): void
     {
-        $order->order_taxes()->get()->map( function ($order_tax) {
-            $order_tax_item_amount = $order_tax->order_tax_items()->get()->map( function ($order_item) {
-                return $order_item->amount;
-            })->toArray();
-            $order_tax->update(["amount" => array_sum($order_tax_item_amount)]);
-        });
+        try
+        {
+            $order->order_taxes()->get()->map( function ($order_tax) {
+                $order_tax_item_amount = $order_tax->order_tax_items()->get()->map( function ($order_item) {
+                    return $order_item->amount;
+                })->toArray();
+                $order_tax->update(["amount" => array_sum($order_tax_item_amount)]);
+            });
+        }
+        catch (Exception $exception)
+        {
+            throw $exception;
+        }
     }
 
     public function getMethodList(object $request): mixed
