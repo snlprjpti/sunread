af1049cd65af1f00fbc81f2fea7f43eb7074a801
Rupesh <rupesh.shrestha@hazesoft.co>
2021-12-20 13:49:56 +0545

    updated postal code typo

--- Modules/PaymentKlarna/Repositories/KlarnaRepository.php
+++ Modules/PaymentKlarna/Repositories/KlarnaRepository.php
@@ -268,7 +268,7 @@ class KlarnaRepository extends BasePaymentMethodRepository implements PaymentMet
                 "email" => $address->email,
                 "street_address" => $address->address_line_1,
                 "street_address2" => $address->address_line_2,
-                "postal_code" => $address->postal_code,
+                "postal_code" => $address->postcpde,
                 "city" => $city,
                 "region" => $region,
                 "phone" => $address->phone,
