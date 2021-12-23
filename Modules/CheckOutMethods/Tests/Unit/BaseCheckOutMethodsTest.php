10bff6bbb7ec6fb3089a8c1586e9a7af56f9537f
Rupesh <rupesh.shrestha@hazesoft.co>
2021-12-23 17:12:27 +0545

    updated test case

--- Modules/CheckOutMethods/Tests/Unit/BaseCheckOutMethodsTest.php
+++ Modules/CheckOutMethods/Tests/Unit/BaseCheckOutMethodsTest.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Arr;
 use Mockery\MockInterface;
 use Modules\CheckOutMethods\Services\BaseCheckOutMethods;
 use Modules\Core\Tests\BaseUnitTestCase;
+use Modules\Sales\Entities\Order;
 
 class BaseCheckOutMethodsTest extends BaseUnitTestCase
 {
@@ -34,22 +35,16 @@ class BaseCheckOutMethodsTest extends BaseUnitTestCase
         $this->methods = ["delivery_methods", "payment_methods"];
     }
 
-    public function test_has_class_attributes(): void
+    public function test_class_attributes_should_be_initilized(): void
     {
         foreach ($this->class_attributes as $attribute) $this->assertClassHasAttribute($attribute, $this->class);
     }
 
-    // public function test_class_has_all_method(): void
-    // {
-    //     $method_list = $this->base_checkout_method_class->all();
-    //     dd($method_list);
-    //     $this->assertArrayHasKey("delivery_methods", $method_list);
-    //     $this->assertArrayHasKey("payment_methods", $method_list);
-    //     foreach ($method_list as $method) {
-    //         dd($method);
-            
-    //     }
-    // }
+    public function test_class_all_method_should_return_valid_data(): void
+    {
+        $data = $this->base_checkout_method_class->all();
+        $this->assertIsObject($data);
+    }
 
     public function test_class_get_method_should_return_valid_data(): void
     {
@@ -65,26 +60,6 @@ class BaseCheckOutMethodsTest extends BaseUnitTestCase
         $data = $this->base_checkout_method_class->fetch($method);
         $this->assertIsObject($data);
         foreach (["title", "slug", "check_out_method", "repository"] as $attribute) $this->assertArrayHasKey($attribute, $data->toArray());
-    }
-
-    public function test_class_process_method_should_return_valid_data(): void
-    {
-        $request = Request::create("/", "GET", ["shipping_methods" => "flat_rate"]);
-        dd($this->createRequest());
-        $request->headers->add(["hc-host" => "", ]);
-        dd($request->header("hc-host"));
-
-        // $valid = "bank_transfer";
-        // $request = $this->mock(Request::class, static function (MockInterface $mock) use ($valid): void {
-        //     $mock->shouldReceive('validated')->once()->andReturn($valid);
-        // });
-        $this->assertInstanceOf(MockInterface::class, resolve(Request::class));
-        dd("asd");
-
-        // $request->payment_method = Arr::random(["klarna", "bank_transfer", "cash_on_delivery"]);
-        dd($request);
-        
-        // $data = $this->base_checkout_method_class->process(, );
     }
 
 
