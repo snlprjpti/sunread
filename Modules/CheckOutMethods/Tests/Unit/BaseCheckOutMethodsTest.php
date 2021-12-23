a230f553141f318a5589e80124ed11ceb477ad4a
Rupesh <rupesh.shrestha@hazesoft.co>
2021-12-23 14:29:19 +0545

    work in progress

--- Modules/CheckOutMethods/Tests/Unit/BaseCheckOutMethodsTest.php
+++ Modules/CheckOutMethods/Tests/Unit/BaseCheckOutMethodsTest.php
@@ -7,9 +7,13 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithoutEvents;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Mockery\MockInterface;
 use Modules\CheckOutMethods\Services\BaseCheckOutMethods;
+use Modules\Core\Tests\BaseUnitTestCase;
 
-class BaseCheckOutMethodsTest extends TestCase
+class BaseCheckOutMethodsTest extends BaseUnitTestCase
 {
     protected object $base_checkout_method_class;
     protected string $class;
@@ -35,20 +39,53 @@ class BaseCheckOutMethodsTest extends TestCase
         foreach ($this->class_attributes as $attribute) $this->assertClassHasAttribute($attribute, $this->class);
     }
 
-    public function test_class_has_all_method(): void
-    {
-        $method_list = $this->base_checkout_method_class->all();
-        dd($method_list);
-        $this->assertArrayHasKey("delivery_methods", $method_list);
-        $this->assertArrayHasKey("payment_methods", $method_list);
-        foreach ($method_list as $method) {
-            dd($method);
+    // public function test_class_has_all_method(): void
+    // {
+    //     $method_list = $this->base_checkout_method_class->all();
+    //     dd($method_list);
+    //     $this->assertArrayHasKey("delivery_methods", $method_list);
+    //     $this->assertArrayHasKey("payment_methods", $method_list);
+    //     foreach ($method_list as $method) {
+    //         dd($method);
             
-        }
+    //     }
+    // }
+
+    public function test_class_get_method_should_return_valid_data(): void
+    {
+        $checkout_method = Arr::random(["payment_methods", "delivery_methods"]);
+        $data = $this->base_checkout_method_class->get($checkout_method);
+        $this->assertIsObject($data);
+        foreach ( ["title", "slug", "check_out_method", "repository"] as $attribute) $this->assertArrayHasKey($attribute, $data->first());
     }
 
-    public function test_class_get_method_has_keys(): void
+    public function test_class_fetch_method_should_return_valid_data(): void
     {
-        $method = 
+        $method = Arr::random(["bank_transfer", "flat_rate", "free_shipping"]);
+        $data = $this->base_checkout_method_class->fetch($method);
+        $this->assertIsObject($data);
+        foreach (["title", "slug", "check_out_method", "repository"] as $attribute) $this->assertArrayHasKey($attribute, $data->toArray());
     }
+
+    public function test_class_process_method_should_return_valid_data(): void
+    {
+        $request = Request::create("/", "GET", ["shipping_methods" => "flat_rate"]);
+        dd($this->createRequest());
+        $request->headers->add(["hc-host" => "", ]);
+        dd($request->header("hc-host"));
+
+        // $valid = "bank_transfer";
+        // $request = $this->mock(Request::class, static function (MockInterface $mock) use ($valid): void {
+        //     $mock->shouldReceive('validated')->once()->andReturn($valid);
+        // });
+        $this->assertInstanceOf(MockInterface::class, resolve(Request::class));
+        dd("asd");
+
+        // $request->payment_method = Arr::random(["klarna", "bank_transfer", "cash_on_delivery"]);
+        dd($request);
+        
+        // $data = $this->base_checkout_method_class->process(, );
+    }
+
+
 }
--- Modules/Core/Tests/BaseUnitTestCase.php
+++ Modules/Core/Tests/BaseUnitTestCase.php
@@ -7,15 +7,62 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithoutEvents;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Modules\Core\Entities\Channel;
+use Modules\Core\Entities\Store;
+use Modules\Core\Entities\Website;
 
 class BaseUnitTestCase extends TestCase
 {
     use WithoutMiddleware, WithoutEvents;
     
-    
+    public array $headers;
+    public array $request_data;
+
     public function setUp(): void
     {
+        parent::setUp();
+        $this->headers = $this->createHeader();
 
+        $this->hasWebsiteHost = true;
+        $this->hasChannel = true;
+        $this->hasStore = true;
+    }
+
+    public function createHeader(): void
+    {
+        $this->website = Website::factory()->create();
+
+        if ($this->hasWebsiteHost) $this->headers["hc-host"] = $this->website->hostname;
+        if ($this->hasChannel) {
+            $this->channel = $this->createChannel();
+            $this->headers["hc-channel"] = $this->channel->code;
+        } 
+        if ($this->hasStore) {
+            $this->channel = $this->channel ?? $this->createChannel();
+            $this->store = $this->createStore();
+            $this->headers["hc-store"] = $this->store->code;
+        }
+    }
+
+    public function createChannel(): object
+    {
+        $channelData = array_merge(Channel::factory()->make()->toArray(), [
+            "website_id" => $this->website->id
+        ]);
+        return Channel::create($channelData); 
+    }
+
+    public function createStore(): object
+    {
+        $storeData = array_merge(Store::factory()->make()->toArray(), [
+            "channel_id" => $this->channel->id
+        ]);
+        return Store::create($storeData);
+    }
+
+    public function createRequest(): object
+    {
+        dd($this->headers);  
     }
 
 
